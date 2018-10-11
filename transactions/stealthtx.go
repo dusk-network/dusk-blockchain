59e6023f65bc7c78a3ca7a226313b143d5091965
BlockChainDev <kevtheappdev@gmail.com>
2018-10-11 22:58:31 +0100

    [StealthTx]
    - Added sizes in comments

--- transactions/stealthtx.go
+++ transactions/stealthtx.go
@@ -1,26 +1,26 @@
 package transactions
 
 type Stealth struct {
-	Version uint8
-	Type    uint8
-	R       []byte
-	TA      TypeAttributes
+	Version uint8          // 1 byte
+	Type    uint8          // 1 byte
+	R       []byte         // 32 bytes
+	TA      TypeAttributes // (m * 2565) + 32 + (n * 40) m = # inputs, n = # of outputs
 }
 
 type TypeAttributes struct {
-	Inputs   []Input
-	TxPubKey []byte
-	Outputs  []Output
+	Inputs   []Input  //  m * 2565 bytes
+	TxPubKey []byte   // 32 bytes
+	Outputs  []Output // n * 40 bytes
 }
 
 type Input struct {
-	KeyImage  []byte
-	TxID      []byte
-	Index     uint8
-	Signature []byte
+	KeyImage  []byte // 32 bytes
+	TxID      []byte // 32 bytes
+	Index     uint8  // 1 byte
+	Signature []byte // ~2500 bytes
 }
 
 type Output struct {
-	Amount uint64
-	P      []byte
+	Amount uint64 // 8 bytes
+	P      []byte // 32 bytes
 }
