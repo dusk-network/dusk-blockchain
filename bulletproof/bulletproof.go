624c0b775f13c193dc43b0d5f4bd5144368b7440
BlockChainDev <kevtheappdev@gmail.com>
2018-10-16 21:28:43 +0100

    [BP]
    - Prove aL and aR

--- bulletproof/bulletproof.go
+++ bulletproof/bulletproof.go
@@ -5,6 +5,7 @@ package bulletproof
 
 import (
 	"errors"
+	"math/big"
 
 	"github.com/toghrulmaharramov/dusk-go/ristretto"
 )
@@ -43,10 +44,10 @@ type Proof struct {
 	r []ristretto.Scalar // []scalar
 }
 
-// vecExp takes two scalars and returns a vector commitment C
+// vecExp takes two scalar arrays and returns a vector commitment C
 // the calculation goes as follows C = aG + bH
 // Where H is the Hash of the Generator G
-func vecExp(a ristretto.Scalar, b ristretto.Scalar) (ristretto.Point, error) {
+func vecExp(a, b []ristretto.Scalar) (ristretto.Point, error) {
 	result := ristretto.Point{} // defaults to zero
 
 	if len(a) != len(b) {
@@ -58,14 +59,17 @@ func vecExp(a ristretto.Scalar, b ristretto.Scalar) (ristretto.Point, error) {
 	}
 
 	// aG+bH
-	var aG ristretto.Point
-	aG.ScalarMultBase(&a)
 
-	var bH ristretto.Point
-	bH.SetBytes(&H)
-	bH.ScalarMult(&bH, &b)
+	for i := range a {
+		var aG ristretto.Point
+		aG.ScalarMultBase(&a[i])
 
-	result.Add(&aG, &bH)
+		var bH ristretto.Point
+		bH.SetBytes(&H)
+		bH.ScalarMult(&bH, &b[i])
+
+		result.Add(&aG, &bH)
+	}
 
 	return result, nil
 }
@@ -214,11 +218,137 @@ func scInv(a ristretto.Scalar) *ristretto.Scalar {
 	return &a
 }
 
+// hashCacheMash will take an existing hashCache
+// add n amount of byte slices
+// then return it all using hashToScalar
+func hashCacheMash(hashCache ristretto.Scalar, mashes ...[]byte) *ristretto.Scalar {
+
+	data := []byte{}
+	data = append(data, hashCache.Bytes()...)
+
+	for _, mash := range mashes {
+		data = append(data, mash...)
+	}
+
+	var s *ristretto.Scalar
+	s.Derive(data)
+	return s
+}
+
 // Prove takes an amount and a mask and constructs a bulletproof
 // Given a set of values v (0..2^N-1) and masks gamma, construct a range proof
-func Prove(amount, mask ristretto.Scalar) Proof {
+// Will do single proof for now, then multiple proof later on
+func Prove(v, gamma ristretto.Scalar) (Proof, error) {
 
-	return Proof{}
+	// TODO : check v is in correct range
+	// will produce an incorrect proof anyways
+	// but will be fast to exit quickly
+
+	// V = v *H + gamma * G
+	var V ristretto.Point
+	var vH ristretto.Point
+	vH.SetBytes(&H)
+	vH.ScalarMult(&vH, &v)
+	var gamG ristretto.Point
+	gamG.ScalarMultBase(&gamma)
+	V.Add(&vH, &gamG)
+
+	// This hash is updated for Fiat-Shamir throughout the proof
+	var hashCache ristretto.Scalar
+	hashCache.Derive(V.Bytes())
+
+	// PAPER LINES 36-37
+	var aL, aR [N]ristretto.Scalar
+
+	tempV := v.BigInt()
+
+	// zero and one scalars
+	var zero ristretto.Scalar
+	zero.SetZero()
+	var one ristretto.Scalar
+	one.SetOne()
+
+	for i := N - 1; i >= 0; i-- {
+		var basePow, e = big.NewInt(2), big.NewInt(int64(i))
+		basePow.Exp(basePow, e, nil)
+
+		tV := big.NewInt(0) // we don't want to edit the tempV value, so we have this dummy val
+
+		if (tV.Div(tempV, basePow)).Cmp(big.NewInt(0)) == 0 {
+			aL[i] = zero
+		} else {
+
+			aL[i] = one
+			tempV.Sub(tempV, basePow)
+		}
+		var aRi ristretto.Scalar
+		aRi.Sub(&aL[i], &one)
+		aR[i] = aRi
+	}
+
+	// DEBUG : Test to ensure we recover the values
+	testAL := big.NewInt(0)
+	testAR := big.NewInt(0)
+
+	for i := 0; i < N; i++ {
+
+		var basePow, e = big.NewInt(2), big.NewInt(int64(i))
+		basePow.Exp(basePow, e, nil)
+
+		if aL[i].Equals(&one) {
+			testAL = testAL.Add(testAL, basePow)
+		}
+		if aR[i].Equals(&zero) {
+			testAR = testAR.Add(testAR, basePow)
+		}
+	}
+
+	if testAL.Cmp(v.BigInt()) != 0 {
+		return Proof{}, errors.New("Wrong Value for AL")
+	}
+
+	if testAR.Cmp(v.BigInt()) != 0 {
+		return Proof{}, errors.New("Wrong Value for AL")
+	}
+
+	return Proof{}, nil
+}
+
+func computeK(y, z *ristretto.Scalar) ristretto.Scalar {
+	var res ristretto.Scalar
+	res.SetZero()
+
+	var one ristretto.Scalar
+	one.SetOne()
+	var two ristretto.Scalar
+	two.SetBigInt(big.NewInt(2))
+
+	vpy := vecPowers(*y, N)
+	vp1 := vecPowers(one, N)
+	ip, err := innerProduct(vp1, vpy)
+	if err != nil {
+		return ristretto.Scalar{}
+	}
+
+	var tempZ ristretto.Scalar
+	tempZ = *z
+
+	res.Sub(&res, tempZ.Square(&tempZ).Mul(&tempZ, &ip))
+
+	vp2 := vecPowers(two, N)
+	ip, err = innerProduct(vp1, vp2)
+	if err != nil {
+		return ristretto.Scalar{}
+	}
+
+	tempZ = *z
+
+	for i := 0; i < 3; i++ { //cubed
+		tempZ.Mul(&tempZ, &tempZ)
+	}
+
+	res.Sub(&res, tempZ.Mul(&tempZ, &ip))
+	return res
 }
 
 // Verify takes a bullet proof and
