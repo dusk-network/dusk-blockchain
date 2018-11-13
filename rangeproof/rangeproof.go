86ec8a52c9d8b77b323cc70e2e2fb110992fe342
BlockChainDev <kevtheappdev@gmail.com>
2018-11-13 12:44:26 +0000

    [BP]
    Refactored rangeproof to suit the new layout

--- rangeproof/rangeproof.go
+++ rangeproof/rangeproof.go
@@ -3,8 +3,8 @@ package rangeproof
 import (
 	"errors"
 	"fmt"
-	"math/big"
 
+	"github.com/toghrulmaharramov/dusk-go/rangeproof/pedersen"
 	"github.com/toghrulmaharramov/dusk-go/ristretto"
 )
 
@@ -29,301 +29,112 @@ var (
 
 // Proof is the constructed BulletProof
 type Proof struct {
-	V  []ristretto.Point // Curve points 32 bytes, multi-proof but limit size to one
-	A  ristretto.Point   // Curve point 32 bytes
-	S  ristretto.Point   // Curve point 32 bytes
-	T1 ristretto.Point   // Curve point 32 bytes
-	T2 ristretto.Point   // Curve point 32 bytes
+	V  ristretto.Point // Curve points 32 bytes, multi-proof but limit size to one
+	A  ristretto.Point // Curve point 32 bytes
+	S  ristretto.Point // Curve point 32 bytes
+	T1 ristretto.Point // Curve point 32 bytes
+	T2 ristretto.Point // Curve point 32 bytes
 
 	taux ristretto.Scalar //scalar
 	mu   ristretto.Scalar //scalar
+	t    ristretto.Scalar
 
 	l []ristretto.Scalar // []scalar
 	r []ristretto.Scalar // []scalar
 }
 
-// vecExp takes two scalar arrays and returns a vector commitment C
-// the calculation goes as follows C = aG + bH
-// Where H is the Hash of the Generator G
-func vecExp(a, b []ristretto.Scalar) (ristretto.Point, error) {
-	result := ristretto.Point{} // defaults to zero
+//cX means a commitment to the value x
 
-	if len(a) != len(b) {
-		return result, errors.New("length of scalar a does not equal length of scalar b")
+func Prove(v ristretto.Scalar) (Proof, error) {
+
+	ped := pedersen.New([]byte("dusk.BulletProof.vec1")) // XXX: this will set the generator, should we use the standard base
+
+	// Hash for Fiat-Shamir
+	hs := hashCacher{cache: []byte{}}
+
+	// compute commmitment to v
+	cV := ped.CommitToScalars(v)
+
+	// update Fiat-Shamir
+	hs.Append(cV.Value.Bytes())
+
+	// compute Bitcomments aL and aR to v
+	BitCommitment := BitCommit(v.BigInt())
+
+	// Compute A
+	cA := computeA(ped, BitCommitment.AL, BitCommitment.AR)
+
+	// Compute S
+	cS, sL, sR := computeS(ped)
+
+	// update Fiat-Shamir
+	hs.Append(cA.Value.Bytes())
+	hs.Append(cS.Value.Bytes())
+
+	// compute y and z
+	y, z := computeYAndZ(hs)
+
+	// compute polynomial
+	poly := computePoly(BitCommitment.AL, BitCommitment.AR, sL, sR, y, z, v)
+
+	// Compute T1 and T2
+	cT1 := ped.CommitToScalars(poly.t1)
+	cT2 := ped.CommitToScalars(poly.t2)
+
+	// update Fiat-Shamir
+	hs.Append(z.Bytes()) // XXX: why do we not add y to hashcache as it is a part of the transcript
+	hs.Append(cT1.Value.Bytes())
+	hs.Append(cT2.Value.Bytes())
+
+	// compute x
+	x := computeX(hs)
+
+	// compute taux which is just the polynomial for the blinding factors at a point x
+	taux := computeTaux(x, z, cV.BlindingFactor, cT1.BlindingFactor, cT2.BlindingFactor)
+
+	// TODO: compute mu
+
+	// compute l dot r
+	l := poly.computeL(x)
+	r := poly.computeR(x)
+	t, _ := innerProduct(l, r)
+
+	tPoly := poly.eval(x)
+	if !t.Equals(&tPoly) {
+		return Proof{}, errors.New("The t value computed from the t-poly, does not match the t value computed from the inner product of l and r")
 	}
 
-	if len(a) < N*M {
-		return result, errors.New("length of scalar a is not less than N*M")
+	polyt0 := poly.computeT0(y, z, v)
+	if !polyt0.Equals(&poly.t0) {
+		return Proof{}, errors.New("Mismatched values for t0")
 	}
 
-	// aG+bH
+	// TODO: calculate inner product proof
 
-	for i := range a {
-		var aG ristretto.Point
-		aG.ScalarMultBase(&a[i])
-
-		var bH ristretto.Point
-		bH.SetBytes(&H)
-		bH.ScalarMult(&bH, &b[i])
-
-		result.Add(&aG, &bH)
-	}
-
-	return result, nil
+	return Proof{
+		V:    cV.Value,
+		A:    cA.Value,
+		S:    cS.Value,
+		T1:   cT1.Value,
+		T2:   cT2.Value,
+		l:    l,
+		r:    r,
+		t:    t,
+		taux: taux,
+	}, nil
 }
 
-// Given a scalar, construct a vector of powers
-func vecPowers(a ristretto.Scalar, n uint8) []ristretto.Scalar {
+// A = kH + aL*G + aR * H
+func computeA(ped *pedersen.Pedersen, aL, aR [N]ristretto.Scalar) pedersen.Commitment {
 
-	res := make([]ristretto.Scalar, n)
+	cA := ped.CommitToVectors(aL[:], aR[:])
 
-	if n == 0 {
-		return res
-	}
-
-	// id
-	var k ristretto.Scalar
-	k.SetBytes(&Identity)
-	res[0] = k
-
-	if n == 1 {
-		return res
-	}
-	res[1] = a
-
-	for i := uint8(2); i < n; i++ {
-		res[i].Add(&res[i-1], &a)
-	}
-
-	return res
+	return cA
 }
 
-// Given two scalar arrays, construct the inner product
-func innerProduct(a, b []ristretto.Scalar) (ristretto.Scalar, error) {
+// S = kH + sL*G + sR * H
+func computeS(ped *pedersen.Pedersen) (pedersen.Commitment, [N]ristretto.Scalar, [N]ristretto.Scalar) {
 
-	var res ristretto.Scalar
-
-	if len(a) != len(b) {
-		return res, errors.New("Length of a does not equal length of b")
-	}
-
-	for i := 0; i < len(a); i++ {
-		res.MulAdd(&a[i], &b[i], &res)
-	}
-
-	return res, nil
-}
-
-// Given two scalar arrays, construct the Hadamard product
-func hadamard(a, b []ristretto.Scalar) ([]ristretto.Scalar, error) {
-
-	if len(a) != len(b) {
-		return nil, errors.New("Length of a does not equal length of b")
-	}
-
-	res := make([]ristretto.Scalar, len(a))
-
-	for i := 0; i < len(a); i++ {
-		res[i].Add(&a[i], &b[i])
-	}
-	return res, nil
-}
-
-// Given two curvepoint arrays, construct the Hadamard product
-func hadamard2(a, b []ristretto.Point) ([]ristretto.Point, error) {
-
-	if len(a) != len(b) {
-		return nil, errors.New("Length of a does not equal length of b")
-	}
-
-	res := make([]ristretto.Point, len(a))
-	for i := 0; i < len(a); i++ {
-		res[i].Add(&a[i], &b[i])
-	}
-
-	return res, nil
-}
-
-func vecAdd(a, b []ristretto.Scalar) ([]ristretto.Scalar, error) {
-	if len(a) != len(b) {
-		return nil, errors.New("Length of a does not equal b")
-	}
-
-	var res []ristretto.Scalar
-
-	for i := 0; i < len(a); i++ {
-		res[i].Add(&a[i], &b[i])
-	}
-
-	return res, nil
-}
-
-func vecSub(a, b []ristretto.Scalar) ([]ristretto.Scalar, error) {
-	if len(a) != len(b) {
-		return nil, errors.New("Length of a does not equal b")
-	}
-
-	res := make([]ristretto.Scalar, len(a))
-
-	for i := 0; i < len(a); i++ {
-		res[i].Sub(&a[i], &b[i])
-	}
-
-	return res, nil
-}
-
-func vecScal(a ristretto.Scalar, b []ristretto.Scalar) ([]ristretto.Scalar, error) {
-
-	var res []ristretto.Scalar
-
-	for i := 0; i < len(a); i++ {
-		res[i].Mul(&b[i], &a)
-	}
-
-	return res, nil
-}
-
-// Given a scalar, return the sum of its powers from 0 to n-1
-func vecPowerSum(a ristretto.Scalar, n uint64) ristretto.Scalar {
-
-	var res ristretto.Scalar
-	res.SetZero()
-
-	if n == 0 {
-		return res
-	}
-
-	res.SetOne()
-
-	if n == 1 {
-		return res
-	}
-
-	prev := a
-
-	for i := uint64(1); i < n; i++ {
-		if i > 1 {
-			prev.Mul(&prev, &a)
-		}
-		res.Add(&res, &prev)
-	}
-
-	return res
-}
-
-func scInv(a ristretto.Scalar) *ristretto.Scalar {
-	a.Inverse(&a)
-	return &a
-}
-
-// hashCacheMash will take an existing hashCache
-// add n amount of byte slices
-// then return it all using hashToScalar
-func hashCacheMash(hashCache ristretto.Scalar, mashes ...[]byte) ristretto.Scalar {
-
-	data := []byte{}
-	data = append(data, hashCache.Bytes()...)
-
-	for _, mash := range mashes {
-		data = append(data, mash...)
-	}
-
-	var s ristretto.Scalar
-	s.Derive(data)
-	return s
-}
-
-// Prove takes an amount and a mask and constructs a bulletproof
-// Given a set of values v (0..2^N-1) and masks gamma, construct a range proof
-// Will do single proof for now, then multiple proof later on
-func Prove(v, gamma ristretto.Scalar) (Proof, error) {
-
-	// TODO : check v is in correct range
-	// will produce an incorrect proof anyways
-	// but will be fast to exit quickly
-
-	// V = v *H + gamma * G
-	var V ristretto.Point
-	var vH ristretto.Point
-	vH.SetBytes(&H)
-	vH.ScalarMult(&vH, &v)
-	var gamG ristretto.Point
-	gamG.ScalarMultBase(&gamma)
-	V.Add(&vH, &gamG)
-
-	// This hash is updated for Fiat-Shamir throughout the proof
-	var hashCache ristretto.Scalar
-	hashCache.Derive(V.Bytes())
-
-	// PAPER LINES 36-37
-	var aL, aR [N]ristretto.Scalar
-
-	tempV := v.BigInt()
-
-	// zero and one scalars
-	var zero ristretto.Scalar
-	zero.SetZero()
-	var one ristretto.Scalar
-	one.SetOne()
-
-	for i := N - 1; i >= 0; i-- {
-		var basePow, e = big.NewInt(2), big.NewInt(int64(i))
-		basePow.Exp(basePow, e, nil)
-
-		tV := big.NewInt(0) // we don't want to edit the tempV value, so we have this dummy val
-
-		if (tV.Div(tempV, basePow)).Cmp(big.NewInt(0)) == 0 {
-			aL[i] = zero
-		} else {
-
-			aL[i] = one
-			tempV.Sub(tempV, basePow)
-		}
-		var aRi ristretto.Scalar
-		aRi.Sub(&aL[i], &one)
-		aR[i] = aRi
-	}
-
-	// DEBUG : Test to ensure we recover the values
-	testAL := big.NewInt(0)
-	testAR := big.NewInt(0)
-
-	for i := 0; i < N; i++ {
-
-		var basePow, e = big.NewInt(2), big.NewInt(int64(i))
-		basePow.Exp(basePow, e, nil)
-
-		if aL[i].Equals(&one) {
-			testAL = testAL.Add(testAL, basePow)
-		}
-		if aR[i].Equals(&zero) {
-			testAR = testAR.Add(testAR, basePow)
-		}
-	}
-
-	if testAL.Cmp(v.BigInt()) != 0 {
-		return Proof{}, errors.New("Wrong Value for AL")
-	}
-
-	if testAR.Cmp(v.BigInt()) != 0 {
-		return Proof{}, errors.New("Wrong Value for AL")
-	}
-
-	// PAPER LINES 38-39
-	var alpha ristretto.Scalar
-	alpha.Rand()
-
-	var alpG ristretto.Point
-	alpG.ScalarMultBase(&alpha)
-
-	A, err := vecExp(aL[:], aR[:])
-	if err != nil {
-		fmt.Println(len(aL), len(aR), M*N)
-		return Proof{}, err
-	}
-	A.Add(&A, &alpG)
-
-	// PAPER LINES 40-42
 	var sL, sR [N]ristretto.Scalar
 	for i := 0; i < N; i++ {
 		var randA ristretto.Scalar
@@ -335,114 +146,133 @@ func Prove(v, gamma ristretto.Scalar) (Proof, error) {
 		sR[i] = randB
 	}
 
-	var rho ristretto.Scalar
-	rho.Rand()
-	S, err := vecExp(sL[:], sR[:])
-	if err != nil {
-		return Proof{}, err
-	}
-	var rhoG ristretto.Point
-	rhoG.ScalarMultBase(&rho)
-	S.Add(&S, &rhoG)
+	cS := ped.CommitToVectors(sL[:], sR[:])
 
-	// PAPER LINES 43-45
-	hashCache = hashCacheMash(hashCache, A.Bytes(), S.Bytes())
-	y := hashCache
-	hashCache.Derive(hashCache.Bytes())
-	z := hashCache
-
-	var t0, tempZ ristretto.Scalar
-
-	tempZ = z
-	vp1 := vecPowers(one, N)
-	vpy := vecPowers(y, N)
-	if err != nil {
-		return Proof{}, err
-	}
-	ip, err := innerProduct(vp1, vpy)
-	if err != nil {
-		return Proof{}, err
-	}
-	t0.Add(&t0, tempZ.Mul(&z, &ip))
-	tempZ = z
-	t0.Add(&t0, tempZ.Square(&tempZ).Mul(&tempZ, &v))
-
-	k := computeK(&y, &z)
-	t0.Add(&t0, &k)
-
-	// DEBUG: Test the value of t0 has the correct form
-
-	hd, err := hadamard(aR[:], vecPowers(y, N))
-	if err != nil {
-		return Proof{}, err
-	}
-	ipt0, err := innerProduct(aL[:], hd)
-	if err != nil {
-		return Proof{}, err
-	}
-	test_t0 := ipt0
-
-	vS, err := vecSub(aL[:], aR[:])
-	if err != nil {
-		return Proof{}, err
-	}
-	ipt1, err := innerProduct(vS, vecPowers(y, N))
-	if err != nil {
-		return Proof{}, err
-	}
-	test_t0.Add(&test_t0, &ipt1)
-
-	var two ristretto.Scalar
-	two.SetBigInt(big.NewInt(2))
-	ipt2, err := innerProduct(vecPowers(two, N), aL[:])
-	test_t0.Add(&test_t0, &ipt2)
-
-	test_t0.Add(&test_t0, &k)
-	if test_t0.Equals(&t0) {
-		return Proof{}, errors.New("Wrong value for t0")
-	}
-	return Proof{}, nil
+	return cS, sL, sR
 }
 
-func computeK(y, z *ristretto.Scalar) ristretto.Scalar {
+func computeYAndZ(hs hashCacher) (ristretto.Scalar, ristretto.Scalar) {
+
+	var y ristretto.Scalar
+	y.Derive(hs.Result())
+
+	var z ristretto.Scalar
+	z.Derive(y.Bytes())
+
+	return y, z
+}
+func computeX(hs hashCacher) ristretto.Scalar {
+	var x ristretto.Scalar
+	x.Derive(hs.Result())
+	return x
+}
+
+// compute polynomial for blinding factors l61
+// N.B. tau1 means tau superscript 1
+func computeTaux(x, z, vBlind, t1Blind, t2Blind ristretto.Scalar) ristretto.Scalar {
+	tau1X := t1Blind.Mul(&x, &t1Blind)
+
+	var xsq ristretto.Scalar
+	xsq.Square(&x)
+
+	tau2Xsq := t2Blind.Mul(&xsq, &t2Blind)
+
+	var zSq ristretto.Scalar
+	zSq.Square(&z)
+
+	var zSqvBlind ristretto.Scalar
+	zSqvBlind.Mul(&zSq, &vBlind)
+
 	var res ristretto.Scalar
-	res.SetZero()
+	res.Add(tau1X, tau2Xsq)
+	res.Add(&res, &zSqvBlind)
 
-	var one ristretto.Scalar
-	one.SetOne()
-	var two ristretto.Scalar
-	two.SetBigInt(big.NewInt(2))
-
-	vpy := vecPowers(*y, N)
-	vp1 := vecPowers(one, N)
-	ip, err := innerProduct(vp1, vpy)
-	if err != nil {
-		return ristretto.Scalar{}
-	}
-
-	var tempZ ristretto.Scalar
-	tempZ = *z
-
-	res.Sub(&res, tempZ.Square(&tempZ).Mul(&tempZ, &ip))
-
-	vp2 := vecPowers(two, N)
-	ip, err = innerProduct(vp1, vp2)
-	if err != nil {
-		return ristretto.Scalar{}
-	}
-
-	tempZ = *z
-
-	for i := 0; i < 3; i++ { //cubed
-		tempZ.Mul(&tempZ, &tempZ)
-	}
-
-	res.Sub(&res, tempZ.Mul(&tempZ, &ip))
 	return res
 }
 
 // Verify takes a bullet proof and
 // returns true only if the proof was valid
-func Verify(p Proof) bool {
-	return false
+func Verify(p Proof) (bool, error) {
+
+	ped := pedersen.New([]byte("dusk.BulletProof.vec1"))
+	ped.BaseVector.Compute(2)
+
+	if len(p.l) != len(p.r) {
+		return false, errors.New("Sizes of l and r do not match")
+	}
+
+	if len(p.l) <= 0 {
+		fmt.Println("size of l is ", p.l)
+		return false, errors.New("size of l or r cannot be zero; empty proof")
+	}
+
+	// Reconstruct the challenges
+	hs := hashCacher{[]byte{}}
+	hs.Append(p.V.Bytes())
+	hs.Append(p.A.Bytes())
+	hs.Append(p.S.Bytes())
+
+	y, z := computeYAndZ(hs)
+	_ = y
+	_ = z
+
+	hs.Append(z.Bytes())
+	hs.Append(p.T1.Bytes())
+	hs.Append(p.T2.Bytes())
+
+	x := computeX(hs)
+
+	// compute l dot r
+	t, _ := innerProduct(p.l, p.r)
+
+	// compute tG + tauH = Z^2 * V + delta(y,z) -- Prove t0 is correct
+
+	// LHS
+	var tG ristretto.Point
+	tG.ScalarMult(&ped.BaseVector.Bases[1], &t)
+	var tauH ristretto.Point
+	tauH.ScalarMult(&ped.BaseVector.Bases[0], &p.taux)
+
+	var LHS ristretto.Point
+	LHS.Add(&tG, &tauH)
+
+	// RHS
+	var cT1x ristretto.Point
+	cT1x.ScalarMult(&p.T1, &x)
+
+	var xsq ristretto.Scalar
+	xsq.Square(&x)
+
+	var cT2xsq ristretto.Point
+	cT2xsq.ScalarMult(&p.T2, &xsq)
+
+	var zsq ristretto.Scalar
+	zsq.Square(&z)
+
+	var zsqV ristretto.Point
+	zsqV.ScalarMult(&p.V, &zsq)
+
+	var deltaG ristretto.Point
+	delta, err := computeDelta(y, z)
+	if err != nil {
+		return false, err
+	}
+
+	deltaG.ScalarMult(&ped.BaseVector.Bases[0], &delta)
+
+	var RHS1 ristretto.Point
+	var RHS2 ristretto.Point
+	var RHS ristretto.Point
+	RHS1.Add(&zsqV, &deltaG)
+	RHS2.Add(&cT2xsq, &cT1x)
+	RHS.Add(&RHS1, &RHS2)
+
+	if !LHS.Equals(&RHS) {
+		fmt.Println(LHS, RHS)
+		return false, errors.New("LHS != RHS; proof that t0 is correct is wrong")
+	}
+
+	// prove l(x) and r(x) is correct
+
+	return true, nil
 }
--- rangeproof/rangeproof_test.go
+++ rangeproof/rangeproof_test.go
@@ -1,6 +1,7 @@
 package rangeproof
 
 import (
+	"fmt"
 	"math/big"
 	"testing"
 
@@ -12,11 +13,53 @@ func TestProveBulletProof(t *testing.T) {
 
 	var amount ristretto.Scalar
 	amount.SetBigInt(big.NewInt(200))
-	// TODO: test values over the threshold, with named errors
 
-	var gamma ristretto.Scalar
-	gamma.Rand()
-
-	_, err := Prove(amount, gamma)
+	// Prove
+	p, err := Prove(amount)
+	fmt.Printf("%+v\n", p)
 	assert.Equal(t, nil, err)
+	_ = p
+
+	// Verify
+	// ok, err := Verify(p)
+	// assert.Equal(t, nil, err)
+	// assert.Equal(t, true, ok)
 }
+
+func TestComputeTau(t *testing.T) {
+	a := ristretto.Scalar{}
+	a.SetBigInt(big.NewInt(1))
+	b := ristretto.Scalar{}
+	b.SetBigInt(big.NewInt(2))
+	c := ristretto.Scalar{}
+	c.SetBigInt(big.NewInt(1))
+	d := ristretto.Scalar{}
+	d.SetBigInt(big.NewInt(1))
+	e := ristretto.Scalar{}
+	e.SetBigInt(big.NewInt(1))
+
+	res := computeTaux(a, b, c, d, e)
+
+	assert.Equal(t, int64(6), res.BigInt().Int64())
+
+}
+
+func TestComputeDelta(t *testing.T) {
+	a := ristretto.Scalar{}
+	a.SetBigInt(big.NewInt(1))
+	b := ristretto.Scalar{}
+	b.SetBigInt(big.NewInt(1))
+	res, _ := computeDelta(a, b)
+	t.Fail()
+	c := ristretto.Scalar{}
+	c.SetBigInt(big.NewInt(-15000))
+	fmt.Println(res.BigInt().Int64())
+	fmt.Println(c.BigInt().Int64())
+	fmt.Println(c.BigInt().Int64() - res.BigInt().Int64())
+}
+
+/*
+TODO: test values over the N threshold and named errors named errors
+input: 2^N+1
+output: error: value too large
+*/
