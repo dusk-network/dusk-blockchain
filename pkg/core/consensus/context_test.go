ad9801b219b88765e06a87a66668509392d0e536
BlockChainDev <kevtheappdev@gmail.com>
2019-01-08 01:35:47 +0000

    [consensus]
    - test func for context reset

--- pkg/core/consensus/context_test.go
+++ pkg/core/consensus/context_test.go
@@ -20,3 +20,22 @@ func TestNewContext(t *testing.T) {
 
 	assert.Equal(t, tau, ctx.Tau)
 }
+
+func TestReset(t *testing.T) {
+
+	tau := rand.Uint64()
+
+	keys, err := NewRandKeys()
+	assert.Equal(t, err, nil)
+
+	ctx, err := NewContext(tau, keys)
+
+	// check consensus values were resetted
+	assert.Nil(t, ctx.X, nil, nil)
+	assert.Nil(t, ctx.Y, nil, nil)
+	assert.Nil(t, ctx.Z, nil, nil)
+	assert.Nil(t, ctx.M, nil, nil)
+	assert.Nil(t, ctx.k, nil, nil)
+	assert.Equal(t, uint64(0), ctx.Q)
+	assert.Equal(t, uint64(0), ctx.d)
+}
