a69f74790008ebab89569affe8eb7c486f151e3e
Jules de Smit <julesdesmit@gmail.com>
2019-01-23 17:20:07 +0100

    [Block]
    - Move block code into its own package to avoid cyclical imports for consensus payloads

---
+++ pkg/p2p/wire/payload/block/block.go
@@ -0,0 +1,175 @@
+package block
+
+import (
+	"errors"
+	"io"
+	"time"
+
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/crypto/hash"
+
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/encoding"
+
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/crypto/merkletree"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/transactions"
+)
+
+// Block defines a block on the Dusk blockchain.
+type Block struct {
+	Header *Header
+	Txs    []merkletree.Payload
+}
+
+// NewBlock will return an empty Block with an empty BlockHeader.
+func NewBlock() *Block {
+	return &Block{
+		Header: &Header{
+			// CertImage should take up space from creation to
+			// ensure proper decoding during block selection.
+			CertImage: make([]byte, 32),
+		},
+	}
+}
+
+// NewEmptyBlock will return a fully populated empty block, to be used
+// for consensus purposes. Use NewBlock in any other circumstance.
+func NewEmptyBlock(prevHeader *Header) (*Block, error) {
+	block := &Block{
+		Header: &Header{
+			Height: prevHeader.Height + 1,
+			// CertImage and TxRoot should take up space from creation to
+			// ensure proper decoding during block selection.
+			CertImage: make([]byte, 32),
+			TxRoot:    make([]byte, 32),
+		},
+	}
+
+	if err := block.SetPrevBlock(prevHeader); err != nil {
+		return nil, err
+	}
+
+	// Set seed to hash of previous seed
+	seedHash, err := hash.Sha3256(prevHeader.Seed)
+	if err != nil {
+		return nil, err
+	}
+
+	block.Header.Seed = seedHash
+	block.SetTime(time.Now().Unix())
+	if err := block.SetHash(); err != nil {
+		return nil, err
+	}
+
+	return block, nil
+}
+
+// SetPrevBlock will set all the fields of the Block struct that are
+// taken from the previous block.
+func (b *Block) SetPrevBlock(prevHeader *Header) error {
+	b.Header.Height = prevHeader.Height + 1 // XXX: Can we move this else where, as it sets the currentHeight and not PrevHeight, as the func name suggests
+	b.Header.PrevBlock = prevHeader.Hash
+
+	return nil
+}
+
+// SetSeed will set the seed for the current block
+func (b *Block) SetSeed(Seed []byte) error {
+
+	b.Header.Seed = Seed
+
+	return nil
+}
+
+// SetTime will set the block timestamp.
+func (b *Block) SetTime(time int64) {
+	b.Header.Timestamp = time
+}
+
+// SetRoot will set the block merkle root hash.
+func (b *Block) SetRoot() error {
+	tree, err := merkletree.NewTree(b.Txs)
+	if err != nil {
+		return err
+	}
+
+	b.Header.TxRoot = tree.MerkleRoot
+	return nil
+}
+
+// AddTx will add a transaction to the block.
+func (b *Block) AddTx(tx *transactions.Stealth) {
+	b.Txs = append(b.Txs, tx)
+}
+
+// AddCertImage will take a hash from a Certificate and put
+// it in the block's CertImage field.
+func (b *Block) AddCertImage(cert *Certificate) error {
+	if cert.Hash == nil {
+		if err := cert.SetHash(); err != nil {
+			return err
+		}
+	}
+
+	b.Header.CertImage = cert.Hash
+	return nil
+}
+
+// Clear will empty out all the block's fields.
+func (b *Block) Clear() {
+	b.Header = &Header{}
+	b.Txs = nil
+}
+
+// SetHash will set the block hash.
+func (b *Block) SetHash() error {
+	return b.Header.SetHash()
+}
+
+// Encode a Block struct and write to w.
+func (b *Block) Encode(w io.Writer) error {
+	if err := b.Header.Encode(w); err != nil {
+		return err
+	}
+
+	lTxs := uint64(len(b.Txs))
+	if err := encoding.WriteVarInt(w, lTxs); err != nil {
+		return err
+	}
+
+	for _, v := range b.Txs {
+		tx, ok := v.(*transactions.Stealth)
+		if !ok {
+			return errors.New("non-tx object found in block txs array")
+		}
+
+		if err := tx.Encode(w); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Decode a Block struct from r into b.
+func (b *Block) Decode(r io.Reader) error {
+	b.Header = &Header{}
+	if err := b.Header.Decode(r); err != nil {
+		return err
+	}
+
+	lTxs, err := encoding.ReadVarInt(r)
+	if err != nil {
+		return err
+	}
+
+	b.Txs = make([]merkletree.Payload, lTxs)
+	for i := uint64(0); i < lTxs; i++ {
+		tx := &transactions.Stealth{}
+		if err := tx.Decode(r); err != nil {
+			return err
+		}
+
+		b.Txs[i] = tx
+	}
+
+	return nil
+}
---
+++ pkg/p2p/wire/payload/block/blockheader.go
@@ -0,0 +1,125 @@
+package block
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/crypto/hash"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/encoding"
+)
+
+// Header defines a block header on a Dusk block.
+type Header struct {
+	Height    uint64 // Block height
+	Timestamp int64  // Block timestamp
+
+	PrevBlock []byte // Hash of previous block (32 bytes)
+	Seed      []byte // Marshaled BLS signature or hash of the previous block seed (32 bytes)
+	TxRoot    []byte // Root hash of the merkle tree containing all txes (32 bytes)
+
+	Hash      []byte // Hash of all previous fields
+	CertImage []byte // Hash of the block certificate (32 bytes)
+}
+
+// SetHash will set this block header's hash by encoding all the relevant
+// fields and then hashing the result.
+func (b *Header) SetHash() error {
+	buf := new(bytes.Buffer)
+	if err := b.EncodeHashable(buf); err != nil {
+		return err
+	}
+
+	h, err := hash.Sha3256(buf.Bytes())
+	if err != nil {
+		return err
+	}
+
+	b.Hash = h
+	return nil
+}
+
+// EncodeHashable will encode all the fields needed from a Header to create
+// a block hash. Result will be written to w.
+func (b *Header) EncodeHashable(w io.Writer) error {
+	if err := encoding.WriteUint64(w, binary.LittleEndian, b.Height); err != nil {
+		return err
+	}
+
+	if err := encoding.WriteUint64(w, binary.LittleEndian, uint64(b.Timestamp)); err != nil {
+		return err
+	}
+
+	if err := encoding.Write256(w, b.PrevBlock); err != nil {
+		return err
+	}
+
+	if err := encoding.Write256(w, b.Seed); err != nil {
+		return err
+	}
+
+	if err := encoding.Write256(w, b.TxRoot); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Encode a Header struct and write to w.
+func (b *Header) Encode(w io.Writer) error {
+	if err := b.EncodeHashable(w); err != nil {
+		return err
+	}
+
+	if err := encoding.Write256(w, b.Hash); err != nil {
+		return err
+	}
+
+	if err := encoding.Write256(w, b.CertImage); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Decode a Header struct from r into b.
+func (b *Header) Decode(r io.Reader) error {
+	if err := encoding.ReadUint64(r, binary.LittleEndian, &b.Height); err != nil {
+		return err
+	}
+
+	var timestamp uint64
+	if err := encoding.ReadUint64(r, binary.LittleEndian, &timestamp); err != nil {
+		return err
+	}
+
+	b.Timestamp = int64(timestamp)
+	if err := encoding.Read256(r, &b.PrevBlock); err != nil {
+		return err
+	}
+
+	if err := encoding.Read256(r, &b.Seed); err != nil {
+		return err
+	}
+
+	if err := encoding.Read256(r, &b.TxRoot); err != nil {
+		return err
+	}
+
+	if err := encoding.Read256(r, &b.Hash); err != nil {
+		return err
+	}
+
+	if err := encoding.Read256(r, &b.CertImage); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// Bytes returns the block header, encoded as a slice of bytes.
+func (b *Header) Bytes() ([]byte, error) {
+	buf := new(bytes.Buffer)
+	err := b.Encode(buf)
+	return buf.Bytes(), err
+}
