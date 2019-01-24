868bf7b2fac9e3b5ff9baeb0a032efa4211e9f92
Jules de Smit <julesdesmit@gmail.com>
2019-01-24 15:13:17 +0100

    [ConsensusMsg]
    - Complete preliminary code for candidate payload

--- pkg/p2p/wire/payload/consensusmsg/candidate.go
+++ pkg/p2p/wire/payload/consensusmsg/candidate.go
@@ -6,21 +6,29 @@ import (
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/block"
 )
 
-// Candidate defines a candidate score consensus payload on the Dusk wire protocol.
+// Candidate defines a candidate consensus payload on the Dusk wire protocol.
 type Candidate struct {
 	Block *block.Block
+}
+
+// NewCandidate returns a candidate consensus payload with the specified block.
+func NewCandidate(blk *block.Block) *Candidate {
+	return &Candidate{
+		Block: blk,
+	}
 }
 
 // Encode a Candidate struct to w
 // Implements Msg interface.
 func (c *Candidate) Encode(w io.Writer) error {
-	return nil
+	return c.Block.Encode(w)
 }
 
 // Decode a Candidate from r
 // Implements Msg interface.
 func (c *Candidate) Decode(r io.Reader) error {
-	return nil
+	c.Block = &block.Block{}
+	return c.Block.Decode(r)
 }
 
 // Type returns the consensus payload identifier.
--- pkg/p2p/wire/payload/consensusmsg/candidate_test.go
+++ pkg/p2p/wire/payload/consensusmsg/candidate_test.go
@@ -1,7 +1,87 @@
 package consensusmsg
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/crypto"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/block"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/transactions"
+)
 
 func TestCandidateEncodeDecode(t *testing.T) {
+	b := block.NewBlock()
 
+	// Add 10 standard transactions
+	for i := 0; i < 10; i++ {
+		byte32 := []byte{1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4}
+
+		sig, _ := crypto.RandEntropy(2000)
+
+		txPubKey, _ := crypto.RandEntropy(32)
+		s := transactions.NewTX(transactions.StandardType, nil)
+		in := transactions.NewInput(txPubKey, txPubKey, 0, sig)
+		s.AddInput(in)
+		s.AddTxPubKey(txPubKey)
+
+		out := transactions.NewOutput(200, byte32, sig)
+		s.AddOutput(out)
+		if err := s.SetHash(); err != nil {
+			t.Fatal(err)
+		}
+
+		b.AddTx(s)
+	}
+
+	// Spoof previous hash and seed
+	h, _ := crypto.RandEntropy(32)
+	b.Header.PrevBlock = h
+	b.Header.Seed = h
+
+	// Add cert image
+	rand1, _ := crypto.RandEntropy(32)
+	rand2, _ := crypto.RandEntropy(32)
+
+	sig, _ := crypto.RandEntropy(32)
+
+	cert := block.NewCertificate(sig)
+	for i := 1; i < 4; i++ {
+		step := block.NewStep(uint32(i))
+		step.AddData(rand1, rand2)
+		cert.AddStep(step)
+	}
+
+	if err := cert.SetHash(); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := b.AddCertImage(cert); err != nil {
+		t.Fatal(err)
+	}
+
+	// Finish off
+	if err := b.SetRoot(); err != nil {
+		t.Fatal(err)
+	}
+
+	b.SetTime(time.Now().Unix())
+	if err := b.SetHash(); err != nil {
+		t.Fatal(err)
+	}
+
+	pl := NewCandidate(b)
+
+	buf := new(bytes.Buffer)
+	if err := pl.Encode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	pl2 := &Candidate{}
+	if err := pl2.Decode(buf); err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, pl, pl2)
 }
