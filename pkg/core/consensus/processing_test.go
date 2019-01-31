ff76e2012dcc1b806270bea47518328ea9284173
Jules de Smit <julesdesmit@gmail.com>
2019-01-31 18:10:40 +0100

    [Consensus]
    - Remove redundant test

--- pkg/core/consensus/processing_test.go
+++ pkg/core/consensus/processing_test.go
@@ -3,44 +3,8 @@ package consensus
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/block"
 )
-
-func TestProcessMsgReduction(t *testing.T) {
-	// Create context
-	ctx, err := provisionerContext()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Create a reduction phase voting message and get their amount of votes
-	emptyBlock, err := block.NewEmptyBlock(ctx.LastHeader)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	votes, msg, err := newVoteReduction(ctx, 500, emptyBlock.Header.Hash)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Process the message
-	valid, retVotes, err := processMsg(ctx, msg)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// Message should be valid
-	if retVotes == 0 || !valid {
-		t.Fatal("message was not valid")
-	}
-
-	// Votes should be equal
-	assert.Equal(t, votes, retVotes)
-}
-
-// TODO: implement missing message types
 
 func TestFaultyMsgRound(t *testing.T) {
 	// Create context
