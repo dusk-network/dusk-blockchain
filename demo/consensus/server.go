9c46a370d34f822458084ae6926a7832588a9ef3
BlockChainDev <kevtheappdev@gmail.com>
2019-02-12 18:49:33 +0000

    [demo-consensus]
    - Add config and nonce to server
    - Send a mock consensus message upon connecting to a peer

--- demo/consensus/server.go
+++ demo/consensus/server.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"net"
+	"os"
 
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/crypto"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/peer/peermgr"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/consensusmsg"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/protocol"
 )
 
 type Server struct {
+	nonce uint64
 	peers []*peermgr.Peer
+	cfg   *peermgr.Config
 }
 
 func (s *Server) OnAccept(conn net.Conn) {
 	fmt.Printf("someone has tried to connect to us, with the address %s \n", conn.RemoteAddr().String())
 
 	// Add peer to server's list of peers
-	peer := peermgr.NewPeer(conn, true, s.setupPeerConfig())
+	peer := peermgr.NewPeer(conn, true, s.cfg)
+	peer.Run()
 	s.peers = append(s.peers, peer)
 }
 
@@ -26,17 +31,22 @@ func (s *Server) OnConnection(conn net.Conn, addr string) {
 	fmt.Printf("we have connected to the node with the address %s \n", conn.RemoteAddr().String())
 
 	// Add peer to server's list of peers
-	peer := peermgr.NewPeer(conn, false, s.setupPeerConfig())
+	peer := peermgr.NewPeer(conn, false, s.cfg)
+	peer.Run()
 	s.peers = append(s.peers, peer)
+
+	// Send a mock message upon connection
+	msg := mockConsensusMsg()
+	err := peer.Write(msg)
+	fmt.Println(err)
 }
 
-func (s *Server) OnConsensus(peer *peermgr.Peer, msg *payload.MsgConsensus) {
-	fmt.Printf("we have received a consensus message from peer %s , message type %d", peer.RemoteAddr().String(), msg.Payload.Type())
+func OnConsensus(peer *peermgr.Peer, msg *payload.MsgConsensus) {
+	fmt.Printf("we have received a consensus message from peer %s , message type %d\n", peer.RemoteAddr().String(), msg.Payload.Type())
 }
 
-func (s *Server) setupPeerConfig() *peermgr.Config {
-
-	hndlr := peermgr.ResponseHandler{
+func setupPeerConfig(nonce uint64) *peermgr.Config {
+	handler := peermgr.ResponseHandler{
 		OnHeaders:        nil,
 		OnNotFound:       nil,
 		OnGetData:        nil,
@@ -46,7 +56,7 @@ func (s *Server) setupPeerConfig() *peermgr.Config {
 		OnGetAddr:        nil,
 		OnGetBlocks:      nil,
 		OnBlock:          nil,
-		OnConsensus:      s.OnConsensus,
+		OnConsensus:      OnConsensus,
 		OnCertificate:    nil,
 		OnCertificateReq: nil,
 		OnMemPool:        nil,
@@ -55,10 +65,34 @@ func (s *Server) setupPeerConfig() *peermgr.Config {
 		OnReject:         nil,
 		OnInv:            nil,
 	}
-
 	return &peermgr.Config{
 		Magic:   protocol.TestNet,
-		Nonce:   rand.Uint64(),
-		Handler: hndlr,
+		Nonce:   nonce,
+		Handler: handler,
 	}
+}
+
+func mockConsensusMsg() *payload.MsgConsensus {
+	byte32, err := crypto.RandEntropy(32)
+	byte33, err := crypto.RandEntropy(33)
+	if err != nil {
+		fmt.Println("Could not create a Mock Consensus message")
+		os.Exit(1)
+	}
+	byte64, err := crypto.RandEntropy(64)
+	if err != nil {
+		fmt.Println("Could not create a Mock Consensus message")
+		os.Exit(1)
+	}
+	reductionPayload, err := consensusmsg.NewReduction(byte32, byte33, byte32)
+	if err != nil {
+		fmt.Println("Could not create a Mock Consensus message")
+		os.Exit(1)
+	}
+	msg, err := payload.NewMsgConsensus(10, 10, byte32, 10, byte64, byte32, reductionPayload)
+	if err != nil {
+		fmt.Println("Could not create a Mock Consensus message")
+		os.Exit(1)
+	}
+	return msg
 }
