c9421657de60c9b2b287b6f06a7b2a88bfcf9f59
BlockChainDev <kevtheappdev@gmail.com>
2019-02-14 12:24:04 +0000

    [collection]
    - Small typo
    - Elaborated on some comments

--- pkg/core/consensus/collection/blockcollection.go
+++ pkg/core/consensus/collection/blockcollection.go
@@ -12,10 +12,10 @@ import (
 
 // Block is the function that is ran by provisioners and generators during block generation.
 // After the function terminates, the context object should hold a block hash that
-// came with the highest score it found during message processing, which should then
-// be voted on in later phases.
+// came with the highest score found during message processing, which should then
+// be voted on in later phases, if the corresponding block was also received for it.
 func Block(ctx *user.Context) error {
-	// Keep track of those who have already propagated their messages
+	// Keeps track of those who have already propagated their score messages
 	senders := make(map[string]bool)
 
 	// Keep track of the highest bid score seen
@@ -24,7 +24,6 @@ func Block(ctx *user.Context) error {
 	// Empty out our CandidateBlocks map
 	ctx.CandidateBlocks = make(map[string]*block.Block)
 
-	// Start the timer
 	timer := time.NewTimer(user.CandidateTime * (time.Duration(ctx.Multiplier)))
 
 	for {
@@ -35,30 +34,29 @@ func Block(ctx *user.Context) error {
 			pl := m.Payload.(*consensusmsg.Candidate)
 			blockHash := hex.EncodeToString(pl.Block.Header.Hash)
 
-			// See if we already have it
+			// Add candidate block to map of known candidate blocks, if unknown
 			if ctx.CandidateBlocks[blockHash] != nil {
 				break
 			}
-
-			// Add to the mapping
 			ctx.CandidateBlocks[blockHash] = pl.Block
+
 		case m := <-ctx.CandidateScoreChan:
 			pl := m.Payload.(*consensusmsg.CandidateScore)
 			pkEd := hex.EncodeToString(m.PubKey)
 
-			// Check if this node's candidate was already recorded
+			// Add candidate score to map of known candidate scores, if unknown
 			if senders[pkEd] {
 				break
 			}
-
-			// Log information
 			senders[pkEd] = true
 
-			// If the score is higher than our current one, replace
+			// If the received score is higher than our current one, replace
+			// the current blockhash and score with the received block hash and score.
 			if pl.Score > highest {
 				highest = pl.Score
 				ctx.BlockHash = pl.CandidateHash
 			}
+
 		}
 	}
 }
