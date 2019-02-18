bcc8ecdb1b6870f6f581e983549070b3c1236443
Jules de Smit <julesdesmit@gmail.com>
2019-02-18 19:31:06 +0100

    [Agreement]
    - Comment out certificate related code
    - Save only our own sets
    - Count votes and voters
    - Fix channel typo

--- pkg/core/consensus/agreement/setagreement.go
+++ pkg/core/consensus/agreement/setagreement.go
@@ -6,7 +6,6 @@ import (
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/core/consensus/sortition"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/core/consensus/user"
 
-	"gitlab.dusk.network/dusk-core/dusk-go/pkg/crypto/bls"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/consensusmsg"
 )
@@ -14,6 +13,9 @@ import (
 // SignatureSet is the function that runs during the signature set reduction
 // phase, used to collect vote sets and find a winning signature set.
 func SignatureSet(ctx *user.Context, c chan bool) {
+	// Keep track of all nodes who have voted
+	voters := make(map[string]bool)
+
 	// Make a mapping of steps, pointing to a slice of vote sets.
 	sets := make(map[uint8][][]*consensusmsg.Vote)
 
@@ -21,6 +23,14 @@ func SignatureSet(ctx *user.Context, c chan bool) {
 		select {
 		case m := <-ctx.SigSetAgreementChan:
 			pl := m.Payload.(*consensusmsg.SigSetAgreement)
+			pkEd := hex.EncodeToString(m.PubKey)
+
+			// Check if this node's vote is already recorded
+			if voters[pkEd] {
+				break
+			}
+
+			voters[pkEd] = true
 
 			// Get amount of votes
 			votes := sortition.Verify(ctx.CurrentCommittee, m.PubKey)
@@ -34,46 +44,53 @@ func SignatureSet(ctx *user.Context, c chan bool) {
 				sets[m.Step] = append(sets[m.Step], pl.VoteSet)
 			}
 
+			// Gossip the message
+			if err := ctx.SendMessage(ctx.Magic, m); err != nil {
+				// Log
+				c <- false
+				return
+			}
+
 			// Check if we have exceeded the limit
 			limit := float64(len(ctx.CurrentCommittee)) * 0.75
 			if len(sets[m.Step]) < int(limit) {
 				break
 			}
 
-			// Populate certificate
-			ctx.Certificate.SRPubKeys = make([][]byte, len(pl.VoteSet))
-			for i := 0; i < len(pl.VoteSet); i++ {
-				pkBLS := hex.EncodeToString(pl.VoteSet[i].PubKey)
-				ctx.Certificate.SRPubKeys = append(ctx.Certificate.SRPubKeys, ctx.NodeBLS[pkBLS])
-			}
+			// // Populate certificate
+			// ctx.Certificate.SRPubKeys = make([][]byte, len(pl.VoteSet))
+			// for i := 0; i < len(pl.VoteSet); i++ {
+			// 	pkBLS := hex.EncodeToString(pl.VoteSet[i].PubKey)
+			// 	ctx.Certificate.SRPubKeys = append(ctx.Certificate.SRPubKeys, ctx.NodeBLS[pkBLS])
+			// }
 
-			agSig := &bls.Signature{}
-			if err := agSig.Decompress(ctx.SigSetVotes[0].Sig); err != nil {
-				// Log
-				c <- false
-				return
-			}
+			// agSig := &bls.Signature{}
+			// if err := agSig.Decompress(ctx.SigSetVotes[0].Sig); err != nil {
+			// 	// Log
+			// 	c <- false
+			// 	return
+			// }
 
-			// Batch all the signatures together
-			for i, vote := range ctx.SigSetVotes {
-				if i != 0 {
-					continue
-				}
+			// // Batch all the signatures together
+			// for i, vote := range ctx.SigSetVotes {
+			// 	if i != 0 {
+			// 		continue
+			// 	}
 
-				sig := &bls.Signature{}
-				if err := sig.Decompress(vote.Sig); err != nil {
-					// Log
-					c <- false
-					return
-				}
+			// 	sig := &bls.Signature{}
+			// 	if err := sig.Decompress(vote.Sig); err != nil {
+			// 		// Log
+			// 		c <- false
+			// 		return
+			// 	}
 
-				agSig.Aggregate(sig)
-				ctx.Certificate.SRPubKeys[i] = vote.PubKey
-			}
+			// 	agSig.Aggregate(sig)
+			// 	ctx.Certificate.SRPubKeys[i] = vote.PubKey
+			// }
 
-			cSig := agSig.Compress()
-			ctx.Certificate.SRBatchedSig = cSig
-			ctx.Certificate.SRStep = ctx.Step
+			// cSig := agSig.Compress()
+			// ctx.Certificate.SRBatchedSig = cSig
+			// ctx.Certificate.SRStep = ctx.Step
 			c <- true
 			return
 		}
@@ -88,7 +105,8 @@ func SendSigSet(ctx *user.Context) error {
 		return err
 	}
 
-	pl, err := consensusmsg.NewSigSetAgreement(ctx.BlockHash, hash, ctx.SigSetVotes)
+	pl, err := consensusmsg.NewSigSetAgreement(ctx.BlockHash, hash, ctx.SigSetVotes,
+		ctx.Certificate.BRStep)
 	if err != nil {
 		return err
 	}
