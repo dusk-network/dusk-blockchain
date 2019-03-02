227d50327bcbe63aabc89b280a6d0dd77586fbc6
Jules de Smit <julesdesmit@gmail.com>
2019-03-02 09:49:41 +0100

    [Consensus]
    - Add rudimentary publist management function

---
+++ pkg/core/consensus/generators.go
@@ -0,0 +1,19 @@
+package consensus
+
+import (
+	"math/big"
+
+	ristretto "github.com/bwesterb/go-ristretto"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/core/consensus/zkproof"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/transactions"
+)
+
+func (c *Consensus) AddGenerator(bid *transactions.Bid) {
+	// Set X value in publist
+	dScalar := ristretto.Scalar{}
+	dScalar.SetBigInt(big.NewInt(0).SetUint64(bid.Output.Amount))
+	mScalar := ristretto.Scalar{}
+	mScalar.SetBigInt(big.NewInt(0).SetBytes(bid.M))
+	x := zkproof.CalculateX(dScalar, mScalar)
+	c.ctx.PubList = append(c.ctx.PubList, x)
+}
