278b780516222c0408ca40452b1057252a7fb30d
Jules de Smit <julesdesmit@gmail.com>
2019-03-04 11:34:24 +0100

    [Consensus]
    - Use Uint64ToScalar for d

--- pkg/core/consensus/generators.go
+++ pkg/core/consensus/generators.go
@@ -1,9 +1,6 @@
 package consensus
 
 import (
-	"math/big"
-
-	ristretto "github.com/bwesterb/go-ristretto"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/core/consensus/zkproof"
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire/payload/transactions"
 )
@@ -12,8 +9,7 @@ import (
 // It then gets put into the public list.
 func (c *Consensus) AddGenerator(bid transactions.Bid) {
 	// Set X value in publist
-	dScalar := ristretto.Scalar{}
-	dScalar.SetBigInt(big.NewInt(0).SetUint64(bid.Output.Amount))
+	dScalar := zkproof.Uint64ToScalar(bid.Output.Amount)
 	m := zkproof.BytesToScalar(bid.M)
 	x := zkproof.CalculateX(dScalar, m)
 	c.ctx.PubList.AddBid(x.Bytes())
