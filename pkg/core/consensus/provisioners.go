e422fd2d8deda9cbb93493daef64594a268c2d6a
Jules de Smit <julesdesmit@gmail.com>
2019-03-05 10:18:17 +0100

    [Consensus]
    - Use new Committee type for setting up provisioner committee

--- pkg/core/consensus/provisioners.go
+++ pkg/core/consensus/provisioners.go
@@ -1,10 +1,8 @@
 package consensus
 
 import (
-	"bytes"
 	"encoding/hex"
 	"math"
-	"sort"
 
 	"gitlab.dusk.network/dusk-core/dusk-go/pkg/crypto/bls"
 
@@ -67,7 +65,7 @@ func (c *Consensus) SetupProvisioners() error {
 
 // AddProvisionerInfo will add the tx info to a provisioner, and update their total
 // stake amount.
-func (c *Consensus) AddProvisionerInfo(tx *transactions.Stealth, amount uint64) {
+func (c *Consensus) AddProvisionerInfo(tx *transactions.Stealth, amount uint64) error {
 	// Set information on blockchain struct
 	info := tx.TypeInfo.(*transactions.Stake)
 	pkEd := hex.EncodeToString(info.PubKeyEd)
@@ -78,10 +76,13 @@ func (c *Consensus) AddProvisionerInfo(tx *transactions.Stealth, amount uint64) 
 	c.ctx.NodeWeights[pkEd] = c.provisioners[pkEd].TotalAmount
 	pkBLS := hex.EncodeToString(info.PubKeyBLS)
 	c.ctx.NodeBLS[pkBLS] = info.PubKeyEd
-	c.ctx.Committee = append(c.ctx.Committee, info.PubKeyEd)
+	if err := c.ctx.Committee.AddMember(info.PubKeyEd); err != nil {
+		return err
+	}
 
 	// Sort committee list
-	c.SortProvisioners()
+	c.ctx.Committee.Sort()
+	return nil
 }
 
 // RemoveOldProvisioners will run through all known nodes and check if they have
@@ -112,23 +113,14 @@ func (c *Consensus) RemoveOldProvisioners() {
 				continue
 			}
 
-			for i, pkBytes := range c.ctx.Committee {
-				pkStr := hex.EncodeToString(pkBytes)
-				if pk == pkStr {
-					c.ctx.Committee = append(c.ctx.Committee[:i], c.ctx.Committee[i+1:]...)
+			for _, member := range *c.ctx.Committee {
+				if pk == member.String() {
+					c.ctx.Committee.RemoveMember(member[:])
 				}
 			}
 		}
 	}
 
 	// Sort committee list
-	c.SortProvisioners()
-}
-
-// SortProvisioners will sort the Committee slice on the context object
-// lexicographically.
-func (c *Consensus) SortProvisioners() {
-	sort.SliceStable(c.ctx.Committee, func(i, j int) bool {
-		return bytes.Compare(c.ctx.Committee[i], c.ctx.Committee[j]) < bytes.Compare(c.ctx.Committee[j], c.ctx.Committee[i])
-	})
+	c.ctx.Committee.Sort()
 }
