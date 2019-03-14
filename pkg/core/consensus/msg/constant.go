7dde7a2746aa4767d230026fe8055c1d610accad
Jules de Smit <julesdesmit@gmail.com>
2019-03-14 12:00:50 +0100

    [Msg]
    - Add selection result topic
    - Rename signature verification function
    - Remove vote verification

--- pkg/core/consensus/msg/constant.go
+++ pkg/core/consensus/msg/constant.go
@@ -1,6 +1,7 @@
 package msg
 
 const RoundUpdateTopic = "roundupdate"
+const SelectionResultTopic = "selectionresult"
 const NewProvisionerTopic = "newprovisioner"
 const ProvisionerAddedTopic = "provisioneradded"
 const QuitTopic = "quit"
