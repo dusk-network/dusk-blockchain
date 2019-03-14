d2c00f12873977d5d06598d8f0cea6a39166fadc
Jules de Smit <julesdesmit@gmail.com>
2019-03-14 16:35:04 +0100

    [Msg]
    - Add topics for initialization, reduction results, and outgoing reduction and agreement messages

--- pkg/core/consensus/msg/constant.go
+++ pkg/core/consensus/msg/constant.go
@@ -1,7 +1,11 @@
 package msg
 
+const InitializationTopic = "initialization"
 const RoundUpdateTopic = "roundupdate"
 const SelectionResultTopic = "selectionresult"
+const ReductionResultTopic = "reductionresult"
 const NewProvisionerTopic = "newprovisioner"
 const ProvisionerAddedTopic = "provisioneradded"
+const OutgoingReductionTopic = "outgoingreduction"
+const OutgoingAgreementTopic = "outgoingagreement"
 const QuitTopic = "quit"
