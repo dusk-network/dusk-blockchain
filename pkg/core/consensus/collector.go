94998de11f59e051f0dfacfdb33f0303be9965ab
Jules de Smit <julesdesmit@gmail.com>
2019-03-20 22:07:29 +0100

    [Consensus]
    - format NewEventSubscriber calls

--- pkg/core/consensus/collector.go
+++ pkg/core/consensus/collector.go
@@ -165,7 +165,8 @@ type RoundCollector struct {
 func InitRoundCollector(eventBus *wire.EventBus) *RoundCollector {
 	roundChan := make(chan uint64, 1)
 	roundCollector := &RoundCollector{roundChan}
-	go wire.NewEventSubscriber(eventBus, roundCollector, string(msg.RoundUpdateTopic)).Accept()
+	go wire.NewEventSubscriber(eventBus, roundCollector,
+		string(msg.RoundUpdateTopic)).Accept()
 	return roundCollector
 }
 
@@ -183,7 +184,8 @@ type PhaseCollector struct {
 func InitPhaseCollector(eventBus *wire.EventBus) *PhaseCollector {
 	phaseUpdateChan := make(chan []byte)
 	collector := &PhaseCollector{phaseUpdateChan}
-	go wire.NewEventSubscriber(eventBus, collector, string(msg.SigSetAgreementTopic)).Accept()
+	go wire.NewEventSubscriber(eventBus, collector,
+		string(msg.SigSetAgreementTopic)).Accept()
 	return collector
 }
 
