190506bfbe8b502a20e4a786d1566ebcdf294867
emanuele francioni <emanuele@dusk.network>
2019-03-26 16:17:35 +0100

    wired datadog client to the BlockCollector

--- pkg/log/monitor/datadog.go
+++ pkg/log/monitor/datadog.go
@@ -1,21 +1,48 @@
 package monitor
 
 import (
-	"gitlab.dusk.network/dusk-core/dusk-go/pkg/log"
+	"time"
+
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/core/consensus"
+	"gitlab.dusk.network/dusk-core/dusk-go/pkg/p2p/wire"
+	datadog "gopkg.in/zorkian/go-datadog-api.v2"
 )
 
-type DataDog struct {
-}
-
-func (d *DataDog) Connect(logChan <-chan *log.Event) {
-	go func() {
-		for {
-			ev := <-logChan
-			d.SendToDog(ev)
+func launchBlockTimeMonitor(eventBus *wire.EventBus, client *datadog.Client, roundChan <-chan uint64) {
+	var lastBlockTime *time.Time
+	for {
+		<-roundChan
+		nowRef := time.Now()
+		now := &nowRef
+		if lastBlockTime == nil {
+			lastBlockTime = now
+			continue
 		}
-	}()
+
+		blockDuration := now.Sub(*lastBlockTime)
+		lastBlockTime = now
+
+		dataType := "consensus_time"
+		metric := newData(dataType, float64(now.Unix()), float64(blockDuration.Seconds()))
+
+		//TODO: handler error
+		_ = client.PostMetrics(metric)
+	}
 }
 
-func (d *DataDog) SendToDog(ev *log.Event) {
-	//do your thing
+func newData(dataType string, timestamp float64, scalar float64) []datadog.Metric {
+	dataPoint := datadog.DataPoint{&timestamp, &scalar}
+	return []datadog.Metric{
+		datadog.Metric{
+			Metric: &dataType,
+			Points: []datadog.DataPoint{dataPoint},
+		},
+	}
+}
+
+func LaunchDataDog(eventBus *wire.EventBus) {
+	client := datadog.NewClient("6378ad8a96906938795b580341e6cf88", "4361397144bbd1ee3b27fcde599e940f736fa101")
+
+	roundChan := consensus.InitRoundUpdate(eventBus)
+	launchBlockTimeMonitor(eventBus, client, roundChan)
 }
