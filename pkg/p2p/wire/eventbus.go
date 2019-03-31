3a31d7d60373773d62406f303299efcc9a681328
BlockChainDev <kevtheappdev@gmail.com>
2019-03-31 16:10:12 +0100

    [eventbus]
    - add rwlock to event-bus

--- pkg/p2p/wire/eventbus.go
+++ pkg/p2p/wire/eventbus.go
@@ -4,10 +4,12 @@ import (
 	"bytes"
 	"fmt"
 	"math/rand"
+	"sync"
 )
 
 // EventBus - box for handlers and callbacks.
 type EventBus struct {
+	busLock     sync.RWMutex
 	handlers    map[string][]*eventHandler
 	broadcaster []*eventHandler
 }
@@ -20,6 +22,7 @@ type eventHandler struct {
 // New returns new EventBus with empty handlers.
 func New() *EventBus {
 	return &EventBus{
+		sync.RWMutex{},
 		make(map[string][]*eventHandler),
 		nil,
 	}
@@ -33,6 +36,8 @@ func (bus *EventBus) doSubscribe(topic string, handler *eventHandler) {
 
 // Subscribe subscribes to a topic with an asynchronous callback.
 func (bus *EventBus) Subscribe(topic string, messageChannel chan<- *bytes.Buffer) uint32 {
+	bus.busLock.Lock()
+	defer bus.busLock.Unlock()
 	id := rand.Uint32()
 	bus.doSubscribe(topic, &eventHandler{
 		id, messageChannel,
@@ -44,6 +49,8 @@ func (bus *EventBus) Subscribe(topic string, messageChannel chan<- *bytes.Buffer
 // Unsubscribe removes a handler defined for a topic.
 // Returns error if there are no handlers subscribed to the topic.
 func (bus *EventBus) Unsubscribe(topic string, id uint32) error {
+	bus.busLock.Lock()
+	defer bus.busLock.Unlock()
 	if _, ok := bus.handlers[topic]; ok {
 		for i, handler := range bus.handlers[topic] {
 			if handler.id == id {
@@ -60,6 +67,8 @@ func (bus *EventBus) Unsubscribe(topic string, id uint32) error {
 // to the event handler the function creates. The ID can later be used to
 // unsubscribe from all topics.
 func (bus *EventBus) SubscribeAll(messageChannel chan<- *bytes.Buffer) uint32 {
+	bus.busLock.Lock()
+	defer bus.busLock.Unlock()
 	id := rand.Uint32()
 	bus.broadcaster = append(bus.broadcaster, &eventHandler{
 		id, messageChannel,
@@ -71,6 +80,8 @@ func (bus *EventBus) SubscribeAll(messageChannel chan<- *bytes.Buffer) uint32 {
 // UnsubscribeAll will unsubscribe from all topics. The function takes an ID
 // which points to the handler associated to the caller.
 func (bus *EventBus) UnsubscribeAll(id uint32) {
+	bus.busLock.Lock()
+	defer bus.busLock.Unlock()
 	for i, handler := range bus.broadcaster {
 		if handler.id == id {
 			bus.broadcaster = append(bus.broadcaster[:i], bus.broadcaster[i+1:]...)
@@ -80,12 +91,16 @@ func (bus *EventBus) UnsubscribeAll(id uint32) {
 
 // HasHandler returns true if exists any handler is subscribed to the topic.
 func (bus *EventBus) HasHandler(topic string) bool {
+	bus.busLock.RLock()
+	defer bus.busLock.RUnlock()
 	_, ok := bus.handlers[topic]
 	return ok
 }
 
 // Publish executes callback defined for a topic.
 func (bus *EventBus) Publish(topic string, messageBuffer *bytes.Buffer) {
+	bus.busLock.Lock()
+	defer bus.busLock.Unlock()
 	if handlers, ok := bus.handlers[topic]; ok {
 		go bus.doPublish(handlers, messageBuffer)
 	}
