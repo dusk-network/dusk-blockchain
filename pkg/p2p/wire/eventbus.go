6a7425e8c7afee77d237c8811e91620f6cee713d
Jules de Smit <julesdesmit@gmail.com>
2019-04-01 12:31:00 +0200

    [Wire]
    - Add Georgis suggested patch for the event bus

--- pkg/p2p/wire/eventbus.go
+++ pkg/p2p/wire/eventbus.go
@@ -110,6 +110,10 @@ func (bus *EventBus) Publish(topic string, messageBuffer *bytes.Buffer) {
 
 func (bus *EventBus) doPublish(handlers []*eventHandler, messageBuffer *bytes.Buffer) {
 	for _, handler := range handlers {
-		handler.messageChannel <- messageBuffer
+		select {
+		case handler.messageChannel <- messageBuffer:
+		default:
+			fmt.Printf("handler.messageChannel buffer failed for handler.id %d\n", handler.id)
+		}
 	}
 }
