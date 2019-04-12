d387f22fcfed840af38fc17c632b0e98f8a0d013
Jules de Smit <julesdesmit@gmail.com>
2019-04-12 17:03:43 +0200

    Add AsyncState

--- pkg/core/consensus/state.go
+++ pkg/core/consensus/state.go
@@ -2,25 +2,40 @@ package consensus
 
 import (
 	"fmt"
+	"math/rand"
 	"strconv"
 	"sync"
 	"time"
 )
+
+var empty struct{}
 
 type (
 	State interface {
 		fmt.Stringer
 		Round() uint64
 		Step() uint8
+		SubscribeState() *StateSubscriber
 		Update(uint64)
 		IncrementStep()
 		Cmp(round uint64, step uint8) int
 	}
 
+	AsyncState struct {
+		Step  uint8
+		Round uint64
+	}
+
 	SyncState struct {
-		Lock  sync.RWMutex
-		round uint64
-		step  uint8
+		Lock             sync.RWMutex
+		round            uint64
+		step             uint8
+		stateSubscribers []*StateSubscriber
+	}
+
+	StateSubscriber struct {
+		StateChan chan struct{}
+		id        uint32
 	}
 
 	Timer struct {
@@ -29,8 +44,36 @@ type (
 	}
 )
 
+func (a *AsyncState) Cmp(round uint64, step uint8) int {
+	cmp := a.Round - round
+	if cmp == 0 {
+		return int(a.Step) - int(step)
+	}
+
+	return int(cmp)
+}
+
+func (a *AsyncState) String() string {
+	return "round: " + strconv.Itoa(int(a.Round)) +
+		" / step: " + strconv.Itoa(int(a.Step))
+}
+
 func NewState() *SyncState {
-	return &SyncState{sync.RWMutex{}, 0, 1}
+	return &SyncState{
+		round:            0,
+		step:             1,
+		stateSubscribers: make([]*StateSubscriber, 0),
+	}
+}
+
+func (s *SyncState) SubscribeState() *StateSubscriber {
+	sub := &StateSubscriber{
+		id:        rand.Uint32(),
+		StateChan: make(chan struct{}, 1),
+	}
+
+	s.stateSubscribers = append(s.stateSubscribers, sub)
+	return sub
 }
 
 func (s *SyncState) Round() uint64 {
@@ -61,6 +104,9 @@ func (s *SyncState) IncrementStep() {
 	s.Lock.Lock()
 	s.step++
 	s.Lock.Unlock()
+	for _, sub := range s.stateSubscribers {
+		sub.StateChan <- empty
+	}
 }
 
 // Cmp returns negative number if the SyncState is in the future, 0 if they are the same and positive if the SyncState is in the past
