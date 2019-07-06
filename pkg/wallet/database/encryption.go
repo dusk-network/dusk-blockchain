b0ed34cd14abbdfdd008e4271c3c615893230882
BlockChainDev <kevtheappdev@gmail.com>
2019-07-06 13:05:13 +0100

    [wallet]
    - Remove unused function

--- pkg/wallet/database/encryption.go
+++ pkg/wallet/database/encryption.go
@@ -1,15 +1,5 @@
 package database
 
-import (
-	"golang.org/x/crypto/sha3"
-)
-
-func createHash(key []byte) []byte {
-	hasher := sha3.New256()
-	hasher.Write([]byte(key))
-	return hasher.Sum(nil)
-}
-
 func encrypt(data []byte, passphrase []byte) ([]byte, error) {
 	return data, nil
 }
