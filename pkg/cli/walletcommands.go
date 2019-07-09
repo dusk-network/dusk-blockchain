261a157a28f2dad7a6723fa4931b46a0664bf17b
BlockChainDev <kevtheappdev@gmail.com>
2019-07-09 21:13:05 +0100

    [cli]
    - Calculate WireTx hash

--- pkg/cli/walletcommands.go
+++ pkg/cli/walletcommands.go
@@ -178,12 +178,11 @@ func transferCMD(args []string, publisher wire.EventBroker, rpcBus *wire.RPCBus)
 		return
 	}
 
-	hash, err := tx.Hash()
+	_, err = wireTx.CalculateHash()
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "%s\n", err.Error())
 		return
 	}
-	wireTx.TxID = hash
 	fmt.Fprintf(os.Stdout, "hash: %s\n", hex.EncodeToString(wireTx.TxID))
 
 	publisher.Publish(string(topics.Tx), buf)
@@ -307,12 +306,11 @@ func sendStakeCMD(args []string, publisher wire.EventBroker, rpcBus *wire.RPCBus
 		return
 	}
 
-	hash, err := tx.Hash()
+	_, err = wireTx.CalculateHash()
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "%s\n", err.Error())
 		return
 	}
-	wireTx.TxID = hash
 	fmt.Fprintf(os.Stdout, "hash: %s\n", hex.EncodeToString(wireTx.TxID))
 
 	publisher.Publish(string(topics.Tx), buf)
@@ -377,12 +375,11 @@ func sendBidCMD(args []string, publisher wire.EventBroker, rpcBus *wire.RPCBus) 
 		return
 	}
 
-	hash, err := tx.Hash()
+	_, err = wireTx.CalculateHash()
 	if err != nil {
 		fmt.Fprintf(os.Stdout, "%s\n", err.Error())
 		return
 	}
-	wireTx.TxID = hash
 	fmt.Fprintf(os.Stdout, "hash: %s\n", hex.EncodeToString(wireTx.TxID))
 
 	publisher.Publish(string(topics.Tx), buf)
