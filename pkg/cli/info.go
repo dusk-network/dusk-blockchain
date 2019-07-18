5a99482de0363ff22b687a204dcbf6e1e169488b
Jules de Smit <julesdesmit@gmail.com>
2019-07-18 14:07:02 +0200

    Fix typos

--- pkg/cli/info.go
+++ pkg/cli/info.go
@@ -9,7 +9,7 @@ var commandInfo = map[string]string{
 		Loads the encrypted wallet file.`,
 	"createfromseed": `Usage: createfromseed [seed] [password]
 		Loads the encrypted wallet file from a hex seed.`,
-	"sync": "Sync the wallet with the attached node blockchain."
+	"sync": "Sync the wallet with the attached node blockchain.",
 	"balance": `Usage: balance
 		Prints the balance of the loaded wallet. Make sure to use the sync command first.`,
 	"transfer": `Usage: transfer [amount] [address] [password]
@@ -21,6 +21,6 @@ var commandInfo = map[string]string{
 	"startprovisioner": `Send a signal to the connected DUSK node to start participating in consensus as a provisioner.`,
 	"startblockgenerator": `Usage: startblockgenerator [bidtxhash]
 		Send a signal to the connected DUSK node to start participating in consensus as a block generator`,
-	"showlogs": "Close the shell and show the internal logs on the terminal. Press any key to return to the shell."
+	"showlogs":  "Close the shell and show the internal logs on the terminal. Press any key to return to the shell.",
 	"exit/quit": `Shut down the node and close the console`,
 }
