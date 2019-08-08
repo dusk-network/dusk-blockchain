daa97ca14a058df7e810db7467a93bd396cc9d42
Georgi Goranov <georgi@dusk.network>
2019-08-08 17:39:13 +0300

    [gql] Additional blocks query tests

--- pkg/gql/query/blocks_test.go
+++ pkg/gql/query/blocks_test.go
@@ -41,3 +41,114 @@ func TestBlocksByHeight(t *testing.T) {
 	`
 	assertQuery(t, query, response)
 }
+
+func TestBlocksByHash(t *testing.T) {
+	query := `
+		{
+		  blocks(hash: "GU3RPuimCsAXqCxBwOLAJJjXX0h1Q1EHLzkqCF1GliA=") {
+			header {
+               hash
+			   height
+			}
+		  },
+		}
+		`
+	response := `
+		{
+        	"data": {
+        		"blocks": [
+        			{
+        				"header": {
+        					"hash": "GU3RPuimCsAXqCxBwOLAJJjXX0h1Q1EHLzkqCF1GliA=",
+        					"height": 0
+        				}
+        			}
+        		]
+        	}
+        }
+	`
+	assertQuery(t, query, response)
+}
+
+func TestBlocksByHashes(t *testing.T) {
+	query := `
+		{
+		  blocks(hashes: ["m/UOOUu4E0b4uNtCvd0oWsNEJgwCSg34CLr3YBQX10g=", "lGfF53TrG0gl0IwFmaCwgV/KXawW2WkAJoVO2NHyKck="] ) {
+			header {
+               hash
+			   height
+			}
+		  },
+		}
+		`
+	response := `
+		{
+        	"data": {
+        		"blocks": [
+        			{
+        				"header": {
+        					"hash": "m/UOOUu4E0b4uNtCvd0oWsNEJgwCSg34CLr3YBQX10g=",
+        					"height": 1
+        				}
+        			},
+        			{
+        				"header": {
+        					"hash": "lGfF53TrG0gl0IwFmaCwgV/KXawW2WkAJoVO2NHyKck=",
+        					"height": 2
+        				}
+        			}
+        		]
+        	}
+        }
+	`
+	assertQuery(t, query, response)
+
+	// Test Blocks By Range (same response expected)
+	query = `
+		{
+		  blocks(range: [1,2] ) {
+			header {
+               hash
+			   height
+			}
+		  },
+		}
+		`
+	assertQuery(t, query, response)
+}
+
+func TestBlocksTxs(t *testing.T) {
+	query := `
+		{
+		  blocks(height: -1) {
+			header {
+			   height
+			}
+			transactions {
+				txid
+				txtype
+			}
+		  }
+		}
+		`
+	response := `
+		{
+        	"data": {
+        		"blocks": [
+        			{
+        				"header": {
+        					"height": 2
+        				},
+        				"transactions": [
+        					{
+        						"txid": "Ts4G1WI0BRutCsW5m5UaAqWdVyTrib3bhVHz1q47JnU=",
+        						"txtype": "3"
+        					}
+        				]
+        			}
+        		]
+        	}
+        }
+	`
+	assertQuery(t, query, response)
+}
