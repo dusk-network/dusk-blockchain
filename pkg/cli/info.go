e99137aa40efcc46b69f055091da8eeb633badda
Jules de Smit <julesdesmit@gmail.com>
2019-08-13 17:00:25 +0200

    Update descriptions of startprovisioner and startblockgenerator

--- pkg/cli/info.go
+++ pkg/cli/info.go
@@ -17,9 +17,9 @@ var commandInfo = map[string]string{
 		Stake a given amount of DUSK, to enter the provisioner committee. If you wish to participate in consensus, make sure to use the startprovisioner command afterwards.`,
 	"bid": `Usage: bid [amount] [locktime] [password]
 		Bid a given amount of DUSK, to allow participation as a block generator in consensus. To start generating blocks, use the startblockgenerator command, supplying the hash returned by the bid command. Note that the transaction needs to be included in a block before this will work.`,
-	"startprovisioner": `Turns on the provisioner components in the node, needed to perform consensus. If you have not staked, the node will only relay consensus messages, until you stake any amount of DUSK.`,
+	"startprovisioner": `Turns on the provisioner components in the node, needed to participate in consensus. If you have not staked, the node will only relay consensus messages, until you stake any amount of DUSK.`,
 	"startblockgenerator": `Usage: startblockgenerator [bidtxhash]
-		Send a signal to the connected DUSK node to start participating in consensus as a block generator. Specified bid tx must be included in a block before trying to start the block generation component.`,
+		Turns on the block generation components in your node, needed to generate candidate blocks. Specified bid tx must be included in a block before trying to start the block generation component.`,
 	"showlogs":  "Close the shell and show the internal logs on the terminal. Press enter to return to the shell.",
 	"exit/quit": `Shut down the node and close the console`,
 }
