002bb0799d82ea8b05af7cb75e262a41e4219778
Jules de Smit <julesdesmit@gmail.com>
2019-09-03 17:58:21 +0200

    Update CLI information

--- pkg/cli/info.go
+++ pkg/cli/info.go
@@ -11,16 +11,16 @@ var commandInfo = map[string]string{
 		Loads the encrypted wallet file from a hex seed.`,
 	"balance": `Usage: balance
 		Prints the balance of the loaded wallet.`,
-	"transfer": `Usage: transfer [amount] [address] [password]
+	"transfer": `Usage: transfer [amount] [address]
 		Send DUSK to a given address.`,
-	"stake": `Usage: stake [amount] [locktime] [password]
-		Stake a given amount of DUSK, to allow participation as a provisioner in consensus.`,
-	"bid": `Usage: bid [amount] [locktime] [password]
-		Bid a given amount of DUSK, to allow participation as a block generator in consensus.`,
+	"stake": `Usage: stake 
+		Stake a set amount of DUSK, to allow participation as a provisioner in consensus. The amount is determined by the value specified with the 'setdefaultvalue' command, or the config if none was specified.`,
+	"bid": `Usage: bid 
+		Bid a set amount of DUSK, to allow participation as a block generator in consensus. The amount is determined by the value specified with the 'setdefaultvalue' command, or the config if none was specified.`,
 	"setdefaultlocktime": `Usage: setdefaultlocktime [locktime]
-		Sets the locktime for automatic consensus transactions.`,
+		Sets the locktime for consensus transactions.`,
 	"setdefaultvalue": `Usage: setdefaultvalue [amount]
-		Sets the default value for automatic consensus transactions.`,
+		Sets the default value for consensus transactions.`,
 	"showlogs":  "Close the shell and show the internal logs on the terminal. Press enter to return to the shell.",
 	"exit/quit": `Shut down the node and close the console`,
 }
