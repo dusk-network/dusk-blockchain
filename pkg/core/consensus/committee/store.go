c1b23c5d899a09fcd34d3af9ec94e31a5942c09b
Jules de Smit <julesdesmit@gmail.com>
2019-09-18 16:03:24 +0200

    Rename Extractor to Cache
    The reason for this is that the Stakers struct
    can already extract committees by itself. The Extractor is merely just
    a cache, saving the client the hassle of having to re-extract
    the committee for each event.
    Additionally, the Stakers struct has been removed from the Cache.
    It is now passed into the required methods, to avoid needing to
    declare methods to adjust state on the interface types.

--- pkg/core/consensus/committee/store.go
+++ pkg/core/consensus/committee/store.go
@@ -9,34 +9,33 @@ type (
 	// Committee is the interface for operations depending on the set of Provisioners
 	// extracted for a given step
 	Committee interface {
-		IsMember([]byte, uint64, uint8) bool
-		Quorum() int
+		IsMember(user.Stakers, []byte, uint64, uint8) bool
+		Quorum(user.Stakers) int
 	}
 
 	// Foldable represents a Committee which can be packed into a bitset, to drastically
 	// decrease the size needed for committee representation over the wire.
 	Foldable interface {
 		Committee
-		Pack(sortedset.Set, uint64, uint8) uint64
-		Unpack(uint64, uint64, uint8) sortedset.Set
+		Pack(user.Stakers, sortedset.Set, uint64, uint8) uint64
+		Unpack(user.Stakers, uint64, uint64, uint8) sortedset.Set
 	}
 
-	// Extractor is a wrapper around the Stakers struct, and contains the phase-specific
+	// Cache is a wrapper around the Stakers struct, and contains the phase-specific
 	// information, as well as a voting committee cache. It calls methods on the
 	// Stakers, passing its own parameters to extract the desired info for a specific
 	// phase.
-	Extractor struct {
-		user.Stakers
+	Cache struct {
 		round          uint64
 		committeeCache map[uint8]user.VotingCommittee
 	}
 )
 
-// NewExtractor returns a committee extractor which maintains it's own cache.
+// NewCache returns a committee extractor which maintains it's own cache.
 // The extractor will still have an empty Stakers field, which should be populated by
 // information received on round updates.
-func NewExtractor() *Extractor {
-	return &Extractor{
+func NewCache() *Cache {
+	return &Cache{
 		committeeCache: make(map[uint8]user.VotingCommittee),
 	}
 }
@@ -44,23 +43,23 @@ func NewExtractor() *Extractor {
 // PregenerateCommittees will generate committees for a given range of steps.
 // This method is best called when the Stakers field is updated, allowing the user to
 // do some of the calculation before any events come in.
-func (e *Extractor) PregenerateCommittees(round uint64, initialStep, stepAmount uint8, size int) {
+func (e *Cache) PregenerateCommittees(stakers user.Stakers, round uint64, initialStep, stepAmount uint8, size int) {
 	for i := initialStep; i <= initialStep+stepAmount; i++ {
-		e.UpsertCommitteeCache(round, i, size)
+		e.UpsertCommitteeCache(stakers, round, i, size)
 	}
 }
 
 // UpsertCommitteeCache will return a voting committee for a given round, step and size.
 // If the committee has not yet been produced before, it is put on the cache. If it has,
 // it is simply retrieved and returned.
-func (e *Extractor) UpsertCommitteeCache(round uint64, step uint8, size int) user.VotingCommittee {
+func (e *Cache) UpsertCommitteeCache(stakers user.Stakers, round uint64, step uint8, size int) user.VotingCommittee {
 	if round > e.round {
 		e.round = round
 		e.committeeCache = make(map[uint8]user.VotingCommittee)
 	}
 	votingCommittee, found := e.committeeCache[step]
 	if !found {
-		votingCommittee = e.Stakers.CreateVotingCommittee(round, e.Stakers.TotalWeight, step, size)
+		votingCommittee = stakers.CreateVotingCommittee(round, stakers.TotalWeight, step, size)
 		e.committeeCache[step] = votingCommittee
 	}
 	return votingCommittee
