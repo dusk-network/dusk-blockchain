34a40e79a24143dd38190f4846e65a1fcff482c4
Jules de Smit <julesdesmit@gmail.com>
2019-09-20 11:59:04 +0200

    Fix maintainer tests, by adapting them to the new provisioner and bidlist setup

--- pkg/core/consensus/maintainer/maintainer_test.go
+++ pkg/core/consensus/maintainer/maintainer_test.go
@@ -2,7 +2,6 @@ package maintainer_test
 
 import (
 	"bytes"
-	"encoding/binary"
 	"math/rand"
 	"os"
 	"testing"
@@ -18,7 +17,6 @@ import (
 	"github.com/dusk-network/dusk-blockchain/pkg/core/database/lite"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/transactor"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire"
-	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/encoding"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/wallet"
 	"github.com/dusk-network/dusk-blockchain/pkg/wallet/database"
@@ -33,7 +31,7 @@ const pass = "password"
 // Test that the maintainer will properly send new stake and bid transactions, when
 // one is about to expire, or if none exist.
 func TestMaintainStakesAndBids(t *testing.T) {
-	bus, txChan := setupMaintainerTest(t)
+	bus, txChan, p, keys, m := setupMaintainerTest(t)
 	defer os.RemoveAll(dbPath)
 	defer os.Remove("wallet.dat")
 
@@ -44,16 +42,22 @@ func TestMaintainStakesAndBids(t *testing.T) {
 	assert.True(t, txs[0].Type() == transactions.BidType)
 	assert.True(t, txs[1].Type() == transactions.StakeType)
 
-	// Populate committee and bidlist
-	propagateTxValues(txs, bus, 1)
-
-	time.Sleep(200 * time.Millisecond)
-
-	// Send round update, to update the end heights.
-	consensus.UpdateRound(bus, 1)
-
 	// Send round update, to a point where the buffer will kick in
-	consensus.UpdateRound(bus, 6)
+	// We should also add ourselves to the provisioners and the bidlist,
+	// so that the maintainer gets the proper ending heights.
+	// Provisioners
+	member := consensus.MockMember(keys)
+	member.Stakes[0].EndHeight = 10
+	p.Set.Insert(member.PublicKeyBLS)
+	p.Members[string(member.PublicKeyBLS)] = member
+	// Bidlist
+	bl := make([]user.Bid, 1)
+	var mArr [32]byte
+	copy(mArr[:], m.Bytes())
+	bid := user.Bid{mArr, mArr, 10}
+	bl[0] = bid
+	bus.Publish(msg.RoundUpdateTopic, consensus.MockRoundUpdateBuffer(2, p, bl))
+	bus.Publish(msg.RoundUpdateTopic, consensus.MockRoundUpdateBuffer(6, p, bl))
 
 	// We should get another set of two txs
 	txs = receiveTxs(t, txChan)
@@ -64,16 +68,15 @@ func TestMaintainStakesAndBids(t *testing.T) {
 
 // Ensure the maintainer does not keep sending bids and stakes until they are included.
 func TestSendOnce(t *testing.T) {
-	bus, txChan := setupMaintainerTest(t)
+	bus, txChan, p, _, _ := setupMaintainerTest(t)
 	defer os.RemoveAll(dbPath)
 	defer os.Remove("wallet.dat")
 
 	// receive first txs
-	txs := receiveTxs(t, txChan)
-	propagateTxValues(txs, bus, 1)
+	_ = receiveTxs(t, txChan)
 
-	// Update round. We should not receive any new txs
-	consensus.UpdateRound(bus, 1)
+	// Update round
+	bus.Publish(msg.RoundUpdateTopic, consensus.MockRoundUpdateBuffer(2, p, nil))
 
 	select {
 	case <-txChan:
@@ -83,67 +86,7 @@ func TestSendOnce(t *testing.T) {
 	}
 }
 
-func propagateTxValues(txs []transactions.Transaction, bus *wire.EventBus, height uint64) {
-	for _, tx := range txs {
-		switch tx.Type() {
-		case transactions.BidType:
-			bidTx := tx.(*transactions.Bid)
-			var dScalar ristretto.Scalar
-			dScalar.UnmarshalBinary(bidTx.Outputs[0].Commitment.Bytes())
-			var mScalar ristretto.Scalar
-			mScalar.UnmarshalBinary(bidTx.M)
-			x := zkproof.CalculateX(dScalar, mScalar)
-			var bid user.Bid
-			copy(bid.X[:], x.Bytes())
-			copy(bid.M[:], bidTx.M)
-			bid.EndHeight = height + bidTx.Lock
-			propagateBid(bid, bus)
-		case transactions.StakeType:
-			stake := tx.(*transactions.Stake)
-			propagateStake(stake, height, bus)
-		}
-	}
-}
-
-func propagateBid(bid user.Bid, bus *wire.EventBus) {
-	buf := new(bytes.Buffer)
-	if err := encoding.Write256(buf, bid.X[:]); err != nil {
-		panic(err)
-	}
-
-	if err := encoding.Write256(buf, bid.M[:]); err != nil {
-		panic(err)
-	}
-
-	if err := encoding.WriteUint64(buf, binary.LittleEndian, bid.EndHeight); err != nil {
-		panic(err)
-	}
-
-	bus.Publish(msg.BidListTopic, buf)
-}
-
-func propagateStake(tx *transactions.Stake, startHeight uint64, bus *wire.EventBus) {
-	buffer := bytes.NewBuffer(tx.PubKeyEd)
-	if err := encoding.WriteVarBytes(buffer, tx.PubKeyBLS); err != nil {
-		panic(err)
-	}
-
-	if err := encoding.WriteUint64(buffer, binary.LittleEndian, tx.Outputs[0].EncryptedAmount.BigInt().Uint64()); err != nil {
-		panic(err)
-	}
-
-	if err := encoding.WriteUint64(buffer, binary.LittleEndian, startHeight); err != nil {
-		panic(err)
-	}
-
-	if err := encoding.WriteUint64(buffer, binary.LittleEndian, startHeight+tx.Lock); err != nil {
-		panic(err)
-	}
-
-	bus.Publish(msg.NewProvisionerTopic, buffer)
-}
-
-func setupMaintainerTest(t *testing.T) (*wire.EventBus, chan *bytes.Buffer) {
+func setupMaintainerTest(t *testing.T) (*wire.EventBus, chan *bytes.Buffer, *user.Provisioners, user.Keys, ristretto.Scalar) {
 	// Initial setup
 	bus := wire.NewEventBus()
 	wdb, err := database.New(dbPath)
@@ -162,9 +105,19 @@ func setupMaintainerTest(t *testing.T) (*wire.EventBus, chan *bytes.Buffer) {
 
 	k, err := w.ReconstructK()
 	assert.NoError(t, err)
-	assert.NoError(t, maintainer.Launch(bus, w.ConsensusKeys().BLSPubKeyBytes, zkproof.CalculateM(k), transactor.New(w, db), 10, 10, 5))
+	mScalar := zkproof.CalculateM(k)
+	m, err := maintainer.New(bus, w.ConsensusKeys().BLSPubKeyBytes, mScalar, transactor.New(w, db), 10, 10, 5)
+	assert.NoError(t, err)
+	go m.Listen()
 
-	return bus, txChan
+	// Mock provisioners, and insert our wallet values
+	p, _ := consensus.MockProvisioners(10)
+	// Note: we don't need to mock the bidlist as we should not be included if we want to trigger a bid transaction
+
+	// Send round update, to start the maintainer.
+	bus.Publish(msg.RoundUpdateTopic, consensus.MockRoundUpdateBuffer(1, p, nil))
+
+	return bus, txChan, p, w.ConsensusKeys(), mScalar
 }
 
 func receiveTxs(t *testing.T, txChan chan *bytes.Buffer) []transactions.Transaction {
