878b46b6c8fe13e42e6f0762f9922c1f03e2e5b1
Georgi Goranov <georgi@dusk.network>
2019-09-26 13:40:37 +0300

    RPCBus default call wrappers

---
+++ pkg/util/nativeutils/rpcbus/methods.go
@@ -0,0 +1,132 @@
+package rpcbus
+
+import (
+	"bytes"
+
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/encoding"
+)
+
+func (rb *RPCBus) LoadWallet(password string) (string, error) {
+
+	pBuf := bytes.NewBufferString(password)
+	req := NewRequest(*pBuf, -1)
+	pubKeyBuf, err := rb.Call(LoadWallet, req)
+	if err != nil {
+		return "", err
+	}
+
+	return pubKeyBuf.String(), nil
+}
+
+func (rb *RPCBus) CreateFromSeed(seed, password string) (string, error) {
+
+	buf := new(bytes.Buffer)
+	if err := encoding.WriteString(buf, seed); err != nil {
+		return "", err
+	}
+
+	if err := encoding.WriteString(buf, password); err != nil {
+		return "", err
+	}
+
+	req := NewRequest(*buf, -1)
+	pubKeyBuf, err := rb.Call(CreateFromSeed, req)
+	if err != nil {
+		return "", err
+	}
+
+	return pubKeyBuf.String(), nil
+}
+
+func (rb *RPCBus) CreateWallet(password string) (string, error) {
+
+	buf := new(bytes.Buffer)
+	if err := encoding.WriteString(buf, password); err != nil {
+		return "", err
+	}
+
+	req := NewRequest(*buf, -1)
+	pubKeyBuf, err := rb.Call(CreateWallet, req)
+	if err != nil {
+		return "", err
+	}
+
+	return pubKeyBuf.String(), nil
+}
+
+func (rb *RPCBus) SendBidTx(amount, lockTime uint64) ([]byte, error) {
+
+	buf := new(bytes.Buffer)
+	if err := encoding.WriteUint64LE(buf, amount); err != nil {
+		return nil, err
+	}
+
+	if err := encoding.WriteUint64LE(buf, lockTime); err != nil {
+		return nil, err
+	}
+
+	req := NewRequest(*buf, -1)
+	txIdBuf, err := rb.Call(SendBidTx, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return txIdBuf.Bytes(), nil
+}
+
+func (rb *RPCBus) SendStakeTx(amount, lockTime uint64) (string, error) {
+
+	buf := new(bytes.Buffer)
+	if err := encoding.WriteUint64LE(buf, amount); err != nil {
+		return "", err
+	}
+
+	if err := encoding.WriteUint64LE(buf, lockTime); err != nil {
+		return "", err
+	}
+
+	req := NewRequest(*buf, -1)
+	txIdBuf, err := rb.Call(SendStakeTx, req)
+	if err != nil {
+		return "", err
+	}
+
+	return txIdBuf.String(), nil
+}
+
+func (rb *RPCBus) SendStandardTx(amount uint64, pubkey string) (string, error) {
+
+	buf := new(bytes.Buffer)
+
+	if err := encoding.WriteUint64LE(buf, amount); err != nil {
+		return "", err
+	}
+
+	if err := encoding.WriteString(buf, pubkey); err != nil {
+		return "", err
+	}
+
+	req := NewRequest(*buf, -1)
+	txIdBuf, err := rb.Call(SendStandardTx, req)
+	if err != nil {
+		return "", err
+	}
+
+	return txIdBuf.String(), nil
+}
+
+func (rb *RPCBus) GetBalance() (uint64, error) {
+	buf := new(bytes.Buffer)
+	req := NewRequest(*buf, -1)
+	balanceBuf, err := rb.Call(GetBalance, req)
+	if err != nil {
+		return 0, err
+	}
+
+	var balance uint64
+	if err := encoding.ReadUint64LE(&balanceBuf, &balance); err != nil {
+		return 0, err
+	}
+
+	return balance, nil
+}
