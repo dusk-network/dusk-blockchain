46babd9ef3554534bbff8b36e056b8ac26821d4a
emanuele francioni <emanuele@dusk.network>
2019-09-28 20:47:24 +0200

    unify the eventbus Subscribe and Publish operations, rename dispatcher into listener, separate the preprocessing from subscription, add TopicCollector for callbacks

---
+++ pkg/util/nativeutils/eventbus/listenermap.go
@@ -0,0 +1,64 @@
+package eventbus
+
+import (
+	"math/rand"
+	"sync"
+)
+
+type idListener struct {
+	id uint32
+	Listener
+}
+
+type listenerMap struct {
+	lock      sync.RWMutex
+	listeners map[string][]idListener
+}
+
+func newListenerMap() *listenerMap {
+	return &listenerMap{
+		listeners: make(map[string][]idListener),
+	}
+}
+
+// Store a Listener into an ordered slice stored at a key
+func (h *listenerMap) Store(key string, value Listener) uint32 {
+	id := rand.Uint32()
+	h.lock.Lock()
+	h.listeners[key] = append(h.listeners[key], idListener{id, value})
+	h.lock.Unlock()
+	return id
+}
+
+// Load a copy of the listeners stored for a given key
+func (h *listenerMap) Load(key string) []Listener {
+	h.lock.RLock()
+	listeners, _ := h.listeners[key]
+	h.lock.RUnlock()
+
+	hs := make([]Listener, len(listeners))
+	for i, h := range listeners {
+		hs[i] = h.Listener
+	}
+	return hs
+}
+
+// Delete a listener using the uint32 key returned during the Store operation. Return wether the item was found or otherwise
+func (h *listenerMap) Delete(key string, id uint32) bool {
+	found := false
+	h.lock.Lock()
+	listeners := h.listeners[key]
+	for i, listener := range listeners {
+		if listener.id == id {
+			listener.Close()
+			h.listeners[key] = append(
+				h.listeners[key][:i],
+				h.listeners[key][i+1:]...,
+			)
+			found = true
+			break
+		}
+	}
+	h.lock.Unlock()
+	return found
+}
