810367f60acbd5e26bf17e4adceff32a02e69db9
Jules de Smit <julesdesmit@gmail.com>
2019-10-28 18:36:16 +0100

    remove test harness helper script

--- harness.sh
+++
@@ -1,8 +0,0 @@
-export DUSK_BLOCKCHAIN=/home/jules/go/src/github.com/dusk-network/dusk-blockchain/launch/testnet/testnet
-export DUSK_BLINDBID=/home/jules/go/src/github.com/dusk-network/dusk-blockchain/launch/testnet/blindbid-avx2
-export DUSK_SEEDER=/home/jules/go/src/gitlab.dusk.network/dusk-core/dusk-voucher-seeder/dusk-voucher-seeder
-export DUSK_WALLET_PASS=password
-
-cd launch/testnet
-go build 
-cd ../..
