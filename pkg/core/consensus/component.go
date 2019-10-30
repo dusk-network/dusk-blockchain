55dc34a8a7c2ab19e2ca6b3a94a4c47cc1b3f601
Jules de Smit <julesdesmit@gmail.com>
2019-10-30 17:07:50 +0100

    include a priority on each Listener, so that we can manage the order in which events are dispatched

--- pkg/core/consensus/component.go
+++ pkg/core/consensus/component.go
@@ -2,12 +2,19 @@ package consensus
 
 import (
 	"bytes"
-	"errors"
+	"fmt"
 	"math/rand"
 
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/header"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
+)
+
+type Priority uint8
+
+const (
+	HighPriority Priority = iota
+	LowPriority
 )
 
 // Signer encapsulate the credentials to sign or authenticate outgoing events
@@ -44,18 +51,20 @@ type Listener interface {
 	// ID is used to later unsubscribe from the Coordinator. This is useful for components active throughout
 	// multiple steps
 	ID() uint32
+	Priority() Priority
 }
 
 // SimpleListener implements Listener and uses a callback for notifying events
 type SimpleListener struct {
 	callback func(Event) error
 	id       uint32
+	priority Priority
 }
 
 // NewSimpleListener creates a SimpleListener
-func NewSimpleListener(callback func(Event) error) Listener {
+func NewSimpleListener(callback func(Event) error, priority Priority) Listener {
 	id := rand.Uint32()
-	return &SimpleListener{callback, id}
+	return &SimpleListener{callback, id, priority}
 }
 
 // NotifyPayload triggers the callback specified during instantiation
@@ -68,6 +77,10 @@ func (s *SimpleListener) ID() uint32 {
 	return s.id
 }
 
+func (s *SimpleListener) Priority() Priority {
+	return s.priority
+}
+
 // FilteringListener is a Listener that performs filtering before triggering the callback specified by the component
 // Normally it is used to filter out events sent by Provisioners not being part of a committee or invalid messages.
 // Filtering is applied to the `header.Header`
@@ -77,15 +90,15 @@ type FilteringListener struct {
 }
 
 // NewFilteringListener creates a FilteringListener
-func NewFilteringListener(callback func(Event) error, filter func(header.Header) bool) Listener {
+func NewFilteringListener(callback func(Event) error, filter func(header.Header) bool, priority Priority) Listener {
 	id := rand.Uint32()
-	return &FilteringListener{&SimpleListener{callback, id}, filter}
+	return &FilteringListener{&SimpleListener{callback, id, priority}, filter}
 }
 
 // NotifyPayload uses the filtering function to let only relevant events through
 func (cb *FilteringListener) NotifyPayload(ev Event) error {
 	if cb.filter(ev.Header) {
-		return errors.New("event has been filtered and won't be forwarded to the component")
+		return fmt.Errorf("event has been filtered and won't be forwarded to the component - round: %d / step: %d", ev.Header.Round, ev.Header.Step)
 	}
 	return cb.SimpleListener.NotifyPayload(ev)
 }
@@ -95,12 +108,5 @@ type TopicListener struct {
 	Listener
 	Preprocessors []eventbus.Preprocessor
 	Topic         topics.Topic
-}
-
-func (t *TopicListener) Pause() {
-
-}
-
-func (t *TopicListener) Resume() {
-
+	Paused        bool
 }
