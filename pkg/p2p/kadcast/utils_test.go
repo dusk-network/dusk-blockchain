d938a79a4d578d8cc55dfcc671adc2295c73d604
CPerezz <carlos@dusk.network>
2019-11-08 18:46:35 +0000

    Refactor tests according to the `uint16` impl.

--- pkg/p2p/kadcast/utils_test.go
+++ pkg/p2p/kadcast/utils_test.go
@@ -8,14 +8,16 @@ import (
 
 func TestXorDistance(t *testing.T) {
 	a := [16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	//b := [16]byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}
-	//c := [16]byte{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+	b := [16]byte{255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255, 255}
+	c := [16]byte{1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
 	d := [16]byte{255, 255, 255, 255, 255, 255, 255, 255, 0, 0, 0, 0, 0, 0, 0, 0}
-	//var max uint8 = 127
-	var half uint8 = 63
-	//var zero uint8 = 0
-	//assert.Equal(t, idXor(a, a), zero)
-	//assert.Equal(t, idXor(a, c), max)
-	//assert.Equal(t, idXor(a, b), max)
+	var max uint16 = 128
+	var half uint16 = 64
+	var sixteen uint16 = 16
+	var zero uint16 = 0
+	assert.Equal(t, idXor(a, a), zero)
+	assert.Equal(t, idXor(a, c), sixteen)
+	assert.Equal(t, idXor(a, b), max)
 	assert.Equal(t, idXor(a, d), half)
+	assert.Equal(t, idXor(c, d), half)
 }
