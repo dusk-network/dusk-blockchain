d7eeee18c9f37440343e03bc35f05c91e1beaccf
CPerezz <carlos@dusk.network>
2019-11-08 18:40:35 +0000

    Refactor `idXor` and `collapseDistance`
    - `Peer` distance is expressed now as `uint16` to hold the
    max range value `128` unsoported on the `byte` type.
    - Instead of using math logarithms, we work with bitwise ops.
    
    - `collapseDistance` has been ranamed as `classifyDistance` and
    optimized since now only counts the ammount of `1` of a byte that
    comes from an XOR op.
    - `idXor` just performs the XOR between the two `Peer-ID` arrays
    and computes the number of different bits on the result, using this
    counter as the result for the distance classification value.
    NOTE that this value has the range (0 - 128] to match the `L-buckets`.

--- pkg/p2p/kadcast/utils.go
+++ pkg/p2p/kadcast/utils.go
@@ -1,52 +1,37 @@
 package kadcast
 
-import (
-	"math/big"
-)
-
 // Computes the XOR distance between 2 different
 //ids.
-func idXor(a [16]byte, b [16]byte) uint8 {
+func idXor(a [16]byte, b [16]byte) uint16 {
 	distance := [16]byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	i := 0
 
 	for i < 16 {
-		distance[i] += a[i] ^ b[i]
+		distance[i] = a[i] ^ b[i]
 		i++
 	}
-	return collapseDistance(distance)
+	return classifyDistance(distance)
 }
 
 // This function gets the XOR distance as a byte-array
 // and collapses it to classify the distance on one of the
 // 128 buckets.
-func collapseDistance(arr [16]byte) uint8 {
-	two := big.NewInt(2)
-	base := big.NewInt(10)
-	pow := big.NewInt(0)
-	collDist := new(big.Int)
-	collDist.SetBytes(arr[:])
-	println(collDist)
-	var i uint8 = 127
-	for i > 0 {
-		pow.Exp(two, big.NewInt(int64(i)), base)
-		println(pow)
-		switch collDist.Cmp(pow) {
-		// When equals, it goes to this length. (Bucket Lenght ID)
-		case 0:
-			return i
-		// When its greater, it goes to the length of this bucket since
-		// we know that it's lower than the previous one
-		case 1:
-			return i
-		default:
-			if i == 0 {
-				return 0
-			}
-		}
-		i--
+func classifyDistance(arr [16]byte) uint16 {
+	var collDist uint16 = 0
+	for i := 0; i < 16; i++ {
+		collDist += countSetBits(arr[i])
 	}
-	return 0
+	return collDist
+}
+
+// Counts the number of setted bits in the given byte.
+func countSetBits(byt byte) uint16 {
+	var count uint16 = 0
+	for byt != 0 {
+		count += uint16(byt & 1)
+		byt >>= 1
+	}
+	return count
 }
 
 // Performs the hash of the wallet Sk
