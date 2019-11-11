6179deaea4147ad83b9fc093f7c8aa8f2f0876be
CPerezz <carlos@dusk.network>
2019-11-11 14:28:28 +0000

    Implement makeTree and addPeer for `Tree`
    - makeTree allocates space for a new routing tree
    and sets it's own Peer info on the lowest order
    bucket.
    - addPeer computes the distance between it's own
    peer info and the other peer to be included.
    Then it adds the peer to its corresponding bucket
    
    If the peer it's the same as our peer, the function
    simply doesn't do anything.

--- pkg/p2p/kadcast/tree.go
+++ pkg/p2p/kadcast/tree.go
@@ -2,14 +2,40 @@ package kadcast
 
 // Tree stores `L` buckets inside of it.
 // This is basically the routing info of every peer.
-type Tree struct{ buckets [128]Bucket }
+type Tree struct {
+	buckets [128]Bucket
+}
 
 // Allocates space for a tree and returns an empty intance of it.
-func makeTree() Tree {
-	panic("unimplemented")
+//
+// It also sets our `Peer` info.
+func makeTree(myPeer Peer) Tree {
+	var bucketList [128]Bucket
+	for i := 0; i < 128; i++ {
+		bucketList[i] = makeBucket(uint8(i))
+	}
+	// Add my `Peer` info on the lowest `Bucket`.
+	bucketList[0].addPeerToBucket(myPeer)
+
+	return Tree{
+		buckets: bucketList,
+	}
 }
 
 // Classifies and adds a Peer to the routing storage tree.
-func (tree *Tree) addPeer(peer *Peer) {
-	panic("unimplemeneted")
+func (tree *Tree) addPeer(peer Peer) {
+	idl := tree.buckets[0].entries[0].computePeerDistance(peer)
+	if idl == 0 {
+		return
+	}
+	tree.buckets[idl].addPeerToBucket(peer)
+}
+
+// Returns the total ammount of peers that a `Peer` is connected to.
+func (tree Tree) getTotalPeers() uint64 {
+	var count uint64 = 0
+	for i := 1; i < 128; i++ {
+		count += uint64(tree.buckets[i].peerCount)
+	}
+	return count
 }
