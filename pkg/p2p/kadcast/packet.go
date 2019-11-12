c79568a4a8a33e278a3d38cc43bc4ffbc4f1e978
CPerezz <carlos@dusk.network>
2019-11-12 19:29:45 +0100

    Create packet.go file and add `processPacket`
    This function should be responsible of getting
    a received UDP packet, parse it and also
    call the needed processes according to the packet.

---
+++ pkg/p2p/kadcast/packet.go
@@ -0,0 +1,10 @@
+package kadcast
+
+import (
+	"net"
+)
+
+// The function recieves a Packet and 
+func processPacket(netw net.Addr, byteNum int, payload []byte)  {
+	panic("Not implemented yet")
+}
