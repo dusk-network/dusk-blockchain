68b7f0dcd32bdb9325343f122ebf9d3044db2952
CPerezz <carlos@dusk.network>
2019-11-15 03:00:10 +0100

    Implement tests for PoW, byteConv and Ping-Pong
    Most of the tests are actually used just with
    debugging and networking testing.

--- pkg/p2p/kadcast/utils_test.go
+++ pkg/p2p/kadcast/utils_test.go
@@ -2,16 +2,35 @@ package kadcast
 
 import "testing"
 
+import "time"
+
 func TestStuff(t *testing.T) {
 	a := [4]byte{255,255,255,255}
 	println(*getUintFromBytes(&a))
 }
-func TestPOW(t *testing.T) {
+func testPOW(t *testing.T) {
 	a := Peer{
 		ip: [4]byte{192, 169, 1, 1},
 		port: 25519,
 		id: [16]byte{22,22,22,22,22,22,22,22,22,22,22,22,22,22,22,22},
 	}
 
-	println(getMyNonce(&a))
+	println(a.computePeerNonce())
+}
+
+func TestUDP(t *testing.T) {
+	var port uint16 = 25519
+	ip := [4]byte{62, 57, 153, 222}
+	router := makeRouter(ip, port)
+	//lAddr := getLocalIPAddress()
+
+	go startUDPListener("udp")
+
+	println("HIIIII")
+
+	destPeer := makePeer(ip, port)
+
+	// Send PING packet
+	router.sendPing(destPeer)
+	time.Sleep(100000000)
 }
