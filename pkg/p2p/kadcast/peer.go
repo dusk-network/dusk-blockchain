e5c274fb5283c4a9d86d41ea53929b3ab3f840e9
CPerezz <carlos@dusk.network>
2019-11-15 02:25:35 +0100

    Refactor peer module to work by-value
    Refactored function arguments to be used by value
    and not by reference.
    Moved UDPAddr<->PeerInfo transformations.

--- pkg/p2p/kadcast/peer.go
+++ pkg/p2p/kadcast/peer.go
@@ -1,6 +1,10 @@
 package kadcast
 
-import "golang.org/x/crypto/sha3"
+import (
+	"net"
+
+	"golang.org/x/crypto/sha3"
+)
 
 // Peer stores the info of a peer which consists on:
 // - IP of the peer.
@@ -13,9 +17,9 @@ type Peer struct {
 }
 
 // Constructs a Peer with it's fields values as inputs.
-func makePeer(ip *[4]byte, port *uint16) Peer {
-	id := computePeerID(*ip)
-	peer := Peer{*ip, *port, id}
+func makePeer(ip [4]byte, port uint16) Peer {
+	id := computePeerID(ip)
+	peer := Peer{ip, port, id}
 	return peer
 }
 
@@ -24,10 +28,10 @@ func makePeer(ip *[4]byte, port *uint16) Peer {
 //
 // This operation is basically a PoW algorithm that ensures
 // that Sybil attacks are more costly.
-func getMyNonce(myPeer *Peer) uint32 {
+func (peer Peer) computePeerNonce() uint32 {
 	var nonce uint32 = 0
 	var hash [32]byte
-	id := myPeer.id
+	id := peer.id
 	for {
 		hash = sha3.Sum256(append(id[:], getBytesFromUint(nonce)[:]...))
 		if (hash[31] | hash[30] | hash[29]) == 0 {
@@ -38,21 +42,38 @@ func getMyNonce(myPeer *Peer) uint32 {
 }
 
 // Sets the Id sent as parameter as the Peer ID.
-func (peer *Peer) addIP(ip *[4]byte) {
-	peer.ip = *ip
+func (peer *Peer) addIP(ip [4]byte) {
+	peer.ip = ip
 }
 
 // Sets the Id sent as parameter as the Peer ID.
-func (peer *Peer) addID(id *[16]byte) {
-	peer.id = *id
+func (peer *Peer) addID(id [16]byte) {
+	peer.id = id
 }
 
 // Sets the port sent as parameter as the Peer port.
-func (peer *Peer) addPort(port *uint16) {
-	peer.port = *port
+func (peer *Peer) addPort(port uint16) {
+	peer.port = port
 }
 
 // Computes the XOR distance between two Peers.
-func (me *Peer) computePeerDistance(peer *Peer) uint16 {
+func (me Peer) computePeerDistance(peer Peer) uint16 {
 	return idXor(&me.id, &peer.id)
+}
+
+// Reads the network info of a `Peer` and returns its
+// corresponding `UDPAddr` struct.
+func (peer Peer) getUDPAddr() net.UDPAddr {
+	return net.UDPAddr{
+		IP:   peer.ip[:],
+		Port: int(peer.port),
+		Zone: "N/A",
+	}
+}
+
+// Builds the Peer info from a UPDAddress struct.
+func getPeerNetworkInfo(udpAddress *net.UDPAddr) ([4]byte, uint8) {
+	var ip [4]byte
+	copy(ip[:], udpAddress.IP[:])
+	return ip, uint8(udpAddress.Port)
 }
