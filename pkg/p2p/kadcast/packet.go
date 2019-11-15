ad0d5e5ec8c30aca857f07216a9e66d57e7d3e87
CPerezz <carlos@dusk.network>
2019-11-15 02:13:15 +0100

    Implement Packet type and processPacket
    processPacket now it's just a debugging tool to catch
    the packets recieved and print the content.

--- pkg/p2p/kadcast/packet.go
+++ pkg/p2p/kadcast/packet.go
@@ -1,53 +1,16 @@
 package kadcast
 
-import (
-	"net"
-)
-// Router holds all of the data needed to interact with
-// the routing data and also the networking utils.
-type Router struct {
-	// Tree represents the routing structure.
-	tree Tree
-	// Even the port and the IP are the same info, the difference
-	// is that one IP has type `IP` and the other `[4]byte`.
-	// Since we only store one tree on the application, it's worth
-	// to keep both in order to avoid convert the types continuously.
-	myPeerUDPAddr net.UDPAddr
-	myPeerInfo Peer
-	// Holds the Nonce that satisfies: `H(ID || Nonce) < Tdiff`.
-	myPeerNonce uint32
+import "net"
+
+import "fmt"
+
+type Packet struct {
+	headers [22]byte
 }
 
-
-
-// Builds and sends a `PING` packet 
-func (router *Router) sendPing(reciever *Peer)  {
-	// `PING` Type = 0
-	packType := [1]byte{0}
-	// Build `PING` payload.
-	// Attach sender ID
-	payload := append(packType[:], router.myPeerInfo.id[:]...)
-	// Attach IdNonce
-	payload = append(payload[:], getBytesFromUint(router.myPeerNonce)[:]...)
-	// Send the packet
-	sendUDPPacket("DuskNetwork", &router.myPeerUDPAddr, reciever.getUDPAddr(), payload)
-}
-
-// Builds and sends a `PONG` packet
-func (router *Router) sendPong(reciever *Peer) {
-	// `PONG` Type = 1
-	packType := [1]byte{1}
-	// Build `PING` payload.
-	// Attach sender ID
-	payload := append(packType[:], router.myPeerInfo.id[:]...)
-	// Attach IdNonce
-	payload = append(payload[:], getBytesFromUint(router.myPeerNonce)[:]...)
-	// Send the packet
-	sendUDPPacket("DuskNetwork", &router.myPeerUDPAddr, reciever.getUDPAddr(), payload)
-}
-
-
-// The function recieves a Packet and 
-func processPacket(netw net.Addr, byteNum int, payload []byte)  {
-	panic("Not implemented yet")
+// The function recieves a Packet and
+func processPacket(netw net.Addr, byteNum int, payload []byte) {
+	fmt.Println(netw.Network())
+	fmt.Println(byteNum)
+	fmt.Println(payload[:])
 }
