e3fdb655b277fe0d9a2ba24ba559f8b951f7c7c3
CPerezz <carlos@dusk.network>
2019-11-18 10:38:31 +0100

    Implement basic Packet processing functions
    Implements basic functions to work with
    packets as:
    - getHeadersInfo => Returns the headers sliced
    in the 3 different elements.
    - processPacket => gets a Packet and processes
    it according to it's type.
    - getPacketFromStream => Gets a stream of bytes
    and returns a Packet struct from it.

--- pkg/p2p/kadcast/packet.go
+++ pkg/p2p/kadcast/packet.go
@@ -7,20 +7,68 @@ import (
 
 type Packet struct {
 	headers [22]byte
+	payload []byte
 }
 
-// The function recieves a Packet and
-func processPacket(sender net.UDPAddr, byteNum int, payload []byte, router *Router) {
-	log.Printf("Revieved new packet from: %s", sender.IP)
-	destIP, port := getPeerNetworkInfo(sender)
-	var id [16]byte
-	copy(id[:], payload[1:17])
-	dest := Peer {
-		ip: destIP,
-		port: port,
-		id: id,
+// Gets a stream of bytes and slices it between headers of Kadcast
+// protocol and the payload.
+func getPacketFromStream(stream []byte) Packet {
+	var headers [22]byte
+	copy(headers[:], stream[0:21])
+	return Packet{
+		headers: headers,
+		payload: stream[22:],
 	}
-	if payload[0] == 0 {
-		router.sendPong(dest)
+}
+
+// Returns the headers info sliced into three pieces:
+// Packet type, SenderId and IdNonce.
+func (pac Packet) getHeadersInfo() (byte, [16]byte, [4]byte) {
+	// Construct type, senderID and Nonce
+	typ := pac.headers[0]
+	var senderID [16]byte
+	copy(senderID[:], pac.headers[1:17])
+	var nonce [4]byte
+	copy(nonce[:], pac.headers[18:21])
+	return typ, senderID, nonce
+}
+
+// The function recieves a Packet and processes it according to 
+// it's type.
+func processPacket(senderAddr net.UDPAddr, byteNum int, payload []byte, router *Router) {
+	log.Printf("Revieved new packet from: %s", senderAddr.IP)
+	// Build packet struct
+	packet := getPacketFromStream(payload[:])
+	// Extract headers info.
+	tipus, senderID, nonce := packet.getHeadersInfo()
+
+	// Verify IDNonce
+	peerInf, err := verifyIDNonce(senderID, senderAddr, nonce)
+	// If we get an error, we just skip the whole process since the
+	// Peer was not validated.
+	if err != nil {
+		log.Printf("%s", err)
+		return
 	}
+	// Check packet type and process it.	
+	switch tipus {
+	case 0:
+		treatPing(*peerInf, router)
+		log.Printf("Recieved PING message from %v", peerInf.ip[:])
+	case 1:
+		treatPong(*peerInf, router)
+		log.Printf("Recieved PONG message from %v", peerInf.ip[:])
+	}
+}
+
+func treatPing(peerInf Peer, router *Router) {
+	// Process peer addition to the tree.
+	router.tree.addPeer(router.myPeerInfo, peerInf)
+	// Send back a `Pong` message.
+	router.sendPong(peerInf)
+}
+
+func treatPong(peerInf Peer, router *Router) {
+	// Process peer addition to the tree.
+	router.tree.addPeer(router.myPeerInfo, peerInf)
 }
