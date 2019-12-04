1bcd6195303365880fa830f39f1b4907c3ea4398
Jules de Smit <julesdesmit@gmail.com>
2019-12-04 16:10:11 +0100

    create an in-memory store for candidate messages; hold the intermediate block and latest certificate on the Chain, information about which is provided by the Agreement component

--- pkg/core/chain/collectors.go
+++ pkg/core/chain/collectors.go
@@ -3,7 +3,6 @@ package chain
 import (
 	"bytes"
 
-	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/header"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/marshalling"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/encoding"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
@@ -12,10 +11,6 @@ import (
 )
 
 type (
-	candidateCollector struct {
-		blockChan chan<- *block.Block
-	}
-
 	certificateCollector struct {
 		certificateChan chan<- certMsg
 	}
@@ -25,28 +20,6 @@ type (
 		cert *block.Certificate
 	}
 )
-
-func initCandidateCollector(eventBus *eventbus.EventBus) chan *block.Block {
-	blockChan := make(chan *block.Block, 1)
-	collector := &candidateCollector{blockChan}
-	eventbus.NewTopicListener(eventBus, collector, topics.Candidate, eventbus.ChannelType)
-	return blockChan
-}
-
-func (c *candidateCollector) Collect(message bytes.Buffer) error {
-	hdr := header.Header{}
-	if err := header.Unmarshal(&message, &hdr); err != nil {
-		return err
-	}
-
-	blk := block.NewBlock()
-	if err := marshalling.UnmarshalBlock(&message, blk); err != nil {
-		return err
-	}
-
-	c.blockChan <- blk
-	return nil
-}
 
 func initCertificateCollector(subscriber eventbus.Subscriber) <-chan certMsg {
 	certificateChan := make(chan certMsg, 10)
