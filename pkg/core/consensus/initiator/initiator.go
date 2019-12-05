b2ec819d33f1f727f5d1da0e5e76d6e419dace5b
Jules de Smit <julesdesmit@gmail.com>
2019-12-05 17:37:06 +0100

    remove obsolete syncing code from the initiator, as the chain now takes care of this

--- pkg/core/consensus/initiator/initiator.go
+++ pkg/core/consensus/initiator/initiator.go
@@ -2,23 +2,17 @@ package initiator
 
 import (
 	"bytes"
-	"encoding/binary"
 	"fmt"
 	"os"
-	"time"
 
 	cfg "github.com/dusk-network/dusk-blockchain/pkg/config"
-	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/factory"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/maintainer"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/database"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/database/heavy"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/peer/processing/chainsync"
-	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/encoding"
-	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/rpcbus"
-	"github.com/dusk-network/dusk-wallet/block"
 	"github.com/dusk-network/dusk-wallet/transactions"
 	"github.com/dusk-network/dusk-wallet/wallet"
 	zkproof "github.com/dusk-network/dusk-zkproof"
@@ -28,7 +22,6 @@ import (
 var l = log.WithField("process", "consensus initiator")
 
 func LaunchConsensus(eventBroker *eventbus.EventBus, rpcBus *rpcbus.RPCBus, w *wallet.Wallet, counter *chainsync.Counter) {
-	// TODO: sync first
 	startBlockGenerator(eventBroker, rpcBus, w)
 	startProvisioner(eventBroker, rpcBus, w, counter)
 	if err := launchMaintainer(eventBroker, rpcBus, w); err != nil {
@@ -41,32 +34,6 @@ func startProvisioner(eventBroker *eventbus.EventBus, rpcBus *rpcbus.RPCBus, w *
 	pubKey := w.PublicKey()
 	f := factory.New(eventBroker, rpcBus, cfg.ConsensusTimeOut, &pubKey, w.ConsensusKeys())
 	f.StartConsensus()
-
-	// Get current height
-	req := rpcbus.NewRequest(bytes.Buffer{})
-	resultBuf, err := rpcBus.Call(rpcbus.GetLastBlock, req, 1*time.Second)
-	if err != nil {
-		l.WithError(err).Warnln("could not retrieve current height, starting from 1")
-		sendInitMessage(eventBroker, 1)
-		return
-	}
-
-	var currentHeight uint64
-	if err := encoding.ReadUint64LE(&resultBuf, &currentHeight); err != nil {
-		l.WithError(err).Warnln("could not decode current height, starting from 1")
-		sendInitMessage(eventBroker, 1)
-		return
-	}
-
-	if currentHeight > 0 {
-		// Get starting round
-		startingRound := getStartingRound(eventBroker, counter)
-		sendInitMessage(eventBroker, startingRound)
-		return
-	}
-
-	// We are at genesis. Start from 1
-	sendInitMessage(eventBroker, 1)
 }
 
 // XXX: clean this up ASAP
@@ -115,47 +82,6 @@ func startBlockGenerator(eventBroker eventbus.Broker, rpcBus *rpcbus.RPCBus, w *
 					panic(err)
 				}
 			}
-		}
-	}
-}
-
-func getStartingRound(eventBroker eventbus.Broker, counter *chainsync.Counter) uint64 {
-	// Start listening for accepted blocks, regardless of if we found stakes or not
-	acceptedBlockChan, listener := consensus.InitAcceptedBlockUpdate(eventBroker)
-	// Unsubscribe from AcceptedBlock once we're done
-	defer listener.Quit()
-
-	// Sync first
-	syncToTip(acceptedBlockChan, counter)
-
-	for {
-		blk := <-acceptedBlockChan
-		return blk.Header.Height + 1
-	}
-}
-
-func sendInitMessage(publisher eventbus.Publisher, startingRound uint64) {
-
-	l.Infof("Initialize consensus from starting round %d", startingRound)
-
-	roundBytes := make([]byte, 8)
-	binary.LittleEndian.PutUint64(roundBytes, startingRound)
-	publisher.Publish(topics.Initialization, bytes.NewBuffer(roundBytes))
-
-}
-
-func syncToTip(acceptedBlockChan <-chan block.Block, counter *chainsync.Counter) {
-	i := 0
-	for {
-		<-acceptedBlockChan
-		if counter.IsSyncing() {
-			i = 0
-			continue
-		}
-
-		i++
-		if i > 2 {
-			break
 		}
 	}
 }
