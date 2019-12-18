3df67be3675fdddab93f1af2817d056a1d8e3ecb
Jules de Smit <julesdesmit@gmail.com>
2019-12-18 12:03:51 +0100

    check both the hash and the merkle root of the block

--- pkg/core/candidate/validator.go
+++ pkg/core/candidate/validator.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
+	"github.com/dusk-network/dusk-wallet/block"
 )
 
 // The candidate validator performs a simple integrity check with an
@@ -19,7 +20,7 @@ func newValidator(pub eventbus.Publisher) *validator {
 }
 
 func (v *validator) Process(b *bytes.Buffer) error {
-	if err := checkHash(*b); err != nil {
+	if err := checkHashAndRoot(*b); err != nil {
 		return err
 	}
 
@@ -35,21 +36,44 @@ func (v *validator) republish(b bytes.Buffer) error {
 	return nil
 }
 
-// Make sure the hash is correct, to avoid malicious nodes from
+// Make sure the hash and root are correct, to avoid malicious nodes from
 // overwriting the candidate block for a specific hash
-func checkHash(b bytes.Buffer) error {
-	cm := &Candidate{}
+func checkHashAndRoot(b bytes.Buffer) error {
+	cm := &Candidate{block.NewBlock(), block.EmptyCertificate()}
 	if err := Decode(&b, cm); err != nil {
 		return err
 	}
 
-	hash := cm.Block.Header.Hash
-	if err := cm.Block.SetHash(); err != nil {
+	if err := checkHash(cm.Block); err != nil {
 		return err
 	}
 
-	if !bytes.Equal(hash, cm.Block.Header.Hash) {
+	return checkRoot(cm.Block)
+}
+
+func checkHash(blk *block.Block) error {
+	hash := make([]byte, 32)
+	copy(hash, blk.Header.Hash)
+	if err := blk.SetHash(); err != nil {
+		return err
+	}
+
+	if !bytes.Equal(hash, blk.Header.Hash) {
 		return errors.New("invalid block hash")
+	}
+
+	return nil
+}
+
+func checkRoot(blk *block.Block) error {
+	root := make([]byte, 32)
+	copy(root, blk.Header.TxRoot)
+	if err := blk.SetRoot(); err != nil {
+		return err
+	}
+
+	if !bytes.Equal(root, blk.Header.TxRoot) {
+		return errors.New("invalid merkle root hash")
 	}
 
 	return nil
