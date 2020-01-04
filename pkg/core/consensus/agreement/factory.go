5d0bbd632730d96ed005c3225a93b27e0ce39394
emanuele francioni <emanuele@dusk.network>
2020-01-04 14:22:56 +0100

    Agreement factory fires up the republisher as an eventbus callback listener

--- pkg/core/consensus/agreement/factory.go
+++ pkg/core/consensus/agreement/factory.go
@@ -1,8 +1,11 @@
 package agreement
 
 import (
+	"bytes"
+
 	cfg "github.com/dusk-network/dusk-blockchain/pkg/config"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus"
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
 	"github.com/dusk-network/dusk-wallet/key"
 )
@@ -12,15 +15,25 @@ type Factory struct {
 	broker       eventbus.Broker
 	keys         key.ConsensusKeys
 	workerAmount int
+	r            *Republisher
+}
+
+// Republisher handles the repropagation of Agreement messages
+type Republisher struct {
+	broker eventbus.Broker
+	id     uint32
 }
 
 // NewFactory instantiates a Factory.
 func NewFactory(broker eventbus.Broker, keys key.ConsensusKeys) *Factory {
 	amount := cfg.Get().Performance.AccumulatorWorkers
+	r := newRepublisher(broker)
+
 	return &Factory{
 		broker,
 		keys,
 		amount,
+		r,
 	}
 }
 
@@ -28,4 +41,33 @@ func NewFactory(broker eventbus.Broker, keys key.ConsensusKeys) *Factory {
 // Implements consensus.ComponentFactory.
 func (f *Factory) Instantiate() consensus.Component {
 	return newComponent(f.broker, f.keys, f.workerAmount)
+}
+
+// newRepublisher creates a Republisher
+func newRepublisher(eb eventbus.Broker) *Republisher {
+	r := &Republisher{broker: eb}
+	r.id = r.Activate()
+	return r
+}
+
+// Stop a Republisher
+func (r *Republisher) Stop() {
+	r.broker.Unsubscribe(topics.Agreement, r.id)
+}
+
+// Activate the Republisher by listening to Agreement messages through a
+// callback listener
+func (r *Republisher) Activate() uint32 {
+	l := eventbus.NewCallbackListener(r.Republish)
+	return r.broker.Subscribe(topics.Agreement, l)
+}
+
+// Republish intercepts an Agreement message and repropagates it immediately
+func (r *Republisher) Republish(b bytes.Buffer) error {
+	if err := topics.Prepend(&b, topics.Agreement); err != nil {
+		return err
+	}
+
+	r.broker.Publish(topics.Gossip, &b)
+	return nil
 }
---
+++ pkg/core/consensus/agreement/factory_test.go
@@ -0,0 +1,31 @@
+package agreement_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/agreement"
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
+	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
+	"github.com/dusk-network/dusk-wallet/key"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRepublisher(t *testing.T) {
+	eb := eventbus.New()
+	gossipChan := make(chan bytes.Buffer, 1)
+	gl := eventbus.NewChanListener(gossipChan)
+	eb.Subscribe(topics.Gossip, gl)
+	k, _ := key.NewRandConsensusKeys()
+	agreement.NewFactory(eb, k)
+
+	mockAggro := bytes.NewBuffer([]byte{1})
+	eb.Publish(topics.Agreement, mockAggro)
+
+	packet := <-gossipChan
+	tpc, err := topics.Extract(&packet)
+	assert.NoError(t, err)
+	assert.Equal(t, topics.Agreement, tpc)
+	assert.Equal(t, []byte{1}, packet.Bytes())
+
+}
