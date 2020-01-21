06bbfae95ce5875136d181169ee109c8128d214d
Jules de Smit <julesdesmit@gmail.com>
2020-01-21 19:07:54 +0100

    remove the transactor from the maintainer tests, and instead use a goroutine that mocks tx sending

--- pkg/core/consensus/maintainer/maintainer_test.go
+++ pkg/core/consensus/maintainer/maintainer_test.go
@@ -13,25 +13,17 @@ import (
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/user"
 	litedb "github.com/dusk-network/dusk-blockchain/pkg/core/database"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/database/lite"
-	"github.com/dusk-network/dusk-blockchain/pkg/core/marshalling"
-	"github.com/dusk-network/dusk-blockchain/pkg/core/transactor"
-	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/encoding"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/rpcbus"
 	"github.com/dusk-network/dusk-wallet/key"
-	"github.com/dusk-network/dusk-wallet/transactions"
-	"github.com/dusk-network/dusk-wallet/wallet"
-	zkproof "github.com/dusk-network/dusk-zkproof"
 	"github.com/stretchr/testify/assert"
 )
-
-const pass = "password"
 
 // Test that the maintainer will properly send new stake and bid transactions, when
 // one is about to expire, or if none exist.
 func TestMaintainStakesAndBids(t *testing.T) {
-	bus, c, p, keys, m := setupMaintainerTest(t)
+	bus, c, p, keys, m, exitChan := setupMaintainerTest(t)
 	defer os.Remove("wallet.dat")
 	defer os.RemoveAll("walletDB")
 
@@ -39,11 +31,7 @@ func TestMaintainStakesAndBids(t *testing.T) {
 	bus.Publish(topics.RoundUpdate, consensus.MockRoundUpdateBuffer(1, p, nil))
 
 	// receive first txs
-	txs := receiveTxs(t, c)
-	// Check correctness
-	// We get the bid first
-	assert.True(t, txs[0].Type() == transactions.BidType)
-	assert.True(t, txs[1].Type() == transactions.StakeType)
+	receiveTxs(t, c)
 
 	// add ourselves to the provisioners and the bidlist,
 	// so that the maintainer gets the proper ending heights.
@@ -65,15 +53,13 @@ func TestMaintainStakesAndBids(t *testing.T) {
 	bus.Publish(topics.RoundUpdate, consensus.MockRoundUpdateBuffer(950, p, bl))
 
 	// We should get another set of two txs
-	txs = receiveTxs(t, c)
-	// Check correctness
-	assert.True(t, txs[0].Type() == transactions.BidType)
-	assert.True(t, txs[1].Type() == transactions.StakeType)
+	receiveTxs(t, c)
+	exitChan <- struct{}{}
 }
 
 // Ensure the maintainer does not keep sending bids and stakes until they are included.
 func TestSendOnce(t *testing.T) {
-	bus, c, p, _, _ := setupMaintainerTest(t)
+	bus, c, p, _, _, exitChan := setupMaintainerTest(t)
 	defer os.Remove("wallet.dat")
 	defer os.RemoveAll("walletDB")
 
@@ -81,7 +67,7 @@ func TestSendOnce(t *testing.T) {
 	bus.Publish(topics.RoundUpdate, consensus.MockRoundUpdateBuffer(1, p, nil))
 
 	// receive first txs
-	_ = receiveTxs(t, c)
+	receiveTxs(t, c)
 
 	// Update round
 	bus.Publish(topics.RoundUpdate, consensus.MockRoundUpdateBuffer(2, p, nil))
@@ -89,29 +75,20 @@ func TestSendOnce(t *testing.T) {
 	select {
 	case <-c:
 		t.Fatal("was not supposed to get another tx in txChan")
-	case <-time.After(2 * time.Second):
+	case <-time.After(1 * time.Second):
 		// success
 	}
+
+	exitChan <- struct{}{}
 }
 
-func setupMaintainerTest(t *testing.T) (*eventbus.EventBus, chan rpcbus.Request, *user.Provisioners, key.ConsensusKeys, ristretto.Scalar) {
+func setupMaintainerTest(t *testing.T) (*eventbus.EventBus, chan struct{}, *user.Provisioners, key.ConsensusKeys, ristretto.Scalar, chan struct{}) {
 	// Initial setup
 	bus := eventbus.New()
 	rpcBus := rpcbus.New()
 
-	tr, err := transactor.New(bus, rpcBus, nil, nil, wallet.GenerateDecoys, wallet.GenerateInputs, true)
-	if err != nil {
-		panic(err)
-	}
-	go tr.Listen()
-
-	os.Remove(cfg.Get().Wallet.File)
-	assert.NoError(t, createWallet(rpcBus, pass))
-
-	time.Sleep(100 * time.Millisecond)
-
-	w, err := tr.Wallet()
-	assert.NoError(t, err)
+	c := make(chan struct{}, 2)
+	exitChan := startFakeTransactor(rpcBus, c)
 
 	_, db := lite.CreateDBConnection()
 	// Ensure we have a genesis block
@@ -120,41 +97,46 @@ func setupMaintainerTest(t *testing.T) (*eventbus.EventBus, chan rpcbus.Request,
 		return t.StoreBlock(genesisBlock)
 	}))
 
-	k, err := w.ReconstructK()
-	assert.NoError(t, err)
-	mScalar := zkproof.CalculateM(k)
-	m := maintainer.New(bus, rpcBus, w.ConsensusKeys().BLSPubKeyBytes, mScalar)
+	var mScalar ristretto.Scalar
+	mScalar.Rand()
+	keys, err := key.NewRandConsensusKeys()
+	if err != nil {
+		panic(err)
+	}
+
+	m := maintainer.New(bus, rpcBus, keys.BLSPubKeyBytes, mScalar)
 	go m.Listen()
 
 	// Mock provisioners, and insert our wallet values
 	p, _ := consensus.MockProvisioners(10)
 	// Note: we don't need to mock the bidlist as we should not be included if we want to trigger a bid transaction
 
-	c := make(chan rpcbus.Request, 1)
-	rpcBus.Register(rpcbus.SendMempoolTx, c)
-
-	return bus, c, p, w.ConsensusKeys(), mScalar
+	return bus, c, p, keys, mScalar, exitChan
 }
 
-func receiveTxs(t *testing.T, c chan rpcbus.Request) []transactions.Transaction {
-	var txs []transactions.Transaction
+func startFakeTransactor(rb *rpcbus.RPCBus, txChan chan struct{}) chan struct{} {
+	exitChan := make(chan struct{}, 1)
+	c := make(chan rpcbus.Request, 10)
+	rb.Register(rpcbus.SendBidTx, c)
+	rb.Register(rpcbus.SendStakeTx, c)
+
+	go func(c chan rpcbus.Request, exitChan chan struct{}) {
+		for {
+			select {
+			case r := <-c:
+				txChan <- struct{}{}
+				r.RespChan <- rpcbus.Response{bytes.Buffer{}, nil}
+			case <-exitChan:
+				return
+			}
+		}
+	}(c, exitChan)
+
+	return exitChan
+}
+
+func receiveTxs(t *testing.T, c chan struct{}) {
 	for i := 0; i < 2; i++ {
-		r := <-c
-		tx, err := marshalling.UnmarshalTx(&r.Params)
-		assert.NoError(t, err)
-		txs = append(txs, tx)
-		r.RespChan <- rpcbus.Response{bytes.Buffer{}, nil}
+		<-c
 	}
-
-	return txs
-}
-
-func createWallet(rpcBus *rpcbus.RPCBus, password string) error {
-	buf := new(bytes.Buffer)
-	if err := encoding.WriteString(buf, password); err != nil {
-		return err
-	}
-
-	_, err := rpcBus.Call(rpcbus.CreateWallet, rpcbus.NewRequest(*buf), 0)
-	return err
 }
