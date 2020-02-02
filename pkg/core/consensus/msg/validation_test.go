9b71b7d51df2b934f0e427c97bc2b87cb9934731
emanuele francioni <emanuele@dusk.network>
2020-02-02 13:22:28 +0100

    add test to ensure no BLS signature mutation happens during verification

---
+++ pkg/core/consensus/msg/validation_test.go
@@ -0,0 +1,25 @@
+package msg
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/dusk-network/dusk-crypto/bls"
+	"github.com/dusk-network/dusk-wallet/key"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestVerifyBLSSignature(t *testing.T) {
+	k, _ := key.NewRandConsensusKeys()
+	payload := bytes.NewBufferString("this is a test message").Bytes()
+
+	sig, _ := bls.Sign(k.BLSSecretKey, k.BLSPubKey, payload)
+	s := sig.Compress()
+	orig := make([]byte, 33)
+	copy(orig, s)
+	if !assert.NoError(t, VerifyBLSSignature(k.BLSPubKeyBytes, payload, s)) {
+		t.FailNow()
+	}
+
+	assert.Equal(t, s, orig)
+}
