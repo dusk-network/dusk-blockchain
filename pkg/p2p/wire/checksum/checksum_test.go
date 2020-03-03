9d7bb22a09c070fff0e774296eb22334bb032368
Goranov <georgi@dusk.network>
2020-03-03 16:18:33 +0200

    Add checksum basic tests

--- pkg/p2p/wire/checksum/checksum_test.go
+++ pkg/p2p/wire/checksum/checksum_test.go
@@ -3,7 +3,9 @@ package checksum_test
 import (
 	"testing"
 
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/checksum"
 	crypto "github.com/dusk-network/dusk-crypto/hash"
+	"github.com/stretchr/testify/assert"
 	"golang.org/x/crypto/blake2b"
 	"golang.org/x/crypto/sha3"
 )
@@ -24,5 +26,18 @@ func BenchmarkBlake2b(b *testing.B) {
 	message, _ := crypto.RandEntropy(200)
 	for i := 0; i < b.N; i++ {
 		_ = blake2b.Sum256(message)
+	}
+}
+
+func TestVerify(t *testing.T) {
+	message, _ := crypto.RandEntropy(32)
+	cs := checksum.Generate(message)
+	assert.True(t, checksum.Verify(message, cs))
+}
+
+func TestInvalidLen(t *testing.T) {
+	_, _, err := checksum.Extract([]byte{0, 0, 0})
+	if err == nil {
+		t.Errorf("invalid len err expected")
 	}
 }
