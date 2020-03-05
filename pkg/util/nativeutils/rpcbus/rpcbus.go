7176574af578db8ab16b1b6e53cac8a1e0d28472
Goranov <georgi@dusk.network>
2020-03-05 20:52:39 +0200

    RPCBus does not close request channels

--- pkg/util/nativeutils/rpcbus/rpcbus.go
+++ pkg/util/nativeutils/rpcbus/rpcbus.go
@@ -124,13 +124,12 @@ func (bus *RPCBus) getReqChan(t topics.Topic) (chan<- Request, error) {
 	return nil, ErrMethodNotExists
 }
 
-// Close all open channels
 func (bus *RPCBus) Close() {
 	bus.mu.Lock()
-	defer bus.mu.Unlock()
-	for _, m := range bus.registry {
-		close(m)
-	}
 
+	// Channels should be closed only by the goroutines/components
+	// that make them
 	bus.registry = nil
+
+	bus.mu.Unlock()
 }
