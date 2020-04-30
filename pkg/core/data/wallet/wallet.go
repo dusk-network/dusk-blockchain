d66886ad00402939d298e60949b4c8c81d865c0d
Jules de Smit <julesdesmit@gmail.com>
2020-04-30 12:39:37 +0200

    Remove functionality for syncing and transaction creation
    This will be delegated to Rusk instead.

--- pkg/core/data/wallet/wallet.go
+++ pkg/core/data/wallet/wallet.go
@@ -4,33 +4,25 @@ import (
 	"crypto/rand"
 	"errors"
 	"fmt"
+	"io"
+	"reflect"
+
 	consensuskey "github.com/dusk-network/dusk-blockchain/pkg/core/consensus/key"
-	"github.com/dusk-network/dusk-blockchain/pkg/core/data/block"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/data/database"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/data/key"
-	"github.com/dusk-network/dusk-blockchain/pkg/core/data/transactions"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/data/txrecords"
 	"github.com/dusk-network/dusk-protobuf/autogen/go/rusk"
 	"github.com/golang/protobuf/proto"
 	log "github.com/sirupsen/logrus"
-	"io"
-	"reflect"
 
 	"github.com/syndtr/goleveldb/leveldb"
 )
-
-// Number of mixins per ring. ringsize = mixin + 1
-const numMixins = 7
 
 // DUSK is one whole unit of DUSK.
 const DUSK = uint64(100000000)
 
 // ErrSeedFileExists is returned if the seed file already exists
 var ErrSeedFileExists = fmt.Errorf("wallet seed file already exists")
-
-// FetchInputs returns a slice of inputs such that Sum(Inputs)- Sum(Outputs) >= 0
-// If > 0, then a change address is created for the remaining amount
-type FetchInputs func(netPrefix byte, db *database.DB, totalAmount int64, key *key.Key) ([]*transactions.Input, int64, error)
 
 // Wallet encapsulates the wallet
 type Wallet struct {
@@ -41,13 +33,6 @@ type Wallet struct {
 	consensusKeys *consensuskey.Keys
 
 	secretKey *rusk.SecretKey
-}
-
-// SignableTx is a signable transaction
-type SignableTx interface {
-	AddDecoys(numMixins int, f transactions.FetchDecoys) error
-	Prove() error
-	StandardTx() *transactions.Standard
 }
 
 // New creates a wallet instance
@@ -207,102 +192,10 @@ func LoadFromFile(netPrefix byte, db *database.DB, password string, seedFile, se
 	}, nil
 }
 
-// CheckWireBlock check a block
-func (w *Wallet) CheckWireBlock(blk block.Block) (uint64, uint64, error) {
-	// Ensure this block is at the height we expect it to be
-	walletHeight, err := w.GetSavedHeight()
-	if err != nil {
-		return 0, 0, err
-	}
-
-	if blk.Header.Height != walletHeight {
-		return 0, 0, errors.New("last seen block does not precede provided block")
-	}
-
-	spentCount, err := w.CheckWireBlockSpent(blk)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	receivedCount, err := w.CheckWireBlockReceived(blk)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	err = w.UpdateWalletHeight(blk.Header.Height + 1)
-	if err != nil {
-		return 0, 0, err
-	}
-
-	privSpend, err := w.keyPair.PrivateSpend()
-	if err != nil {
-		return 0, 0, err
-	}
-
-	if err := w.db.UpdateLockedInputs(privSpend.Bytes(), blk.Header.Height); err != nil {
-		return 0, 0, err
-	}
-
-	return spentCount, receivedCount, nil
-}
-
-// CheckUnconfirmedBalance calculates balance including the unconfirmed
-// transactions from a slice of transactions
-func (w *Wallet) CheckUnconfirmedBalance(txs []transactions.Transaction) (uint64, error) {
-	privView, err := w.keyPair.PrivateView()
-	if err != nil {
-		return 0, err
-	}
-
-	var balance uint64
-	for _, tx := range txs {
-		for i, output := range tx.StandardTx().Outputs {
-			if _, ok := w.keyPair.DidReceiveTx(tx.StandardTx().R, output.PubKey, uint32(i)); !ok {
-				continue
-			}
-
-			var amount uint64
-			if transactions.ShouldEncryptValues(tx) {
-				amountScalar := transactions.DecryptAmount(output.EncryptedAmount, tx.StandardTx().R, uint32(i), *privView)
-				amount = amountScalar.BigInt().Uint64()
-			} else {
-				amount = output.EncryptedAmount.BigInt().Uint64()
-			}
-
-			balance += amount
-		}
-	}
-
-	return balance, nil
-}
-
-// Balance calculates and returns the wallet balance for confirmed transactions
-func (w *Wallet) Balance() (uint64, uint64, error) {
-	privSpend, err := w.keyPair.PrivateSpend()
-	if err != nil {
-		return 0, 0, err
-	}
-	unlockedBalance, lockedBalance, err := w.db.FetchBalance(privSpend.Bytes())
-	if err != nil {
-		return 0, 0, err
-	}
-	return unlockedBalance, lockedBalance, nil
-}
-
 // FetchTxHistory will return a slice containing information about all
 // transactions made and received with this wallet.
 func (w *Wallet) FetchTxHistory() ([]txrecords.TxRecord, error) {
 	return w.db.FetchTxRecords()
-}
-
-// GetSavedHeight returns the saved height
-func (w *Wallet) GetSavedHeight() (uint64, error) {
-	return w.db.GetWalletHeight()
-}
-
-// UpdateWalletHeight update the wallet to the new height
-func (w *Wallet) UpdateWalletHeight(newHeight uint64) error {
-	return w.db.UpdateWalletHeight(newHeight)
 }
 
 //// PublicKey returns the wallet public key
@@ -315,28 +208,9 @@ func (w *Wallet) SecretKey() *rusk.SecretKey {
 	return w.secretKey
 }
 
-// PublicAddress returns the wallet public address
-func (w *Wallet) PublicAddress() (string, error) {
-	pubAddr, err := w.keyPair.PublicKey().PublicAddress(w.netPrefix)
-	if err != nil {
-		return "", err
-	}
-	return pubAddr.String(), nil
-}
-
 // Keys returns the BLS keys
 func (w *Wallet) Keys() consensuskey.Keys {
 	return *w.consensusKeys
-}
-
-// PrivateSpend calls the PrivateSpend method on the keypair
-func (w *Wallet) PrivateSpend() ([]byte, error) {
-	privateSpend, err := w.keyPair.PrivateSpend()
-	if err != nil {
-		return nil, err
-	}
-
-	return privateSpend.Bytes(), nil
 }
 
 // ClearDatabase will remove all info from the database.
--- pkg/core/data/wallet/wallet_test.go
+++ pkg/core/data/wallet/wallet_test.go
@@ -3,21 +3,18 @@ package wallet
 import (
 	"bytes"
 	"context"
-	"github.com/dusk-network/dusk-blockchain/harness/tests"
-	"github.com/dusk-network/dusk-protobuf/autogen/go/rusk"
-	"github.com/stretchr/testify/require"
-	"google.golang.org/grpc"
-	"math/big"
 	"os"
 	"testing"
 	"time"
 
-	"github.com/dusk-network/dusk-blockchain/pkg/core/data/block"
+	"github.com/dusk-network/dusk-blockchain/harness/tests"
+	"github.com/dusk-network/dusk-protobuf/autogen/go/rusk"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc"
+
 	"github.com/dusk-network/dusk-blockchain/pkg/core/data/database"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/data/key"
-	"github.com/dusk-network/dusk-blockchain/pkg/core/data/transactions"
 
-	"github.com/bwesterb/go-ristretto"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -95,172 +92,6 @@ func TestNewWallet(t *testing.T) {
 	assert.True(t, bytes.Equal(w.consensusKeys.BLSPubKeyBytes, loadedWallet.consensusKeys.BLSPubKeyBytes))
 }
 
-func TestReceivedTx(t *testing.T) {
-	netPrefix := byte(1)
-	fee := int64(0)
-
-	client, conn := createRPCConn(t)
-	defer conn.Close()
-
-	db, err := database.New(dbPath)
-	assert.Nil(t, err)
-	defer os.RemoveAll(dbPath)
-	defer os.Remove(seedFile)
-	defer os.Remove(secretFile)
-
-	seed, err := GenerateNewSeed(nil)
-	require.Nil(t, err)
-
-	ctx := context.Background()
-	secretKey, err := client.GenerateSecretKey(ctx, &rusk.GenerateSecretKeyRequest{B: seed})
-	require.Nil(t, err)
-
-	w, err := New(nil, seed, netPrefix, db, "pass", seedFile, secretFile, secretKey)
-	assert.Nil(t, err)
-
-	tx, err := w.NewStandardTx(fee)
-	assert.Nil(t, err)
-
-	var tenDusk ristretto.Scalar
-	tenDusk.SetBigInt(big.NewInt(10))
-
-	sendersAddr := generateSendAddr(t, netPrefix, w.keyPair)
-	assert.Nil(t, err)
-
-	err = tx.AddOutput(sendersAddr, tenDusk)
-	assert.Nil(t, err)
-
-	//err = w.Sign(tx)
-	//assert.Nil(t, err)
-
-	for _, output := range tx.Outputs {
-		_, ok := w.keyPair.DidReceiveTx(tx.R, output.PubKey, output.Index)
-		assert.True(t, ok)
-	}
-
-	var destKeys []ristretto.Point
-	for _, output := range tx.Outputs {
-		destKeys = append(destKeys, output.PubKey.P)
-	}
-	assert.False(t, hasDuplicates(destKeys))
-}
-
-func TestCheckBlock(t *testing.T) {
-	netPrefix := byte(1)
-
-	aliceSeed := "alice-seed.dat"
-	aliceKey := "alice-key.dat"
-	defer os.Remove(aliceSeed)
-	defer os.Remove(aliceKey)
-
-	bobSeed := "bob-seed.dat"
-	bobKey := "bob-key.dat"
-	defer os.Remove(bobSeed)
-	defer os.Remove(bobKey)
-
-	aliceDBPath := "alice"
-	defer os.RemoveAll(aliceDBPath)
-
-	bobDBPath := "bob"
-	defer os.RemoveAll(bobDBPath)
-
-	alice := generateWallet(t, netPrefix, aliceDBPath, aliceSeed, aliceKey)
-	bob := generateWallet(t, netPrefix, bobDBPath, bobSeed, bobKey)
-
-	bobAddr, err := bob.keyPair.PublicKey().PublicAddress(netPrefix)
-	assert.Nil(t, err)
-
-	var numTxs = 3 // numTxs to send to Bob
-
-	blk := block.NewBlock()
-	blk.Header.Height = 0
-	for i := 0; i < numTxs; i++ {
-		tx := generateStandardTx(t, *bobAddr, 20, alice)
-		assert.Nil(t, err)
-		blk.AddTx(tx)
-	}
-
-	count, err := bob.CheckWireBlockReceived(*blk)
-	assert.Nil(t, err)
-	assert.Equal(t, uint64(numTxs), count)
-
-	_, err = alice.CheckWireBlockSpent(*blk)
-	assert.Nil(t, err)
-}
-
-func TestSpendLockedInputs(t *testing.T) {
-	netPrefix := byte(1)
-	alice := generateWallet(t, netPrefix, dbPath, seedFile, secretFile)
-
-	defer os.RemoveAll(dbPath)
-	defer os.Remove(seedFile)
-	defer os.Remove(secretFile)
-
-	blk := block.NewBlock()
-	blk.Header.Height = 0
-	tx := generateStakeTx(t, 20, alice, 100000)
-	blk.AddTx(tx)
-
-	_, err := alice.CheckWireBlockReceived(*blk)
-	assert.Nil(t, err)
-
-	// Attempt to send a Standard tx with this single input we received.
-	// Set our FetchInputs function to a proper one, so that we actually
-	// check the database.
-	standard, err := alice.NewStandardTx(100)
-	assert.NoError(t, err)
-
-	var amount ristretto.Scalar
-	amount.SetBigInt(big.NewInt(5000))
-
-	pubAddr, err := alice.keyPair.PublicKey().PublicAddress(netPrefix)
-	assert.NoError(t, err)
-	assert.NoError(t, standard.AddOutput(*pubAddr, amount))
-
-	// Should fail
-	//assert.Error(t, alice.Sign(standard))
-}
-
-func TestCheckUnconfirmedBalance(t *testing.T) {
-	netPrefix := byte(1)
-
-	aliceSeed := "alice-seed.dat"
-	aliceKey := "alice-key.dat"
-	defer os.Remove(aliceSeed)
-	defer os.Remove(aliceKey)
-
-	bobSeed := "bob-seed.dat"
-	bobKey := "bob-key.dat"
-	defer os.Remove(bobSeed)
-	defer os.Remove(bobKey)
-
-	aliceDBPath := "alice"
-	defer os.RemoveAll(aliceDBPath)
-
-	bobDBPath := "bob"
-	defer os.RemoveAll(bobDBPath)
-
-	alice := generateWallet(t, netPrefix, aliceDBPath, aliceSeed, aliceKey)
-	bob := generateWallet(t, netPrefix, bobDBPath, bobSeed, bobKey)
-
-	bobAddr, err := bob.keyPair.PublicKey().PublicAddress(netPrefix)
-	assert.Nil(t, err)
-
-	var numTxs = 3          // numTxs to send to Bob
-	var amount = int64(500) // amount to send for each tx
-
-	txs := make([]transactions.Transaction, 0, numTxs)
-	for i := 0; i < numTxs; i++ {
-		tx := generateStandardTx(t, *bobAddr, amount, alice)
-		assert.Nil(t, err)
-		txs = append(txs, tx)
-	}
-
-	balance, err := bob.CheckUnconfirmedBalance(txs)
-	assert.Nil(t, err)
-	assert.Equal(t, uint64(int64(numTxs)*amount), balance)
-}
-
 func TestCatchEOF(t *testing.T) {
 	netPrefix := byte(1)
 
@@ -310,60 +141,8 @@ func generateWallet(t *testing.T, netPrefix byte, walletPath, seedFile, secretFi
 	return w
 }
 
-func generateStandardTx(t *testing.T, receiver key.PublicAddress, amount int64, sender *Wallet) *transactions.Standard {
-	tx, err := sender.NewStandardTx(0)
-	assert.Nil(t, err)
-
-	var duskAmount ristretto.Scalar
-	duskAmount.SetBigInt(big.NewInt(amount))
-
-	err = tx.AddOutput(receiver, duskAmount)
-	assert.Nil(t, err)
-
-	//err = sender.Sign(tx)
-	//assert.Nil(t, err)
-
-	return tx
-}
-
-func generateStakeTx(t *testing.T, amount int64, sender *Wallet, lockTime uint64) *transactions.Stake {
-	var duskAmount ristretto.Scalar
-	duskAmount.SetBigInt(big.NewInt(amount))
-
-	tx, err := sender.NewStakeTx(0, lockTime, duskAmount)
-	assert.Nil(t, err)
-
-	//err = sender.Sign(tx)
-	//assert.Nil(t, err)
-
-	return tx
-}
-
 func generateSendAddr(t *testing.T, netPrefix byte, randKeyPair *key.Key) key.PublicAddress {
 	pubAddr, err := randKeyPair.PublicKey().PublicAddress(netPrefix)
 	assert.Nil(t, err)
 	return *pubAddr
-}
-
-// https://www.dotnetperls.com/duplicates-go
-func hasDuplicates(elements []ristretto.Point) bool {
-	encountered := map[ristretto.Point]bool{}
-
-	for v := range elements {
-		if encountered[elements[v]] == true {
-			return true
-		}
-		encountered[elements[v]] = true
-	}
-	return false
-}
-
-func fetchInputs(netPrefix byte, db *database.DB, totalAmount int64, key *key.Key) ([]*transactions.Input, int64, error) {
-	// Fetch all inputs from database that are >= totalAmount
-	// returns error if inputs do not add up to total amount
-	privSpend, err := key.PrivateSpend()
-	if err != nil {
-		return nil, 0, err
-	}
-	return db.FetchInputs(privSpend.Bytes(), totalAmount)
 }
