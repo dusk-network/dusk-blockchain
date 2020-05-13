b40a3bd849f7c7634c64ba7a7467c8d6a85b8359
Jules de Smit <julesdesmit@gmail.com>
2020-05-13 19:30:43 +0200

    Add goroutine which handles mempool rpcbus call, and pass secretkey to GenerateGenesisBlock

--- pkg/core/consensus/candidate/genesis_test.go
+++ pkg/core/consensus/candidate/genesis_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/dusk-network/dusk-blockchain/pkg/core/data/block"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/data/transactions"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/message"
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/rpcbus"
 	"github.com/stretchr/testify/require"
 )
@@ -21,10 +22,25 @@ func TestGenerateGenesis(t *testing.T) {
 	// adjusted to become an integration test
 	rpcBus := rpcbus.New()
 
+	go provideMempoolTxs(rpcBus)
+
 	seed := make([]byte, 64)
 	if _, err := rand.Read(seed); err != nil {
 		t.Fatal(err.Error())
 	}
+	sk := "b201b3b64c30f5cf6980f50c30b73e6d1352f08846fe5546282d84a827509409e83d98da3509ec2f7985e3899e575509101ec4b1db1f898be9c73868e4c8a107"
+	skBytes, err := hex.DecodeString(sk)
+	require.Nil(t, err)
+
+	secretKey := &transactions.SecretKey{
+		A: &transactions.Scalar{
+			Data: skBytes[0:32],
+		},
+		B: &transactions.Scalar{
+			Data: skBytes[32:64],
+		},
+	}
+
 	pk := "61c36e407ac91f20174572eec95f692f5cff1c40bacd1b9f86c7fa7202e93bb6753c2f424caf3c9220876e8cfe0afdff7ffd7c984d5c7d95fa0b46cf3781d883"
 	pkBytes, err := hex.DecodeString(pk)
 	require.Nil(t, err)
@@ -39,7 +55,7 @@ func TestGenerateGenesis(t *testing.T) {
 	}
 
 	// Generate a new genesis block with new wallet pubkey
-	genesisHex, err := GenerateGenesisBlock(rpcBus, publicKey)
+	genesisHex, err := GenerateGenesisBlock(rpcBus, secretKey, publicKey)
 	if err != nil {
 		fmt.Println(err)
 		t.Fatalf("expecting valid genesis block")
@@ -91,4 +107,14 @@ func TestGenesisBlock(t *testing.T) {
 	if b.Txs[0].Type() != transactions.Distribute {
 		t.Fatalf("expecting coinbase tx in cfg.TestNetGenesisBlob")
 	}
+}
+
+func provideMempoolTxs(rpcBus *rpcbus.RPCBus) {
+	c := make(chan rpcbus.Request, 1)
+	if err := rpcBus.Register(topics.GetMempoolTxsBySize, c); err != nil {
+		panic(err)
+	}
+
+	r := <-c
+	r.RespChan <- rpcbus.NewResponse([]transactions.ContractCall{}, nil)
 }
