25697264f1e4e1ec87c4a65d2bc112c4de0f27ee
Jules de Smit <julesdesmit@gmail.com>
2020-05-13 15:38:31 +0200

    Fix transactor.New call

--- cmd/dusk/server.go
+++ cmd/dusk/server.go
@@ -131,7 +131,7 @@ func Setup() *Server {
 	}
 
 	// Setting up the transactor component
-	_ = transactor.New(eventBus, nil, grpcServer, client, proxy.Provider(), proxy.KeyMaster())
+	_ = transactor.New(eventBus, rpcBus, nil, grpcServer, client, proxy.Provider(), proxy.KeyMaster())
 	if err != nil {
 		log.Panic(err)
 	}
