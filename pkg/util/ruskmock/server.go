8b32276dad90f2f9ae29e9b0e0bba7a0cff217b5
Jules de Smit <julesdesmit@gmail.com>
2020-05-18 14:23:18 +0200

    Add comments

--- pkg/util/ruskmock/server.go
+++ pkg/util/ruskmock/server.go
@@ -5,16 +5,25 @@ import (
 	"google.golang.org/grpc"
 )
 
+// Config contains a list of settings that determine the behaviour
+// of the `Server`.
 type Config struct{}
 
+// DefaultConfig returns the default configuration for the Rusk mock server.
 func DefaultConfig() *Config {
 	return &Config{}
 }
 
+// Server is a stand-in Rusk server, which can be used during any kind of
+// testing. Its behaviour can be modified depending on the settings of the
+// `Config` struct, contained in the `Server`, to simulate different types
+// of scenarios on demand.
 type Server struct {
 	cfg *Config
 }
 
+// New returns a new Rusk mock server with the given config. If no config is
+// passed, a default one is put into place.
 func New(cfg *Config) (*Server, error) {
 	grpcServer := grpc.NewServer()
 
