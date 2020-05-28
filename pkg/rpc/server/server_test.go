90730180ba237294f11f70fcdd6c40e529821612
emanuele francioni <emanuele@dusk.network>
2020-05-28 10:34:21 +0200

    add test main to facilitate setup and teardown of grpc server and client

--- pkg/rpc/server/server_test.go
+++ pkg/rpc/server/server_test.go
@@ -1,7 +1,35 @@
 package server_test
 
-import "testing"
+import (
+	"os"
+	"testing"
 
-func TestAuth(t *testing.T) {
+	"github.com/dusk-network/dusk-blockchain/pkg/config"
+	"google.golang.org/grpc"
+)
+
+var client *AuthClient
+
+func TestMain(m *testing.M) {
+	// create the GRPC server here
+	srv, err := SetupGRPCServer()
+	// panic in case of errors
+	if err != nil {
+		panic(err)
+	}
+
+	// get the server address from configuration
+	conf := config.Get().RPC
+	// create the GRPC connection
+	conn := grpc.Dial(conf.Address, grpc.WithInsecure())
+	// create the client
+	client = NewClient(conn)
+	// run the tests
+	res := m.Run()
+	// done
+	os.Exit(res)
+}
+
+func TestCreateSession(t *testing.T) {
 
 }
