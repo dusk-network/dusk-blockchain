f9466b1d89e54855425cf637d5bf2d26d37779b5
emanuele francioni <emanuele@dusk.network>
2020-05-28 09:21:21 +0200

    remove simple authentication parameters, add session duration parameter

--- pkg/config/groups.go
+++ pkg/config/groups.go
@@ -59,8 +59,9 @@ type profileConfiguration struct {
 
 // pkg/rpc package configs
 type rpcConfiguration struct {
-	Network string
-	Address string
+	Network             string
+	Address             string
+	SessionDurationMins uint
 
 	EnableTLS bool
 	CertFile  string
