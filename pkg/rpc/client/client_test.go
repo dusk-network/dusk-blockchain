2720ca09d7ec76ca75e5844b8a1a1fa5b1ec27a4
emanuele francioni <emanuele@dusk.network>
2020-06-17 14:43:18 +0200

    add test for reusing session connection

--- pkg/rpc/client/client_test.go
+++ pkg/rpc/client/client_test.go
@@ -1,6 +1,7 @@
 package client_test
 
 import (
+	"context"
 	"net"
 	"os"
 	"testing"
@@ -8,9 +9,73 @@ import (
 
 	"github.com/dusk-network/dusk-blockchain/pkg/rpc/client"
 	"github.com/dusk-network/dusk-blockchain/pkg/rpc/server"
+	"github.com/dusk-network/dusk-protobuf/autogen/go/node"
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 )
+
+// WWalletSrvMock is a mockup structure used to test the session client and server
+type WalletSrvMock struct{}
+
+// GetTxHistory will return a subset of the transactions that were sent and received.
+func (t *WalletSrvMock) GetTxHistory(ctx context.Context, e *node.EmptyRequest) (*node.TxHistoryResponse, error) {
+	return &node.TxHistoryResponse{}, nil
+}
+
+// CreateWallet creates a new wallet from a password or seed
+func (t *WalletSrvMock) CreateWallet(ctx context.Context, c *node.CreateRequest) (*node.LoadResponse, error) {
+	return &node.LoadResponse{}, nil
+}
+
+// LoadWallet from a password
+func (t *WalletSrvMock) LoadWallet(ctx context.Context, l *node.LoadRequest) (*node.LoadResponse, error) {
+	return &node.LoadResponse{}, nil
+}
+
+// CreateFromSeed creates a wallet from a seed
+func (t *WalletSrvMock) CreateFromSeed(ctx context.Context, c *node.CreateRequest) (*node.LoadResponse, error) {
+	return &node.LoadResponse{}, nil
+}
+
+// ClearWalletDatabase clears the wallet database, containing the unspent outputs.
+func (t *WalletSrvMock) ClearWalletDatabase(ctx context.Context, e *node.EmptyRequest) (*node.GenericResponse, error) {
+	return &node.GenericResponse{}, nil
+}
+
+// CallContract will create a transaction that calls a smart contract.
+func (t *WalletSrvMock) CallContract(ctx context.Context, c *node.CallContractRequest) (*node.TransactionResponse, error) {
+	return &node.TransactionResponse{}, nil
+}
+
+// Transfer will create a normal transaction, transferring DUSK.
+func (t *WalletSrvMock) Transfer(ctx context.Context, tr *node.TransferRequest) (*node.TransactionResponse, error) {
+	return &node.TransactionResponse{}, nil
+}
+
+// Bid will create a bidding transaction.
+func (t *WalletSrvMock) Bid(ctx context.Context, c *node.BidRequest) (*node.TransactionResponse, error) {
+	return &node.TransactionResponse{}, nil
+}
+
+// Stake will create a staking transaction.
+func (t *WalletSrvMock) Stake(ctx context.Context, c *node.StakeRequest) (*node.TransactionResponse, error) {
+	return &node.TransactionResponse{}, nil
+}
+
+// GetWalletStatus returns whether or not the wallet is currently loaded.
+func (t *WalletSrvMock) GetWalletStatus(ctx context.Context, e *node.EmptyRequest) (*node.WalletStatusResponse, error) {
+	return &node.WalletStatusResponse{}, nil
+}
+
+// GetAddress returns the address of the loaded wallet.
+func (t *WalletSrvMock) GetAddress(ctx context.Context, e *node.EmptyRequest) (*node.LoadResponse, error) {
+	return &node.LoadResponse{}, nil
+}
+
+// GetBalance returns the balance of the loaded wallet.
+func (t *WalletSrvMock) GetBalance(ctx context.Context, e *node.EmptyRequest) (*node.BalanceResponse, error) {
+	return &node.BalanceResponse{}, nil
+}
 
 func init() {
 	log.SetLevel(log.ErrorLevel)
@@ -28,6 +93,10 @@ func TestMain(m *testing.M) {
 	if err != nil {
 		panic(err)
 	}
+
+	// register wallet mock server to be able to test the session
+	// the mock is replying with no error and an empty response
+	node.RegisterWalletServer(grpcSrv, &WalletSrvMock{})
 
 	// get the server address from configuration
 	go serve(conf.Network, conf.Address, grpcSrv)
--- pkg/rpc/client/session_test.go
+++ pkg/rpc/client/session_test.go
@@ -1,8 +1,11 @@
 package client_test
 
 import (
+	"context"
 	"testing"
+	"time"
 
+	"github.com/dusk-network/dusk-protobuf/autogen/go/node"
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
 )
@@ -20,14 +23,41 @@ func TestCreateDropSession(t *testing.T) {
 	assert.Error(nodeClient.DropSession(grpc.WithInsecure()))
 }
 
-// TestPersistentSession tests that subsequent calls to
-// NodeClient.GetSessionConn do not recreate a session
-func TestPersistentSession(t *testing.T) {
+// TestPersistentConn tests that subsequent calls to
+// NodeClient.GetSessionConn do not recreate a connection
+func TestPersistentConn(t *testing.T) {
 	assert := assert.New(t)
-	token1, err := nodeClient.GetSessionConn(grpc.WithInsecure(), grpc.WithBlock())
+	conn1, err := nodeClient.GetSessionConn(grpc.WithInsecure(), grpc.WithBlock())
 	assert.NoError(err)
 
-	token2, err := nodeClient.GetSessionConn(grpc.WithInsecure(), grpc.WithBlock())
+	conn2, err := nodeClient.GetSessionConn(grpc.WithInsecure(), grpc.WithBlock())
 	assert.NoError(err)
-	assert.Equal(token1, token2)
+	assert.Equal(conn1, conn2)
+}
+
+// TestPersistentSession tests that the client can exploit the session injected
+// through the AuthClientInterceptor and perform authenticated calls
+func TestPersistentSession(t *testing.T) {
+	assert := assert.New(t)
+	conn, err := nodeClient.GetSessionConn(grpc.WithInsecure(), grpc.WithBlock())
+	assert.NoError(err)
+	assert.NoError(createDumbWallet(conn))
+}
+
+func createDumbWallet(conn grpc.ClientConnInterface) error {
+	// create a wallet client
+	walletClient := node.NewWalletClient(conn)
+
+	// spawn an authenticated RPC call
+	ctx, cancel := context.WithTimeout(context.Background(), 50*time.Millisecond)
+	defer cancel()
+	in := &node.CreateRequest{
+		Password: "pippo",
+		Seed:     []byte("pluto"),
+	}
+
+	// the mock always returns success, so if there is an error it is likely to
+	// be linked to the session and authorization layer
+	_, err := walletClient.CreateWallet(ctx, in)
+	return err
 }
