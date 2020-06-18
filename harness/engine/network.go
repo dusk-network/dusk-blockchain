2c20f688e7d9367f283b666407c72f892ef0faaa
emanuele francioni <emanuele@dusk.network>
2020-06-18 13:13:04 +0200

    tighten the bootstrap delay to 1 second per node bootstrapped in the test-harness

--- harness/engine/network.go
+++ harness/engine/network.go
@@ -85,7 +85,7 @@ func (n *Network) Bootstrap(workspace string) error {
 	log.Infof("Local network workspace: %s", workspace)
 	log.Infof("Running %d nodes", len(n.Nodes))
 
-	time.Sleep(20 * time.Second)
+	time.Sleep(time.Duration(len(n.Nodes)) * time.Second)
 
 	return nil
 }
