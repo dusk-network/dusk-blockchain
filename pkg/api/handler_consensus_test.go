645155447ca46439c066213c02a4ba8d3f4ec083
Thomas Modeneis <modeneisnl@gmail.com>
2020-08-28 11:09:08 +0200

    update testcase to add more cases

--- pkg/api/handler_consensus_test.go
+++ pkg/api/handler_consensus_test.go
@@ -14,19 +14,34 @@ func TestConsensusAPI(t *testing.T) {
 
 	_, dbheavy := heavy.CreateDBConnection()
 	_, db := lite.CreateDBConnection()
-	targetURL := "/consensus/provisioners"
 
 	apiServer, err := api.NewHTTPServer(nil, nil, dbheavy, db)
 	if err != nil {
 		t.Errorf("API http server error: %v", err)
 	}
 
+	var tt = []struct {
+		targetURL     string
+		name          string
+		ExpectSuccess bool
+	}{
+		{
+			targetURL:     "/consensus/provisioners",
+			name:          "Get provisioners",
+			ExpectSuccess: true,
+		},
+	}
+
 	testflight.WithServer(apiServer.Server.Handler, func(r *testflight.Requester) {
 
-		t.Log("testflight.WithServer --> ")
+		for _, tc := range tt {
 
-		response := r.Get(targetURL)
-		require.NotNil(t, response)
+			t.Run(tc.name, func(t *testing.T) {
+
+				response := r.Get(tc.targetURL)
+				require.NotNil(t, response)
+			})
+		}
 
 	})
 }
