d2fde22a53dbf4757200fd88a07fa5a937c51857
Thomas Modeneis <modeneisnl@gmail.com>
2020-09-11 09:43:24 +0200

    fix store_test.go that broke after configurable timeout for broker get candidate was implemented

--- pkg/core/candidate/store_test.go
+++ pkg/core/candidate/store_test.go
@@ -3,6 +3,7 @@ package candidate
 import (
 	"bytes"
 	"fmt"
+	"os"
 	"strconv"
 	"testing"
 	"time"
@@ -57,6 +58,14 @@ func TestDeepCopy(t *testing.T) {
 
 // Test the candidate request functionality.
 func TestRequestCandidate(t *testing.T) {
+	//setup viper timeout
+	cwd, err := os.Getwd()
+	require.Nil(t, err)
+
+	r, err := config.LoadFromFile(cwd + "/../../../dusk.toml")
+	require.Nil(t, err)
+	config.Mock(&r)
+
 	assert := assert.New(t)
 	require := require.New(t)
 	eb := eventbus.New()
