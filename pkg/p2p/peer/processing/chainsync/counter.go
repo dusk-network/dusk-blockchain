bcc4b9ababa8dea18c98ef36e9a1fad657e4282e
Goranov <georgi@dusk.network>
2020-10-09 16:21:52 +0300

    Introduce syncing peer in Counter

--- pkg/p2p/peer/processing/chainsync/counter.go
+++ pkg/p2p/peer/processing/chainsync/counter.go
@@ -1,6 +1,7 @@
 package chainsync
 
 import (
+	"errors"
 	"sync"
 	"time"
 
@@ -13,11 +14,22 @@ import (
 
 var syncTime = 30 * time.Second
 
+// maxBlocksCount maximum number of blocks to fetch from a single peer on syncing procedure
+const maxBlocksCount = 500
+
 // Counter is a simple guarded counter, which can be used to figure out if we are currently
 // syncing with another peer. It is used to orchestrate requests for blocks.
+// Counter also represents a shared state amongst all peer connection synchronizers
 type Counter struct {
-	lock            sync.RWMutex
+	lock sync.RWMutex
+
+	// blocksRemaining remaining blocks to catch up with so-called syncing peer
+	// blocksRemaining  > 0 has meaning of sync mode
 	blocksRemaining uint64
+	// syncingPeerAddr is the address of the peer we are syncing with (while in syncMode)
+	// Only 1 syncingPeer at a time to prevent asking
+	// many peers for the same blocks.
+	syncingPeerAddr string
 
 	timer    *time.Timer
 	stopChan chan struct{}
@@ -34,7 +46,14 @@ func NewCounter(subscriber eventbus.Subscriber) *Counter {
 	return sc
 }
 
+// decrement decrements blockRemaining if a new block is accepted by the chain
 func (s *Counter) decrement(m message.Message) error {
+
+	if !s.IsSyncing() {
+		// no need to decrement in non-syncing mode
+		return nil
+	}
+
 	s.lock.Lock()
 	defer s.lock.Unlock()
 	if s.blocksRemaining > 0 {
@@ -42,11 +61,14 @@ func (s *Counter) decrement(m message.Message) error {
 
 		// Stop the timer goroutine if we're done
 		if s.blocksRemaining == 0 {
+			s.syncingPeerAddr = ""
 			s.stopChan <- struct{}{}
 			return nil
 		}
 
 		// Refresh the timer whenever we get a new block during sync
+		// TODO: Reconsider syncTime default value. Currently the entire sync procedure
+		// with a single peer could consume up to 25mins (maxBlocksCount*syncTime)
 		s.timer.Reset(syncTime)
 	}
 
@@ -60,19 +82,28 @@ func (s *Counter) IsSyncing() bool {
 	return s.blocksRemaining > 0
 }
 
-// StartSyncing with the peers
-func (s *Counter) StartSyncing(heightDiff uint64) {
+// StartSyncing with a single peer
+func (s *Counter) StartSyncing(heightDiff uint64, syncPeerAddr string) (string, error) {
+
 	s.lock.Lock()
 	defer s.lock.Unlock()
 
-	// We can only receive up to 500 blocks at a time
-	if heightDiff > 500 {
-		heightDiff = 500
+	if s.blocksRemaining > 0 {
+		return s.syncingPeerAddr, errors.New("already syncing")
+	}
+
+	s.syncingPeerAddr = syncPeerAddr
+
+	// We can only receive up to maxBlocksCount blocks at a time
+	if heightDiff > maxBlocksCount {
+		heightDiff = maxBlocksCount
 	}
 
 	s.blocksRemaining = heightDiff
 	s.timer = time.NewTimer(syncTime)
 	go s.listenForTimer(s.timer)
+
+	return s.syncingPeerAddr, nil
 }
 
 func (s *Counter) listenForTimer(timer *time.Timer) {
@@ -81,6 +112,15 @@ func (s *Counter) listenForTimer(timer *time.Timer) {
 		s.lock.Lock()
 		defer s.lock.Unlock()
 		s.blocksRemaining = 0
+		s.syncingPeerAddr = ""
 	case <-s.stopChan:
 	}
+
+}
+
+// GetSyncingPeer returns address of the syncing Peer (IP:Port)
+func (s *Counter) GetSyncingPeer() string {
+	s.lock.RLock()
+	defer s.lock.RUnlock()
+	return s.syncingPeerAddr
 }
--- pkg/p2p/peer/processing/chainsync/counter_test.go
+++ pkg/p2p/peer/processing/chainsync/counter_test.go
@@ -19,7 +19,7 @@ func TestStopTimerGoroutine(t *testing.T) {
 	// Set syncTime to something more reasonable for a unit test
 	syncTime = 1 * time.Second
 
-	c.StartSyncing(1)
+	c.StartSyncing(1, "test_peer_addr")
 
 	msg := message.New(topics.AcceptedBlock, message.SafeBuffer{})
 	// Decrement to 0. This should stop the running `listenForTimer` goroutine
@@ -29,7 +29,7 @@ func TestStopTimerGoroutine(t *testing.T) {
 
 	// Set syncTime back to original value, so we can easily check the effects of the previous timer
 	syncTime = 30 * time.Second
-	c.StartSyncing(1)
+	c.StartSyncing(1, "test_peer_addr")
 
 	// Wait one second, and see if the old timer fires
 	time.Sleep(1 * time.Second)
