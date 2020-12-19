028db77d0d980917670a55905679c7f7afc4243f
Jules de Smit <julesdesmit@gmail.com>
2020-12-19 22:12:14 +0000

    Add license headers to all go files
    Fixes #774

--- pkg/core/chain/chain_rc_test.go
+++ pkg/core/chain/chain_rc_test.go
@@ -1,5 +1,11 @@
 // +build race
 
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT License was not distributed with this
+// file, you can obtain one at https://opensource.org/licenses/MIT.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
 package chain
 
 import (
