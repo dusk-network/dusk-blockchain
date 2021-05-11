2ddad26503228e9e861372a8b089875905fe585a
goshawk-3 <goshawk@dusk.network>
2021-05-11 10:17:16 +0300

    Introduce point-to-point messaging in Kadcast

--- pkg/p2p/kadcast/basereader.go
+++ pkg/p2p/kadcast/basereader.go
@@ -12,7 +12,9 @@ import (
 
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/kadcast/encoding"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/peer"
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/message"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/protocol"
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
 )
 
@@ -39,24 +41,29 @@ func newBaseReader(lpeerInfo encoding.PeerInfo, publisher eventbus.Publisher,
 }
 
 func (r *baseReader) handleBroadcast(raddr string, b []byte) error {
+	ll := log.WithField("process", "handle_broadcast")
+
 	var header encoding.Header
 
 	// Unmarshal message header
 	buf := bytes.NewBuffer(b)
 	if err := header.UnmarshalBinary(buf); err != nil {
-		log.WithError(err).Warn("reader rejects a packet")
+		ll.WithError(err).Warn("reader rejects a packet")
 		return err
 	}
 
+	var remotePeer *encoding.PeerInfo
+
 	// Run extra checks over message data
-	if err := isValidMessage(raddr, header); err != nil {
-		log.WithError(err).Warn("reader rejects a packet")
+	var err error
+	if remotePeer, err = isValidMessage(raddr, header); err != nil {
+		ll.WithError(err).Warn("reader rejects a packet")
 		return err
 	}
 
 	// Unmarshal broadcast message payload
 	var p encoding.BroadcastPayload
-	if err := p.UnmarshalBinary(buf); err != nil {
+	if err = p.UnmarshalBinary(buf); err != nil {
 		log.WithError(err).Warn("could not unmarshal message")
 		return err
 	}
@@ -64,18 +71,29 @@ func (r *baseReader) handleBroadcast(raddr string, b []byte) error {
 	// Read `message` from gossip frame
 	buf = bytes.NewBuffer(p.GossipFrame)
 
-	message, err := r.gossip.ReadFrame(buf)
+	m, err := r.gossip.ReadFrame(buf)
 	if err != nil {
-		log.WithError(err).Warnln("could not read the gossip frame")
+		ll.WithError(err).Warnln("could not read the gossip frame")
 		return err
 	}
 
 	// Register message in the global message registry for stats collecting
 	// diagnostics.RegisterWireMsg(topics.Kadcast.String(), packet)
 
-	if err = r.processor.Collect(raddr, message, nil, []byte{p.Height}); err != nil {
-		log.WithField("process", "kadcast_reader").
-			WithError(err).Error("failed to process message")
+	bufs, err := r.processor.Collect(raddr, m, nil, []byte{p.Height})
+	if err != nil {
+		ll.WithError(err).Error("messageProcessor failed to collect message")
+	}
+
+	// Any response message is translated into Kadcast Point-to-Point wire message.
+	// In other words, any bufs item is sent back to the sender node (remotePeer).
+	// Based on that Request-Response messaging is implemented in Kadcast.
+	for i := 0; i < len(bufs); i++ {
+		ll.WithField("raddr", remotePeer.Address()).Trace("send point-to-point message")
+
+		// Send Kadcast point-to-point message with destination raddr
+		r.publisher.Publish(topics.KadcastPoint,
+			message.NewWithHeader(topics.Unknown, bufs[i], []byte(remotePeer.Address())))
 	}
 
 	// Repropagate message here
@@ -93,24 +111,24 @@ func (r *baseReader) handleBroadcast(raddr string, b []byte) error {
 	return nil
 }
 
-func isValidMessage(remotePeerIP string, header encoding.Header) error {
+func isValidMessage(remotePeerIP string, header encoding.Header) (*encoding.PeerInfo, error) {
 	// Reader handles only broadcast-type messages
 	if header.MsgType != encoding.BroadcastMsg {
-		return errors.New("message type not supported")
+		return nil, errors.New("message type not supported")
 	}
 
 	// Make remote peerInfo based on addr from IP datagram and RemotePeerPort
 	// from header
 	remotePeer, err := encoding.MakePeerFromIP(remotePeerIP, header.RemotePeerPort)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Ensure the RemotePeerID from header is correct one
 	// This together with Nonce-PoW aims at providing a bit of DDoS protection
 	if !bytes.Equal(remotePeer.ID[:], header.RemotePeerID[:]) {
-		return errors.New("invalid remote peer id")
+		return nil, errors.New("invalid remote peer id")
 	}
 
-	return nil
+	return &remotePeer, nil
 }
