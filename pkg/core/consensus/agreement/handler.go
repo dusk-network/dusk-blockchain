6999b14a05ee8e6657695fb765c700e8e09c7b23
goshawk-3 <goshawk@dusk.network>
2021-08-02 16:31:20 +0300

    Use consensus.WithFields to facilitate logging

--- pkg/core/consensus/agreement/handler.go
+++ pkg/core/consensus/agreement/handler.go
@@ -13,14 +13,13 @@ import (
 	"math"
 
 	"github.com/dusk-network/bls12_381-sign-go/bls"
-	"github.com/dusk-network/dusk-blockchain/pkg/config"
+	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/committee"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/header"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/key"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/msg"
 	"github.com/dusk-network/dusk-blockchain/pkg/core/consensus/user"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/message"
-	"github.com/dusk-network/dusk-blockchain/pkg/util"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/sortedset"
 	"github.com/sirupsen/logrus"
 	log "github.com/sirupsen/logrus"
@@ -109,18 +108,11 @@ func (a *handler) Verify(ev message.Agreement) error {
 		allVoters += subcommittee.TotalOccurrences()
 
 		if log.GetLevel() >= logrus.DebugLevel {
-			log.WithField("process", "consensus").
-				WithField("round", hdr.Round).
-				WithField("total_votes", allVoters).
-				WithField("hash", util.StringifyBytes(hdr.BlockHash)).
-				WithField("step", hdr.Step).
-				WithField("step_voting_committee", committee).
-				WithField("step_voted_committee", subcommittee).
-				WithField("bitset", fmt.Sprintf("%b", votes.BitSet)).
-				WithField("provisioners", a.Provisioners).
-				WithField("this_provisioner", util.StringifyBytes(a.Keys.BLSPubKey)).
-				WithField("this_node", config.Get().Network.Port).
-				WithField("event", "agreement_received").Debug("")
+			log := consensus.WithFields(hdr.Round, step, "agreement_received",
+				hdr.BlockHash, a.Keys.BLSPubKey, &committee, &subcommittee, &a.Provisioners)
+
+			log.WithField("bitset", votes.BitSet).
+				WithField("total_votes", allVoters).Debug()
 		}
 
 		apk, err := ReconstructApk(subcommittee.Set)
