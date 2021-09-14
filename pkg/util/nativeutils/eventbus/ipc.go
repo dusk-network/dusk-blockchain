584445d3b7e2e2bcb77c41d737c72bb5e66f02b5
Loki Verloren <loki@cybriq.systems>
2021-09-14 16:07:59 +0300

    small fixes to satisfy linter for comments

--- pkg/util/nativeutils/eventbus/ipc.go
+++ pkg/util/nativeutils/eventbus/ipc.go
@@ -7,19 +7,20 @@
 package eventbus
 
 import (
+	"net"
+
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/message"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
-	"net"
 )
 
 // IPCListener is an implementation of eventbus.Listener that allows processes
-// to connect to a bus over a duplex serial connection (pipe, socket, tcp, etc)
+// to connect to a bus over a duplex serial connection (pipe, socket, tcp, etc).
 type IPCListener struct {
 	net.Conn
 }
 
-// NewIPCListener initialises and attaches a listener over a net.Conn with a
-// mapper function to route received messages
+// NewIPCListener initializes and attaches a listener over a net.Conn with a
+// mapper function to route received messages.
 func NewIPCListener(
 	c net.Conn,
 	mapper func(topic topics.Topic) byte,
@@ -40,14 +41,14 @@ func (il *IPCListener) detach() {
 
 }
 
-// Notify passes a message to be relayed by the attached eventbus
+// Notify passes a message to be relayed by the attached eventbus.
 func (il *IPCListener) Notify(message message.Message) error {
 	// dispatch message to eventbus
 	_ = message
 	return nil
 }
 
-// Close the connection and stop receiving Notify calls
+// Close the connection and stop receiving Notify calls.
 func (il *IPCListener) Close() {
 	// close the gRPC
 	il.detach()
