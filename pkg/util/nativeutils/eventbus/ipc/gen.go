b08f4a39fbda512e8e2224722fdf455b7e79b6e6
Loki Verloren <loki@cybriq.systems>
2021-09-15 13:54:38 +0300

    revising proto and wondering about the copyright headers

--- pkg/util/nativeutils/eventbus/ipc/gen.go
+++ pkg/util/nativeutils/eventbus/ipc/gen.go
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT License was not distributed with this
+// file, you can obtain one at https://opensource.org/licenses/MIT.
+//
+// Copyright (c) DUSK NETWORK. All rights reserved.
+
 package ipc
 
 // by this line here, the protocol buffer code can be regenerated with
