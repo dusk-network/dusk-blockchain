b710cdd47b810a3bd2708e1f78ec7360e2a00f9a
loki <david@dusk.network>
2021-12-03 13:37:34 +0200

    Initial test for simple fill/empty

--- pkg/p2p/peer/priority_test.go
+++ pkg/p2p/peer/priority_test.go
@@ -10,7 +10,7 @@ func genMessage(pri bool) (msg []byte) {
 	msg = make([]byte, 1000)
 	if pri {
 		// randomly select the value in one of the priority topics
-		msg[0]=byte(Priority[rand.Intn(len(Priority))])
+		msg[0] = byte(Priority[rand.Intn(len(Priority))])
 	} else {
 		// just use the very first topic since anyway that makes it land in non-priority,
 		// and because that is zero, nothing to be done
@@ -20,13 +20,30 @@ func genMessage(pri bool) (msg []byte) {
 }
 
 // TestPriorityQueue runs a set of basic tests to make sure the queue fills and
-// empties correctly
+// empties correctly. Fills all queues equally, which means they empty equally,
+// non-priority first
 func TestPriorityQueue(t *testing.T) {
-	q := NewPriorityQueue(20, 3)
 	var zero uint32 = 0
+	var qSize uint32 = 20
+	q := NewPriorityQueue(int(qSize), 3)
 	assert.Equal(t, zero, q.Plen.Load())
 	assert.Equal(t, zero, q.Nlen.Load())
-	t.Logf("testing testing\n")
+	for i := uint32(0); i < qSize; i++ {
+		q.Push(genMessage(true))
+		q.Push(genMessage(false))
+	}
+	assert.Equal(t, qSize, q.Plen.Load())
+	assert.Equal(t, qSize, q.Nlen.Load())
+	var counter int
+	for msg := q.Pop(); msg != nil; msg = q.Pop() {
+		assert.Equal(t, msg[0]==0, counter%2==0)
+		//if msg[0] == 0 {
+		//	t.Logf("popping non-priority")
+		//} else {
+		//	t.Logf("popping priority")
+		//}
+		counter++
+	}
 }
 
 // TestPriorityQueuePriorityFlood tests queue behavior under conditions of DoS of
