53aa5b119cfae2c3e9c8fe2a2a6be26cfd4187be
Loki <david@dusk.network>
2021-12-07 16:03:31 +0200

    Add freelist for message buffers for testing to eliminate GC

--- pkg/p2p/peer/priority_test.go
+++ pkg/p2p/peer/priority_test.go
@@ -6,6 +6,40 @@ import (
 	"testing"
 )
 
+const zero uint32 = 0
+const qSize uint32 = 20
+
+// freeList is a simple memory allocator to avoid GC during tests on raw queue processing
+type freeList struct {
+	buf   [][]byte
+	alloc map[*[]byte]bool
+}
+
+func newFreeList(size int) (fl freeList) {
+	fl.buf = make([][]byte, size)
+	for i := range fl.buf {
+		fl.buf[i] = make([]byte, 64)
+	}
+	for i := range fl.buf {
+		fl.alloc[&fl.buf[i]] = false
+	}
+	return
+}
+
+func (fl *freeList) Alloc() *[]byte {
+	for i := range fl.alloc {
+		if !fl.alloc[i] {
+			fl.alloc[i] = true
+			return i
+		}
+	}
+	panic("free list was not made big enough for purpose")
+}
+
+func (fl *freeList) Free(buf *[]byte) {
+	fl.alloc[buf] = false
+}
+
 func genMessage(pri bool) (msg []byte) {
 	msg = make([]byte, 1000)
 	if pri {
@@ -23,8 +57,6 @@ func genMessage(pri bool) (msg []byte) {
 // empties correctly. Fills all queues equally, which means they empty equally,
 // non-priority first
 func TestPriorityQueue(t *testing.T) {
-	var zero uint32 = 0
-	var qSize uint32 = 20
 	q := NewPriorityQueue(int(qSize), 3)
 	assert.Equal(t, zero, q.Plen.Load())
 	assert.Equal(t, zero, q.Nlen.Load())
@@ -47,18 +79,30 @@ func TestPriorityQueue(t *testing.T) {
 	}
 	out += "}"
 	assert.Equal(t, out, expected)
-	//t.Logf("\n%s\n", out) // this will print what is coming out to put in the expected above
+	t.Logf("\n%s\n", out) // this will print what is coming out to put in the expected above
 }
 
 // TestPriorityQueuePriorityFlood tests queue behavior under conditions of DoS of
-// priority messages. The goal of the test is to see how quickly the nonpriority
+// priority messages. The goal of the test is to see how quickly the non-priority
 // pool drains while 2x as many priority messages come in.
 //
 // In order to eliminate the play of the Go garbage collector, for this and the
-// following test the foregoing freelist is used so no deallocation is required
+// following test the foregoing freelist is used so no de-allocation is required
 // for the lifetime of the test. A benchmark can be derived also from this code
 // to determine the overhead incurred by the queue.
+//
+// This test uses factor=3 because it exactly copes with this 2x condition.
 func TestPriorityQueuePriorityFlood(t *testing.T) {
+	q := NewPriorityQueue(int(qSize), 3)
+	assert.Equal(t, zero, q.Plen.Load())
+	assert.Equal(t, zero, q.Nlen.Load())
+	for i := uint32(0); i < qSize/2; i++ {
+		q.Push(genMessage(true))
+		q.Push(genMessage(true))
+		q.Push(genMessage(false))
+	}
+	assert.Equal(t, qSize, q.Plen.Load())
+	assert.Equal(t, qSize/2, q.Nlen.Load())
 
 }
 
