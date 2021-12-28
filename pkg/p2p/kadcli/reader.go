a485996e2c525951f25802d0eb72a8c6bfb196a7
t00ts <abel@dusk.network>
2021-12-28 13:29:46 +0100

    Propagate received messages as KadcastPoints and pass height to upper layers

--- pkg/p2p/kadcli/reader.go
+++ pkg/p2p/kadcli/reader.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/peer"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/checksum"
+	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/message"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/protocol"
 	"github.com/dusk-network/dusk-blockchain/pkg/p2p/wire/topics"
 	"github.com/dusk-network/dusk-blockchain/pkg/util/nativeutils/eventbus"
@@ -24,6 +25,7 @@ import (
 // Reader abstracts all of the logic and fields needed to receive messages from
 // other network nodes.
 type Reader struct {
+	publisher eventbus.Publisher
 	processor *peer.MessageProcessor
 	gossip    *protocol.Gossip
 
@@ -34,6 +36,7 @@ type Reader struct {
 // NewReader makes a new kadcast reader that handles TCP packets of broadcasting.
 func NewReader(publisher eventbus.Publisher, g *protocol.Gossip, p *peer.MessageProcessor, rusk rusk.NetworkClient) *Reader {
 	return &Reader{
+		publisher: publisher,
 		processor: p,
 		gossip:    g,
 		client:    rusk,
@@ -76,24 +79,24 @@ func (r *Reader) Listen() {
 }
 
 // processMessage propagates the received kadcast message into the event bus.
-func (r *Reader) processMessage(message *rusk.Message) {
-	reader := bytes.NewReader(message.Message)
+func (r *Reader) processMessage(msg *rusk.Message) {
+	reader := bytes.NewReader(msg.Message)
 
 	// read message (extract length and magic)
 	b, err := r.gossip.ReadMessage(reader)
 	if err != nil {
-		log.WithField("r_addr", message.Metadata.SrcAddress).
+		log.WithField("r_addr", msg.Metadata.SrcAddress).
 			WithError(err).Warnln("error reading message")
 		return
 	}
 	// extract checksum
-	msg, cs, err := checksum.Extract(b)
+	m, cs, err := checksum.Extract(b)
 	if err != nil {
 		log.WithError(err).Warnln("error extracting message and cs")
 		return
 	}
 	// verify checksum
-	if !checksum.Verify(msg, cs) {
+	if !checksum.Verify(m, cs) {
 		log.WithError(errors.New("invalid checksum")).
 			Warnln("error verifying message cs")
 		return
@@ -101,15 +104,25 @@ func (r *Reader) processMessage(message *rusk.Message) {
 
 	// collect (process) the message
 	go func() {
-		if _, err = r.processor.Collect(message.Metadata.SrcAddress, msg, nil, protocol.FullNode, nil); err != nil {
+		respBufs, err := r.processor.Collect(msg.Metadata.SrcAddress, m, nil, protocol.FullNode, []byte{byte(msg.Metadata.KadcastHeight)})
+		if err != nil {
 			var topic string
-			if len(msg) > 0 {
-				topic = topics.Topic(msg[0]).String()
+			if len(m) > 0 {
+				topic = topics.Topic(m[0]).String()
 			}
 			// log error
 			log.WithField("cs", hex.EncodeToString(cs)).
 				WithField("topic", topic).
 				WithError(err).Error("failed to process message")
+			return
+		}
+		// any response message is translated into Kadcast Point-to-Point wire message
+		// in other words, any bufs item is sent back to the sender node (remotePeer)
+		for i := 0; i < len(respBufs); i++ {
+			log.WithField("r_addr", msg.Metadata.SrcAddress).Trace("send point-to-point message")
+			// send Kadcast point-to-point message with source address as destination
+			msg := message.NewWithHeader(topics.KadcastPoint, respBufs[i], []byte(msg.Metadata.SrcAddress))
+			r.publisher.Publish(topics.KadcastPoint, msg)
 		}
 	}()
 }
