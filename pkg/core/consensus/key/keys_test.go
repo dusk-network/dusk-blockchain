e4fe9297f4f5ef30f72f9ffc90366b23806ad521
Herr Seppia <seppia@dusk.network>
2022-01-31 14:37:16 +0100

    Change the encryption schema for the consensus wallet
    See also dusk-network/rusk#504

--- pkg/core/consensus/key/keys_test.go
+++ pkg/core/consensus/key/keys_test.go
@@ -8,6 +8,7 @@ package key
 
 import (
 	"bytes"
+	b64 "encoding/base64"
 	"os"
 	"testing"
 	"time"
@@ -26,6 +27,29 @@ func TestRandKeys(t *testing.T) {
 
 	assert.True(t, !bytes.Equal(k.BLSPubKey, k2.BLSPubKey))
 	assert.True(t, !bytes.Equal(k.BLSSecretKey, k2.BLSSecretKey))
+}
+
+func TestEncryptedKeysFromFile(t *testing.T) {
+	const (
+		password = "greatpassword"
+	)
+
+	walletenc := "s9CrfaDJY9H5wsBnj9qvRpXaocXHCxJGRd/hhhVfpvnIsW4SuV5AAeS+tVaW6NIVnjVShk6fthtC" +
+		"iAhslbb50lzNxE0pAaiWxuAW4skZxoQCbxXoAK7SMcRez6A4cY94uKpmoakFCji1u8weg7qVzV8C" +
+		"OXxpPlDo62UFwiuH+ZNhnktq+wD3VNAd/raBDQed+59nLoeTD1gE7mEgmxSncUd8+eDHzhmAMdUK" +
+		"5hRU6u/LuQO/vZjQO6WlognYKhLzmr1kSvNTk/hctjmtLOW08LKlwigSRLpaJV1VVZP81HFnVZRS" +
+		"VfRcVAJT43m+Vwpe"
+
+	sDec, _ := b64.StdEncoding.DecodeString(walletenc)
+	path := os.TempDir() + "/consensus-test-fromrusk.keys"
+	err := os.WriteFile(path, sDec, 0644)
+	assert.Nil(t, err)
+
+	// try to load consensus keys with wrong pass
+	k2, err := NewFromFile(password, path)
+	assert.NotNil(t, k2)
+	assert.Nil(t, err)
+
 }
 
 func TestEncryptedKeys(t *testing.T) {
